189b64df70afd36df289eed1ae92a9d3a7a21282
Cardall, Christian Y <cardallcy@ornl.gov>
2017-02-24 14:16:32 -0500

    Temporarily commenting out Integrator_C_1D__Template.

--- Modules/Mathematics/Solvers/Evolutions/Integrators/Integrators.f90
+++ Modules/Mathematics/Solvers/Evolutions/Integrators/Integrators.f90
@@ -4,6 +4,6 @@ module Integrators
   use TimeSeries_Form
   use TimeSeries_C__Form
   use Integrator_C_ASC__Template
-  use Integrator_C_1D__Template
+!  use Integrator_C_1D__Template
 
 end module Integrators
