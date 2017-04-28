040d6bf68f4df3e75fb0fb16bc0999c869081919
Cardall, Christian Y <cardallcy@ornl.gov>
2017-04-27 22:55:32 -0400

    Reworking CONSTANT_Singleton. Will need to tweak UNIT_Singleton and beyond.

--- Modules/Basics/VariableManagement/Specifiers/CONSTANT_Singleton.f90
+++ Modules/Basics/VariableManagement/Specifiers/CONSTANT_Singleton.f90
@@ -12,13 +12,13 @@ module CONSTANT_Singleton
       PI  =  acos ( -1.0_KDR ), &
       !-- Physical MKS
       !   http://pdg.lbl.gov/2016/reviews/rpp2016-rev-phys-constants.pdf
-      SPEED_OF_LIGHT_MKS  =  2.99792458e+8_KDR, &
-      PLANCK_REDUCED_MKS  =  1.054571800e-34_KDR, &
-      PERMEABILITY_MKS    =  4.0e-7_KDR * PI, & 
-      GRAVITATIONAL_MKS   =  6.67408e-11_KDR, &
-      AVOGADRO_MKS        =  6.022140857e+23_KDR, &
-      BOLTZMANN_MKS       =  1.38064852e-23_KDR, &
-      ELECTRON_VOLT_MKS   =  1.6021766208e-19_KDR, &
+      SPEED_OF_LIGHT_MKS    =  2.99792458e+8_KDR, &
+      PLANCK_REDUCED_MKS    =  1.054571800e-34_KDR, &
+      ELECTRON_CHARGE_MKS   =  1.6021766208e-19_KDR, &
+      ATOMIC_MASS_UNIT_MKS  =  1.660539040e-27_KDR, &
+      PERMEABILITY_MKS      =  4.0e-7_KDR * PI, & 
+      GRAVITATIONAL_MKS     =  6.67408e-11_KDR, &
+      BOLTZMANN_MKS         =  1.38064852e-23_KDR, &
       !-- Astrophysical MKS
       !   http://pdg.lbl.gov/2016/reviews
       !          /rpp2016-rev-astrophysical-constants.pdf
@@ -26,49 +26,58 @@ module CONSTANT_Singleton
       SOLAR_MASS_MKS        = 1.98848e+30_KDR, &
       !-- GenASiS Fundamental
       SPEED_OF_LIGHT  =  1.0_KDR, &
+      PLANCK_REDUCED  =  1.0_KDR, &    
       PERMEABILITY    =  1.0_KDR, &
-      GRAVITATIONAL   =  1.0_KDR, &
       BOLTZMANN       =  1.0_KDR, &
       !-- GenASiS Metric
-      METER         =  1.0_KDR, &
-      SECOND        =  SPEED_OF_LIGHT_MKS / SPEED_OF_LIGHT &
-                       *  METER, &
-      KILOGRAM      =  GRAVITATIONAL_MKS / GRAVITATIONAL &
-                       *  METER ** 3  /  SECOND ** 2, &
-      KELVIN        =  BOLTZMANN_MKS / BOLTZMANN &
-                       *  KILOGRAM  *  METER ** 2  /  SECOND ** 2, &
-      AMPERE        =  sqrt ( ( PERMEABILITY_MKS / PERMEABILITY ) &
-                              *  KILOGRAM  *  METER  /  SECOND ** 2 ), &
-      ELECTRON_VOLT =  ELECTRON_VOLT_MKS &
-                       *  KILOGRAM  *  METER ** 2  /  SECOND ** 2, &
+      MEGA_ELECTRON_VOLT  =  1.0_KDR, &
+      JOULE               =  1.0e-6_KDR * MEGA_ELECTRON_VOLT &
+                             / ELECTRON_CHARGE_MKS, & 
+      SECOND              =  PLANCK_REDUCED * SPEED_OF_LIGHT &
+                             / ( PLANCK_REDUCED_MKS * SPEED_OF_LIGHT_MKS ) &
+                             / JOULE, &
+      METER               =  SPEED_OF_LIGHT / SPEED_OF_LIGHT_MKS &
+                             * SECOND, &
+      KILOGRAM            =  JOULE  *  SECOND ** 2  /  METER ** 2, &
+      AMPERE              =  sqrt ( ( PERMEABILITY_MKS / PERMEABILITY ) &
+                                    *  KILOGRAM  *  METER  /  SECOND ** 2 ), &
+      KELVIN              =  BOLTZMANN_MKS / BOLTZMANN &
+                             *  JOULE, &
       !-- GenASiS Derived
-      PLANCK_REDUCED    =  PLANCK_REDUCED_MKS  &
-                           *  KILOGRAM  *  METER ** 2  /  SECOND, &    
-      RADIATION         =  PI ** 2  /  15.0_KDR &
+      ELECTRON_CHARGE   =  ELECTRON_CHARGE_MKS &
+                           *  AMPERE * SECOND, &
+      ATOMIC_MASS_UNIT  =  ATOMIC_MASS_UNIT_MKS &
+                           *  KILOGRAM, &
+      GRAVITATIONAL     =  GRAVITATIONAL_MKS &
+                           *  METER ** 3  /  KILOGRAM  /  SECOND ** 2, &
+      STEFAN_BOLTZMANN  =  PI ** 2  /  60.0_KDR &
                            *  BOLTZMANN ** 4 &
                            /  ( PLANCK_REDUCED * SPEED_OF_LIGHT ) ** 3, &
-      ATOMIC_MASS_UNIT  =  AVOGADRO_MKS ** (-1) &
-                           *  1.0e-3_KDR  *  KILOGRAM, &
       ASTRONOMICAL_UNIT =  ASTRONOMICAL_UNIT_MKS &
                            *  METER, &
       SOLAR_MASS        =  SOLAR_MASS_MKS * KILOGRAM
 
   type, public :: ConstantSingleton
     real ( KDR ) :: &
-      PI                 =  PI, &
-      SPEED_OF_LIGHT     =  SPEED_OF_LIGHT, &
-      PERMEABILITY       =  PERMEABILITY, &
-      GRAVITATIONAL      =  GRAVITATIONAL, &
-      BOLTZMANN          =  BOLTZMANN, &
-      METER              =  METER, &
-      SECOND             =  SECOND, &
-      KILOGRAM           =  KILOGRAM, &
-      KELVIN             =  KELVIN, &
-      AMPERE             =  AMPERE, &
-      ELECTRON_VOLT      =  ELECTRON_VOLT, &
-      PLANCK_REDUCED     =  PLANCK_REDUCED, &
-      RADIATION          =  RADIATION, &
-      ATOMIC_MASS_UNIT   =  ATOMIC_MASS_UNIT, &
+      !-- Mathematical
+      PI                  =  PI, &
+      !-- Metric, SI Base 
+      !   https://en.wikipedia.org/wiki/SI_base_unit
+      METER     =  METER, &
+      KILOGRAM  =  KILOGRAM, &
+      SECOND    =  SECOND, &
+      AMPERE    =  AMPERE, &
+      KELVIN    =  KELVIN, &
+      !-- Physical
+      SPEED_OF_LIGHT    =  SPEED_OF_LIGHT, &
+      PLANCK_REDUCED    =  PLANCK_REDUCED, &
+      ELECTRON_CHARGE   =  ELECTRON_CHARGE, &
+      ATOMIC_MASS_UNIT  =  ATOMIC_MASS_UNIT, &
+      PERMEABILITY      =  PERMEABILITY, &
+      GRAVITATIONAL     =  GRAVITATIONAL, &
+      BOLTZMANN         =  BOLTZMANN, &
+      STEFAN_BOLTZMANN  =  STEFAN_BOLTZMANN, &
+      !-- Astrophysical
       ASTRONOMICAL_UNIT  =  ASTRONOMICAL_UNIT, &
       SOLAR_MASS         =  SOLAR_MASS
   end type ConstantSingleton
--- Programs/UnitTests/Basics/VariableManagement/Specifiers/CONSTANT_Singleton_Test.f90
+++ Programs/UnitTests/Basics/VariableManagement/Specifiers/CONSTANT_Singleton_Test.f90
@@ -6,19 +6,19 @@ program CONSTANT_Singleton_Test
 
   print*
   print*, 'PI                = ', CONSTANT % PI
+  print*, 'METER             = ', CONSTANT % METER
+  print*, 'KILOGRAM          = ', CONSTANT % KILOGRAM
+  print*, 'SECOND            = ', CONSTANT % SECOND
+  print*, 'AMPERE            = ', CONSTANT % AMPERE
+  print*, 'KELVIN            = ', CONSTANT % KELVIN
   print*, 'SPEED_OF_LIGHT    = ', CONSTANT % SPEED_OF_LIGHT
+  print*, 'PLANCK_REDUCED    = ', CONSTANT % PLANCK_REDUCED
+  print*, 'ELECTRON_CHARGE   = ', CONSTANT % ELECTRON_CHARGE
+  print*, 'ATOMIC_MASS_UNIT  = ', CONSTANT % ATOMIC_MASS_UNIT
   print*, 'PERMEABILITY      = ', CONSTANT % PERMEABILITY
   print*, 'GRAVITATIONAL     = ', CONSTANT % GRAVITATIONAL
   print*, 'BOLTZMANN         = ', CONSTANT % BOLTZMANN
-  print*, 'METER             = ', CONSTANT % METER
-  print*, 'SECOND            = ', CONSTANT % SECOND
-  print*, 'KILOGRAM          = ', CONSTANT % KILOGRAM
-  print*, 'KELVIN            = ', CONSTANT % KELVIN
-  print*, 'AMPERE            = ', CONSTANT % AMPERE
-  print*, 'ELECTRON_VOLT     = ', CONSTANT % ELECTRON_VOLT
-  print*, 'PLANCK_REDUCED    = ', CONSTANT % PLANCK_REDUCED
-  print*, 'RADIATION         = ', CONSTANT % RADIATION
-  print*, 'ATOMIC_MASS_UNIT  = ', CONSTANT % ATOMIC_MASS_UNIT
+  print*, 'STEFAN_BOLTZMANN  = ', CONSTANT % STEFAN_BOLTZMANN
   print*, 'ASTRONOMICAL_UNIT = ', CONSTANT % ASTRONOMICAL_UNIT
   print*, 'SOLAR_MASS        = ', CONSTANT % SOLAR_MASS
 
