748c38facc6e65dcb97e4db3a02484e0ce0b34cc
Cardall, Christian Y <cardallcy@ornl.gov>
2017-04-28 21:22:35 -0400

    Adding CHARACTER members to KIND_DEFAULT_Singleton and KIND_BIG_Singleton to allow specification of selected_char_kind 'ASCII' and 'ISO_10646' respectively.

--- Modules/Basics/VariableManagement/Specifiers/KIND_BIG_Singleton.f90
+++ Modules/Basics/VariableManagement/Specifiers/KIND_BIG_Singleton.f90
@@ -10,13 +10,15 @@ module KIND_BIG_Singleton
 
   type, public :: KindBigSingleton
     integer ( kind ( KDI ) ) :: &
-      INTEGER = selected_int_kind ( 15 )
+      INTEGER   = selected_int_kind ( 15 ), &
+      CHARACTER = selected_char_kind ( 'ISO_10646' )
   end type KindBigSingleton
 
   type ( KindBigSingleton ), public, parameter :: &
     KIND_BIG = KindBigSingleton ( )
 
   integer ( KDI ), public, parameter :: &
-    KBI = KIND_BIG % INTEGER
+    KBI  = KIND_BIG % INTEGER, &
+    KBCH = KIND_BIG % CHARACTER
 
 end module KIND_BIG_Singleton
--- Programs/UnitTests/Basics/VariableManagement/Specifiers/KIND_DEFAULT_Singleton_Test.f90
+++ Programs/UnitTests/Basics/VariableManagement/Specifiers/KIND_DEFAULT_Singleton_Test.f90
@@ -4,17 +4,30 @@ program KIND_DEFAULT_Singleton_Test
 
   implicit none
 
-  print *
-  print *, 'KIND_DEFAULT % INTEGER = ', KIND_DEFAULT % INTEGER
-  print *, 'KIND_DEFAULT % REAL    = ', KIND_DEFAULT % REAL
-  print *, 'KIND_DEFAULT % COMPLEX = ', KIND_DEFAULT % COMPLEX
-  print *, 'KIND_DEFAULT % LOGICAL = ', KIND_DEFAULT % LOGICAL
+  integer ( KDI ), parameter :: &
+    PRINTABLE_MIN = 32, &
+    PRINTABLE_MAX = 126, &
+    CHARACTERS_PER_ROW  = 32
+
+  integer ( KDI ) :: &
+    iV, &  !-- iValue
+    iC     !-- iCharacter
+  character ( 80 ) :: &
+    Row
 
   print *
-  print *, 'KDI = ', KDI
-  print *, 'KDR = ', KDR
-  print *, 'KDC = ', KDC
-  print *, 'KDL = ', KDL
+  print *, 'KIND_DEFAULT % INTEGER   = ', KIND_DEFAULT % INTEGER
+  print *, 'KIND_DEFAULT % REAL      = ', KIND_DEFAULT % REAL
+  print *, 'KIND_DEFAULT % COMPLEX   = ', KIND_DEFAULT % COMPLEX
+  print *, 'KIND_DEFAULT % LOGICAL   = ', KIND_DEFAULT % LOGICAL
+  print *, 'KIND_DEFAULT % CHARACTER = ', KIND_DEFAULT % CHARACTER
+
+  print *
+  print *, 'KDI  =  ', KDI
+  print *, 'KDR  =  ', KDR
+  print *, 'KDC  =  ', KDC
+  print *, 'KDL  =  ', KDL
+  print *, 'KDCH =  ', KDCH
 
   print *
   print *, 'huge ( 0_KDI ) = ', huge ( 0_KDI )
@@ -22,5 +35,23 @@ program KIND_DEFAULT_Singleton_Test
   print *
   print *, 'tiny ( 0.0_KDR ) = ', tiny ( 0.0_KDR )
   print *, 'huge ( 0.0_KDR ) = ', huge ( 0.0_KDR )
+
+  print *
+  print *, 'ASCII Printable'
+
+  iC  = 0
+  Row = ''
+  do iV = PRINTABLE_MIN, PRINTABLE_MAX
+
+    iC = iC + 1
+    Row ( 2 * iC : 2 * iC ) = char ( iV, kind = KDCH )
+
+    if ( iC == CHARACTERS_PER_ROW .or. iV == PRINTABLE_MAX ) then
+      print *, Row
+      iC  = 0
+      Row = ''
+    end if
+
+  end do !-- iV
 
 end program KIND_DEFAULT_Singleton_Test
