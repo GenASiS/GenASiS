67cbca9aa7f9a14c74d95969d774c8fb45f0e6a0
Daniel Murphy <rmurph16@vols.utk.edu>
2017-07-21 11:53:28 -0400

    RadiationMoments_Form properly compiles with the Broyden solver implemented in ComputeComovingEnergyMomentumBroyden. RootFinding_Form now has Broyden solver.

--- Modules/Mathematics/Solvers/NonlinearEquations/RootFinding_Form.f90
+++ Modules/Mathematics/Solvers/NonlinearEquations/RootFinding_Form.f90
@@ -9,7 +9,7 @@ module RootFinding_Form
     integer ( KDI ), private :: &
       IGNORABILITY
     integer ( KDI ) :: &
-      MaximumIteration = 20, &
+      MaximumIteration = 100, &
       nIterations      = 0
     real ( KDR ) :: &
       RequestedAccuracy = 1.0e-8_KDR, &
@@ -21,6 +21,8 @@ module RootFinding_Form
     procedure ( ZeroFunctionEvaluatorInterface ), private, pointer, nopass :: &
       FunctionEvaluator => null ( ), &
       FunctionDerivativeEvaluator  => null ( )
+    procedure ( MD_FunctionEvaluatorInterface ), private, pointer, nopass :: &
+      MD_FunctionEvaluator => null ( )
     class ( * ), private, pointer :: &
       FunctionParameters => null ( )
   contains
@@ -32,11 +34,16 @@ module RootFinding_Form
       SolveSecant
     procedure, private, pass :: &
       SolveNewtonRaphson
+    procedure, private, pass :: &
+      SolveBroyden
     generic :: &
-      Solve => SolveBrent, SolveSecant, SolveNewtonRaphson
+      Solve => SolveBrent, SolveSecant, SolveNewtonRaphson, SolveBroyden
     final :: &
       Finalize
   end type RootFindingForm
+
+  private :: &
+    ComputeInverseJacobian
   
   abstract interface
     subroutine ZeroFunctionEvaluatorInterface ( Parameters, Input, Result )
@@ -50,22 +57,40 @@ module RootFinding_Form
         Result
     end subroutine ZeroFunctionEvaluatorInterface
   end interface
+
+  abstract interface
+    subroutine MD_FunctionEvaluatorInterface ( Parameters, Input, Result )
+      use Basics
+      implicit none
+      class ( * ), intent ( in ) :: &
+        Parameters
+      real ( KDR ), dimension ( : ), intent ( in ) :: &
+        Input
+      real ( KDR ), dimension ( : ), intent ( out ) :: &
+        Result
+    end subroutine MD_FunctionEvaluatorInterface
+  end interface
   
   public :: &
     ZeroFunctionEvaluatorInterface
   
 contains
 
-
+!-- Move Accuracy & Parameters to solve calls.
   subroutine Initialize &
-               ( RF, FunctionParameters, FunctionEvaluator, AccuracyOption, &
-                 MaximumIterationOption, VerbosityOption )
+               ( RF, FunctionParameters, FunctionEvaluatorOption, &
+                 MD_FunctionEvaluatorOption, AccuracyOption, &
+                 MaximumIterationOption, VerbosityOption  )
     class ( RootFindingForm ), intent ( inout ) :: &
       RF
     class ( * ), intent ( in ), target :: &
       FunctionParameters
-    procedure ( ZeroFunctionEvaluatorInterface ), intent ( in ), pointer :: &
-      FunctionEvaluator
+    procedure ( ZeroFunctionEvaluatorInterface ), &
+                intent ( in ), pointer, optional :: &
+      FunctionEvaluatorOption
+    procedure ( MD_FunctionEvaluatorInterface ), &
+                 intent ( in ), pointer, optional :: &
+      MD_FunctionEvaluatorOption
     real ( KDR ), intent ( in ), optional :: &
       AccuracyOption
     integer ( KDI ), intent ( in ), optional :: &
@@ -82,7 +107,15 @@ contains
       RF % RequestedAccuracy = AccuracyOption
     
     RF % FunctionParameters => FunctionParameters
-    RF % FunctionEvaluator => FunctionEvaluator
+    
+    if ( present ( FunctionEvaluatorOption ) ) then
+      RF % FunctionEvaluator => FunctionEvaluatorOption
+    else if ( present ( MD_FunctionEvaluatorOption ) ) then
+      RF % MD_FunctionEvaluator => MD_FunctionEvaluatorOption
+    else
+      call Show ( ' No Function Evaluator Specified ', CONSOLE % ERROR )
+     ! call PROGRAM_HEADER % abort
+    end if 
   
   end subroutine Initialize
   
@@ -220,19 +253,6 @@ contains
       
       RF % nIterations = iIteration
       
-      RF % SolutionAccuracy = abs ( Y_1 )
-      Root = X_1
-      
-      if ( abs ( ( X_1 - X_0 ) /  X_0 ) <= RF % RequestedAccuracy &
-           .or. abs ( Y_1 ) <= RF % RequestedAccuracy ) &
-      then
-        RF % Success = .true. 
-        exit
-      end if
-      
-      if ( Y_1 == Y_0 ) &
-        exit
-
       X = X_1 - Y_1 * ( X_1 - X_0 ) / ( Y_1 - Y_0 )
       
       X_0 = X_1
@@ -240,6 +260,16 @@ contains
       
       X_1 = X
       call RF % FunctionEvaluator ( RF % FunctionParameters, X_1, Y_1 )
+      
+      RF % SolutionAccuracy = abs ( Y_1 )
+      
+      if ( abs ( ( X_1 - X_0 ) /  X_0 ) <= RF % RequestedAccuracy &
+           .or. abs ( Y_1 ) <= RF % RequestedAccuracy ) &
+      then
+        RF % Success = .true. 
+        Root = X_1
+        exit
+      end if
       
     end do
     
@@ -277,10 +307,12 @@ contains
     !call Derivative(Parameters, x1, df)
     !call Function(Parameters, x2, fh)
     !call Derivative(Parameters, x2, df)
-    
+
     call RF % FunctionEvaluator ( RF % FunctionParameters, x1, fl )
+    call Show ( fl, 'fl' )
     call RF % FunctionDerivativeEvaluator ( RF % FunctionParameters, x1, df ) 
     call RF % FunctionEvaluator ( RF % FunctionParameters, x2, fh )
+    call Show ( fh, 'fh' )
     call RF % FunctionDerivativeEvaluator ( RF % FunctionParameters, x2, df ) 
     
     if ( ( fl > 0.0_KDR .and. fh > 0.0_KDR ) &
@@ -320,6 +352,8 @@ contains
     call RF % FunctionDerivativeEvaluator ( RF % FunctionParameters, Root, df ) 
     
     do iIteration = 1, RF % MaximumIteration
+      RF % nIterations = iIteration
+
       if ( ( ( Root-xh ) * df - f ) * ( ( Root - xl ) * df - f ) > 0.0_KDR &
            .or. abs ( 2.0_KDR * f ) > abs ( dxold * df ) ) then
         dxold = dx
@@ -352,6 +386,115 @@ contains
 
   end subroutine SolveNewtonRaphson
 
+
+  subroutine SolveBroyden ( RF, Guess_1, Guess_2, Root )
+  
+     class ( RootFindingForm ), intent ( inout ) :: &
+       RF
+     real ( KDR ), dimension ( : ), intent ( in ) :: &
+       Guess_1, &
+       Guess_2
+     real ( KDR ), dimension ( : ), intent ( out ) :: &
+       Root
+      
+     
+    integer ( KDI ) :: &
+      iIteration, &
+      i, &
+      j
+    real ( KDR ) :: &
+      IJ_Y, &
+      dX_L2, &
+      X_0_L2, &
+      X_Error, &
+      Y_Error
+    real ( KDR ), dimension ( : ), allocatable :: &
+      X, &
+      X_0, X_1, dX, &
+      Y_0, Y_1, dY
+    real ( KDR ), dimension ( :, : ), allocatable :: &
+      IJ_0, &
+      IJ_1
+      
+    RF % Success = .false.
+    RF % nIterations = 0
+    
+    allocate &
+      ( X ( size ( Guess_1 ) ), &
+        X_0 ( size ( Guess_1 ) ), &
+        X_1 ( size ( Guess_1 ) ), &
+        dX  ( size ( Guess_1 ) ), &
+        Y_0 ( size ( Guess_1 ) ), &
+        Y_1 ( size ( Guess_1 ) ), &
+        dY  ( size ( Guess_1 ) ), &
+        IJ_0 ( size ( Guess_1 ), size ( Guess_1 ) ), &
+        IJ_1 ( size ( Guess_1 ), size ( Guess_1 ) ) )
+
+    IJ_0 = 0.0_KDR  
+    IJ_1 = 0.0_KDR
+
+    do i = 1, size ( Guess_1 )
+      IJ_0 ( i, i ) = 1.0_KDR
+    end do !-- i
+
+    X_0 = Guess_1
+    X_1 = Guess_2
+    
+    call RF % MD_FunctionEvaluator ( RF % FunctionParameters, X_0, Y_0 )
+    call RF % MD_FunctionEvaluator ( RF % FunctionParameters, X_1, Y_1 )
+
+    do iIteration = 1, RF % MaximumIteration
+      
+      RF % nIterations = iIteration
+
+      dX = X_1 - X_0
+      dY = Y_1 - Y_0
+
+      call ComputeInverseJacobian ( IJ_0, dX, dY, IJ_1 )
+      
+      do i = 1, size ( Guess_1 )
+        IJ_Y = 0.0_KDR
+        do j = 1, size ( Guess_1 )
+          IJ_Y = IJ_Y + IJ_1 ( i, j ) * Y_1 ( j )
+        end do !-- j
+        X ( i ) = X_1 ( i ) - IJ_Y
+      end do !-- i
+      
+      X_0 = X_1
+      Y_0 = Y_1
+      IJ_0 = IJ_1
+      
+      X_1 = X
+
+      call RF % MD_FunctionEvaluator ( RF % FunctionParameters, X_1, Y_1 )
+
+      X_Error = 0.0_KDR
+      Y_Error = 0.0_KDR
+
+      do i = 1, size ( Guess_1 )
+        dX_L2  = dX_L2 + ( X_1 ( i ) - X_0 ( i ) ) ** 2.0_KDR 
+        X_0_L2 = X_0_L2 + X_0 ( i ) ** 2.0_KDR
+        Y_Error = Y_Error + Y_1 ( i ) ** 2.0_KDR
+      end do !-- i
+
+      X_Error = sqrt ( dX_L2 ) / ( 1.0_KDR + sqrt ( X_0_L2 ) )
+      Y_Error = sqrt ( Y_Error )
+
+      RF % SolutionAccuracy = Y_Error
+
+      if ( X_Error <= RF % RequestedAccuracy &
+           .or. Y_Error <= RF % RequestedAccuracy ) then
+        RF % Success = .true. 
+        Root = X_1
+        exit
+      end if
+      
+    end do
+
+    deallocate ( X, X_0, X_1, dX, Y_0, Y_1, dY, IJ_0, IJ_1 )
+    
+  end subroutine SolveBroyden
+
   
   Subroutine Finalize ( RF )
   
@@ -363,6 +506,50 @@ contains
     nullify ( RF % FunctionEvaluator )
   
   end subroutine Finalize
+
+
+  subroutine ComputeInverseJacobian ( IJ_0, dX, dY, IJ_1 )
+    !-- Rudimentary Application of Sherman-Morrison formula 
+   real ( KDR ), dimension ( :, : ), intent ( in ) :: &
+     IJ_0
+   real ( KDR ), dimension ( : ), intent ( in ) :: &
+     dX, &
+     dY
+   real ( KDR ), dimension ( :, : ), intent ( out ) :: &
+     IJ_1 
+   
+   integer ( KDI ) :: &
+     i, &
+     j
+   real ( KDR ) :: &
+     dX_IJ_dY
+   real ( KDR ), dimension ( size ( dX ) ) :: &
+     dX_IJ, &
+     IJ_dY  
+    
+   IJ_dY = 0.0_KDR
+   dX_IJ = 0.0_KDR
+   dX_IJ_dY = 0.0_KDR
+
+    do i = 1, size ( dX )
+      do j = 1, size ( dX )
+        IJ_dY ( i ) = IJ_dY ( i ) + IJ_0 ( i, j ) * dY ( j )
+        dX_IJ ( i ) = dX_IJ ( i ) + dX ( j ) * IJ_0 ( j, i )
+      end do !-- j
+      dX_IJ_dY = dX_IJ_dY + dX_IJ ( i ) * dY ( i )
+    end do !-- i
+
+    dX_IJ_dY = max ( dX_IJ_dY, tiny ( 0.0_KDR ) )
+
+    do i = 1, size ( dX )
+      do j = 1, size ( dX )
+        IJ_1 ( i, j ) = IJ_0 ( i, j ) &
+                        + ( dX ( i ) - IJ_dY ( i ) ) / dX_IJ_dY * dX_IJ ( j )
+      end do !-- j
+    end do !-- i
+
+  end subroutine ComputeInverseJacobian
+
   
 
 end module RootFinding_Form
--- Programs/Examples/Mathematics/GreyTransport2/RadiationMoments_Form.f90
+++ Programs/Examples/Mathematics/GreyTransport2/RadiationMoments_Form.f90
@@ -85,6 +85,8 @@ module RadiationMoments_Form
       ComputeRawFluxes
     procedure, public, pass ( C ) :: &
       ComputeDiffusionFactor_HLL
+    procedure, public, nopass :: &
+      ComputeComovingStress_D
   end type RadiationMomentsForm
 
     private :: &
@@ -99,7 +101,6 @@ module RadiationMoments_Form
 
       private :: &
         ComputeMomentFactors, &
-        ComputeComovingStress_D, &
         ComputeComovingNonlinearSolve, &
         FunctionWrapper, &
         SetParameters, &
@@ -517,7 +518,7 @@ contains
       DF_I
     class ( * ), intent ( in ), target :: &
       Grid
-    class ( RadiationMomentsForm ), intent ( in ) :: &
+    class ( RadiationMomentsForm ), intent ( inout ) :: &
       C
     integer ( KDI ), intent ( in ) :: &
       iDimension
@@ -868,20 +869,18 @@ contains
                H_3 ( iV ), M_DD_22 ( iV ), M_DD_33 ( iV ) )
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 1 ), J ( iV ), H_1 ( iV ), H_2 ( iV ), &
-               H_3 ( iV ), H_1 ( iV ), FF ( iV ), SF ( iV ), &
-               M_DD_22 ( iV ), M_DD_33 ( iV ) )
+             ( K_U_Dim_D, [ H_1 ( iV ), H_2 ( iV ), H_3 ( iV ) ], J ( iV ), &
+               SF ( iV ), M_DD_22 ( iV ), M_DD_33 ( iV ), iD = 1 )
+
       S_1 ( iV )  =  H_1 ( iV )  +  J ( iV ) * V_1 ( iV ) &
                      +  K_U_Dim_D ( 1 )  *  V_1 ( iV )  &
                      +  K_U_Dim_D ( 2 )  *  V_2 ( iV )  &
                      +  K_U_Dim_D ( 3 )  *  V_3 ( iV )
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 2 ), J ( iV ), H_1 ( iV ), H_2 ( iV ), &
-               H_3 ( iV ), H_2 ( iV ), FF ( iV ), SF ( iV ), &
-               M_DD_22 ( iV ), M_DD_33 ( iV ) )
+             ( K_U_Dim_D, [ H_1 ( iV ), H_2 ( iV ), H_3 ( iV ) ], J ( iV ), &
+               SF ( iV ), M_DD_22 ( iV ), M_DD_33 ( iV ), iD = 2 )
+
       S_2 ( iV )  =  M_DD_22 ( iV )  &
                      *  ( H_2 ( iV )  +  J ( iV ) * V_2 ( iV )  &
                           +  K_U_Dim_D ( 1 )  *  V_1 ( iV )  &
@@ -889,10 +888,9 @@ contains
                           +  K_U_Dim_D ( 3 )  *  V_3 ( iV ) )
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 3 ), J ( iV ), H_1 ( iV ), H_2 ( iV ), &
-               H_3 ( iV ), H_3 ( iV ), FF ( iV ), SF ( iV ), &
-               M_DD_22 ( iV ), M_DD_33 ( iV ) )
+             ( K_U_Dim_D, [ H_1 ( iV ), H_2 ( iV ), H_3 ( iV ) ], J ( iV ), &
+               SF ( iV ), M_DD_22 ( iV ), M_DD_33 ( iV ), iD = 3 )
+
       S_3 ( iV )  =  M_DD_33 ( iV )  &
                      *  ( H_3 ( iV )  +  J ( iV ) * V_3 ( iV )  &
                           +  K_U_Dim_D ( 1 )  *  V_1 ( iV )  &
@@ -1043,7 +1041,7 @@ contains
                   E ( iV ), S_1 ( iV ), S_2 ( iV ), S_3 ( iV ), &
                   Parameters )
          call ComputeInitialGuesses ( Guess_1, Guess_2, Parameters )
-         call RF % SolveBroyden ( Guess_1, Guess_2, Root )
+         call RF % Solve ( Guess_1, Guess_2, Root )
 
         if ( .not. RF % Success ) then
           call Show ( '>>> ComputeComoving fail', CONSOLE % ERROR )
@@ -1170,10 +1168,8 @@ contains
     do iV = 1, nValues
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( iDim ), J ( iV ), H_1 ( iV ), H_2 ( iV ), &
-               H_3 ( iV ), H_Dim ( iV ), FF ( iV ), SF ( iV ), &
-               M_DD_22 ( iV ), M_DD_33 ( iV ) )
+             ( K_U_Dim_D, [ H_1 ( iV ), H_2 ( iV ), H_3 ( iV ) ], J ( iV ), &
+               SF ( iV ), M_DD_22 ( iV ), M_DD_33 ( iV ), iDim )
       
       F_E   ( iV )  =  H_Dim ( iV )  +  J ( iV ) * V_Dim ( iV )  &
                        +  K_U_Dim_D ( 1 )  *  V_1 ( iV )  &
@@ -1298,36 +1294,37 @@ contains
   end subroutine ComputeMomentFactors
 
 
-  subroutine ComputeComovingStress_D &
-               ( K_1, K_2, K_3, K_Dim, J, H_1, H_2, H_3, H_Dim, FF, SF, &
-                 M_DD_22, M_DD_33 )
+  subroutine ComputeComovingStress_D ( K_D, H_U, J, SF, M_DD_22, M_DD_33, iD )
 
-    real ( KDR ), intent ( inout ) :: &
-      K_1, K_2, K_3, K_Dim
+    real ( KDR ), dimension ( 3 ), intent ( inout ) :: &
+      K_D
+    real ( KDR ), dimension ( 3 ), intent ( in ) :: &
+      H_U
     real ( KDR ), intent ( in ) :: &
       J, &
-      H_1, H_2, H_3, H_Dim, &
-      FF, SF, &
+      SF, &
       M_DD_22, M_DD_33
+    integer ( KDI ), intent ( in ) :: &
+      iD
 
+    integer ( KDI ) :: &
+      jD
     real ( KDR ) :: &
-      H_Sq
+       H_Sq
+ 
+    H_Sq  =  max (                H_U ( 1 ) ** 2  &
+                   +  M_DD_22  *  H_U ( 2 ) ** 2  &
+                   +  M_DD_33  *  H_U ( 3 ) ** 2, & 
+                   tiny ( 0.0_KDR ) ) 
 
-    H_Sq  =  max (                H_1 ** 2  &
-                   +  M_DD_22  *  H_2 ** 2  &
-                   +  M_DD_33  *  H_3 ** 2, &
-                   tiny ( 0.0_KDR ) )   
-
-    K_1  =  0.5_KDR  *  ( 3.0_KDR * SF  -  1.0_KDR )  &
-            *  H_Dim  *            H_1  /  H_Sq   *  J
-
-    K_2  =  0.5_KDR  *  ( 3.0_KDR * SF  -  1.0_KDR )  &
-            *  H_Dim  *  M_DD_22 * H_2  /  H_Sq   *  J
-
-    K_3  =  0.5_KDR  *  ( 3.0_KDR * SF  -  1.0_KDR )  &
-            *  H_Dim  *  M_DD_33 * H_3  /  H_Sq   *  J
-
-    K_Dim  =  K_Dim  +  0.5_KDR * ( 1.0_KDR  -  SF )  *  J
+    do jD = 1, 3
+      K_D ( jD )  =  0.5_KDR  *  ( 3.0_KDR * SF  -  1.0_KDR )  &
+                     *  H_U ( iD )  *  H_U ( jD )  /  H_Sq   *  J
+    end do
+    K_D ( 2 )  =  M_DD_22 * K_D ( 2 )
+    K_D ( 3 )  =  M_DD_33 * K_D ( 3 )
+    
+    K_D ( iD )  =  K_D ( iD )  +  0.5_KDR * ( 1.0_KDR  -  SF )  *  J
 
   end subroutine ComputeComovingStress_D
 
@@ -1378,27 +1375,24 @@ contains
              ( SF, FF, J_Old, H_Old_1, H_Old_2, H_Old_3, M_DD_22, M_DD_33  )
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 1 ), J_Old, H_Old_1, H_Old_2, H_Old_3, H_Old_1, &
-               FF, SF, M_DD_22, M_DD_33 )
+             ( K_U_Dim_D, [ H_Old_1, H_Old_2, H_Old_3 ], J_Old, SF, &
+               M_DD_22, M_DD_33, iD = 1 )
       H_1  =  S_1  -  J_Old * V_1   &
                    -  K_U_Dim_D ( 1 )  *  V_1   &
                    -  K_U_Dim_D ( 2 )  *  V_2   &
                    -  K_U_Dim_D ( 3 )  *  V_3 
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 2 ), J_Old, H_Old_1, H_Old_2, H_Old_3, H_Old_2, &
-               FF, SF, M_DD_22, M_DD_33 )
+             ( K_U_Dim_D, [ H_Old_1, H_Old_2, H_Old_3 ], J_Old, SF, &
+               M_DD_22, M_DD_33, iD = 2 )
       H_2  =  M_UU_22 * S_2  -  J_Old  * V_2   &
                              -  K_U_Dim_D ( 1 )  *  V_1   &
                              -  K_U_Dim_D ( 2 )  *  V_2   &
                              -  K_U_Dim_D ( 3 )  *  V_3 
 
       call ComputeComovingStress_D &
-             ( K_U_Dim_D ( 1 ), K_U_Dim_D ( 2 ), K_U_Dim_D ( 3 ), &
-               K_U_Dim_D ( 3 ), J_Old, H_Old_1, H_Old_2, H_Old_3, H_Old_3, &
-               FF, SF, M_DD_22, M_DD_33 )
+             ( K_U_Dim_D, [ H_Old_1, H_Old_2, H_Old_3 ], J_Old, SF, &
+               M_DD_22, M_DD_33, iD = 3 )
       H_3  =  M_UU_33 * S_3  -  J_Old * V_3   &
                              -  K_U_Dim_D ( 1 )  *  V_1   &
                              -  K_U_Dim_D ( 2 )  *  V_2   &
@@ -1415,8 +1409,8 @@ contains
       Delta_J_J  =  abs ( J - J_Old )  /  max ( abs ( J ), tiny ( 0.0_KDR ) )
       Delta_H_H  =  NormDelta_H  /  max ( Norm_H, tiny ( 0.0_KDR ) )
 
-      if ( Delta_J_J  <  1.0e-8_KDR &
-           .and. Delta_H_H  <  1.0e-8_KDR ) &
+      if ( Delta_J_J  <  1.0e-10_KDR &
+           .and. Delta_H_H  <  1.0e-9_KDR ) &
       then
         Success  =  .true.
         exit
@@ -1441,7 +1435,8 @@ contains
      jV
     real ( KDR ) :: &
       H, &
-      k, &
+      FF, &
+      SF, &
       vH, &
       L_X
    real ( KDR ), dimension ( 3 ) :: &
@@ -1458,8 +1453,6 @@ contains
                  + P % M_DD ( 2 ) * Input ( 3 ) ** 2.0_KDR &
                  + P % M_DD ( 3 ) * Input ( 4 ) ** 2.0_KDR )
 
-      call ComputeEddingtonFactor ( Input ( 1 ), H, k )
-
       L = 0.0_KDR
       L ( 1, 1 ) = 1.0_KDR
 
@@ -1475,8 +1468,8 @@ contains
       b  = 0.0_KDR 
       
       do iV = 2, 4 
-        Kv ( iV - 1 ) = 0.5_KDR * ( ( 1.0_KDR - k ) * P % V ( iV - 1 ) &
-                       + ( 3.0_KDR * k - 1.0_KDR ) * Input ( iV ) * vH &
+        Kv ( iV - 1 ) = 0.5_KDR * ( ( 1.0_KDR - P % SF ) * P % V ( iV - 1 ) &
+                       + ( 3.0_KDR * P % SF - 1.0_KDR ) * Input ( iV ) * vH &
                           / max (  H ** 2, tiny ( 0.0 ) ) )
         b ( iV ) = Kv ( iV - 1 ) * Input ( 1 )
       end do !-- iV
