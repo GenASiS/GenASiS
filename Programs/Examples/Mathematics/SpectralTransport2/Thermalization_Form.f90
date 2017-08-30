7da575774a14d720bbf2d92d1ae84044e124d8d8
Cardall, Christian Y <cardallcy@ornl.gov>
2017-08-30 16:25:13 -0400

    Thermalization working, though there are a couple things I'd still like to do to it.

--- Programs/Examples/Mathematics/SpectralTransport2/Thermalization_Form.f90
+++ Programs/Examples/Mathematics/SpectralTransport2/Thermalization_Form.f90
@@ -7,6 +7,7 @@ module Thermalization_Form
   use RadiationMoments_Form
   use RadiationMoments_ASC__Form
   use Interactions_F__Form
+  use ApplyRelaxation_RM__Command
   use SetPlanckSpectrum_Command
   use RadiationMoments_BSLL_ASC_CSLD__Form
   use Interactions_BSLL_ASC_CSLD__Form
@@ -166,7 +167,7 @@ contains
     S % ApplyDivergence   % Pointer => null ( )  !-- Disable fluid evolution
     S % ApplyDivergence_S % Pointer => null ( )  !-- Disable spatial 
                                                  !   section evolution
-    S % ApplyRelaxation_F % Pointer => ApplyRelaxation_Interactions
+    S % ApplyRelaxation_F % Pointer => ApplyRelaxation_RM
     end select !-- S
 
     !-- Diagnostics
