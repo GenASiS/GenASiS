a89711504693ef0f9f32c79c25516bb7295a73db
Cardall, Christian Y <cardallcy@ornl.gov>
2017-10-03 10:35:17 -0400

    Removing obsolete spectral Step.

--- Modules/Mathematics/Solvers/DifferentialEquations/Evolutions/Steps/Steps.f90
+++ Modules/Mathematics/Solvers/DifferentialEquations/Evolutions/Steps/Steps.f90
@@ -4,10 +4,8 @@ module Steps
   use Step_RK_C_ASC__Template
   use Step_RK_C_ASC_1D__Template
   use Step_RK_C_BSLL_ASC_CSLD__Template
-  use Step_RK_C_BSLL_ASC_CSLD_C_ASC__Template
   use Step_RK2_C_ASC__Form
   use Step_RK2_C_ASC_1D__Form
   use Step_RK2_C_BSLL_ASC_CSLD__Form
-  use Step_RK2_C_BSLL_ASC_CSLD_C_ASC__Form
 
 end module Steps
