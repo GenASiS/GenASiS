16b67b75cab900196de54c41c9ca5984d0f8e1b0
Cardall, Christian Y <cardallcy@ornl.gov>
2017-10-31 13:28:04 -0400

    Starting test form.

--- Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Constraints/PoissonEquations/Poisson_ASC__Form_Test.f90
+++ Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Constraints/PoissonEquations/Poisson_ASC__Form_Test.f90
@@ -10,6 +10,52 @@ module Poisson_ASC__Form_Test__Form
   character ( LDF ), public, parameter :: &
     ProgramName = 'Poisson_ASC__Form_Test'
 
+  type, public :: Poisson_ASC__Form_Test_Form
+    type ( GridImageStreamForm ), allocatable :: &
+      Stream
+    type ( GeometryFlat_ASC_Form ), allocatable :: &
+      Geometry
+    type ( Atlas_SC_Form ), allocatable :: &
+      Atlas
+!    type ( Poisson_ASC_Form ), allocatable :: &
+!      Poisson
+  contains
+    procedure, public, pass :: &
+      Initialize
+    final :: &
+      Finalize
+  end type Poisson_ASC__Form_Test_Form
+
+contains
+
+
+  subroutine Initialize ( PFT, Name )
+
+    class ( Poisson_ASC__Form_Test_Form ) :: &
+      PFT
+    character ( * ), intent ( in ) :: &
+      Name
+
+  end subroutine Initialize
+
+
+  subroutine Finalize ( PFT )
+
+    type ( Poisson_ASC__Form_Test_Form ) :: &
+      PFT
+
+!    if ( allocated ( PFT % Laplacian ) ) &
+!      deallocate ( PFT % Laplacian )
+    if ( allocated ( PFT % Geometry ) ) &
+      deallocate ( PFT % Geometry )
+    if ( allocated ( PFT % Atlas ) ) &
+      deallocate ( PFT % Atlas )
+    if ( allocated ( PFT % Stream ) ) &
+      deallocate ( PFT % Stream )
+
+  end subroutine Finalize
+
+
 end module Poisson_ASC__Form_Test__Form
 
 
@@ -21,15 +67,15 @@ program Poisson_ASC__Form_Test
 
   implicit none
 
-!  type ( Poisson_ASC__Form_Test_Form ), allocatable :: &
-!    LMFT
+  type ( Poisson_ASC__Form_Test_Form ), allocatable :: &
+    PFT
 
   allocate ( PROGRAM_HEADER )
   call PROGRAM_HEADER % Initialize ( ProgramName )
 
-!  allocate ( LMFT )
-!  call LMFT % Initialize ( PROGRAM_HEADER % Name )
-!  deallocate ( LMFT )
+  allocate ( PFT )
+  call PFT % Initialize ( PROGRAM_HEADER % Name )
+  deallocate ( PFT )
 
   deallocate ( PROGRAM_HEADER )
 
