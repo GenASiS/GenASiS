47f8fd65391347f79ee94e15e7c4d79fd26fec2b
Cardall, Christian Y <cardallcy@ornl.gov>
2018-04-23 16:16:42 -0400

    FluidFeatures_CSL__Form

--- Modules/Physics/StressEnergies/Fluids/Fluids.f90
+++ Modules/Physics/StressEnergies/Fluids/Fluids.f90
@@ -4,10 +4,13 @@ module Fluids
   use Fluid_D__Form
   use Fluid_P__Template
   use Fluid_P_I__Form
+  use FluidFeatures_P__Form
   use Tally_F_D__Form
+  use Tally_F_P__Form
   use Sources_F__Form
   use Sources_F_CSL__Form
   use Sources_F_ASC__Form
+  use FluidFeatures_CSL__Form
   use Fluid_CSL__Form
   use Fluid_ASC__Form
   use ApplyCurvilinear_F__Command
