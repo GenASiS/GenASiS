c4a4e055eedfecf00bc092b94a6cf46a38762daf
Cardall, Christian Y <cardallcy@ornl.gov>
2018-04-23 11:44:07 -0400

    Geometry_CSL__Form_Test

---
+++ Programs/UnitTests/Physics/Spaces/Geometry_CSL__Form_Test.f90
@@ -0,0 +1,44 @@
+program Geometry_CSL__Form_Test
+
+  use Basics
+  use Mathematics
+  use Geometry_CSL__Form
+
+  implicit none
+
+  character ( LDF ) :: &
+    ProgramName = 'Geometry_CSL__Form_Test'
+
+  type ( Atlas_SC_Form ), allocatable :: &
+    A_G, A_N
+  type ( Geometry_CSL_Form ), allocatable :: &
+    GC_G, GC_N
+
+  allocate ( PROGRAM_HEADER )  
+  call PROGRAM_HEADER % Initialize ( ProgramName )
+
+  allocate ( A_G, A_N )
+  call A_G % Initialize ( 'Atlas_SC_G', PROGRAM_HEADER % Communicator )
+  call A_N % Initialize ( 'Atlas_SC_N', PROGRAM_HEADER % Communicator )
+  call A_G % CreateChart ( )
+  call A_N % CreateChart ( )
+
+  select type ( C_G => A_G % Chart )
+  class is ( Chart_SL_Template )
+  select type ( C_N => A_N % Chart )
+  class is ( Chart_SL_Template )
+
+  associate ( nValues => C_G % nProperCells + C_G % nGhostCells )
+
+  allocate ( GC_G, GC_N )
+  call GC_G % Initialize ( C_G, 'Geometry_CSL_G', 'GALILEAN', nValues )
+  call GC_N % Initialize ( C_N, 'Geometry_CSL_N', 'NEWTONIAN', nValues )
+
+  deallocate ( GC_G, GC_N )
+  end associate !-- nValues
+  end select !-- C_N
+  end select !-- C_G
+  deallocate ( A_N, A_G )
+  deallocate ( PROGRAM_HEADER )
+
+end program Geometry_CSL__Form_Test
