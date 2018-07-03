136f6e9361aaf7a84bb138ba176df5f477875218
Reuben D. Budiardja <reubendb@ornl.gov>
2018-07-03 16:09:56 -0400

    DirectoryDifference now loops over filenumber in OutputDirectory.

--- Programs/Examples/Basics/Analysis/DirectoryDifference.f90
+++ Programs/Examples/Basics/Analysis/DirectoryDifference.f90
@@ -5,9 +5,14 @@ program DirectoryDifference
   use Basics
   use L1_Norm_Function
   
+  implicit none
+  
   integer ( KDI ) :: &
+    iV, &
     iS, &
-    Filenumber
+    iFile, &
+    FilenumberStart, &
+    FilenumberEnd
   integer ( KDI ), dimension ( 3 ) :: &
     iaFirst, &
     iaLast, &
@@ -21,7 +26,8 @@ program DirectoryDifference
     OutputDirectory_1, &
     OutputDirectory_2
   type ( GridImageStreamForm ), allocatable, target :: &
-    GIS
+    GIS_1, &
+    GIS_2
   class ( GridImageSiloTemplate ), allocatable :: &
     GI_1, &
     GI_2
@@ -35,8 +41,11 @@ program DirectoryDifference
   Filename = 'FilenameNotSet'
   call PH % GetParameter ( Filename, 'Filename' )
   
-  Filenumber = 0
-  call PH % GetParameter ( Filenumber, 'Filenumber' )
+  FilenumberStart = 0
+  call PH % GetParameter ( FilenumberStart, 'FilenumberStart' )
+  
+  FilenumberEnd = huge ( 1_KDI )
+  call PH % GetParameter ( FilenumberEnd, 'FilenumberEnd' )
   
   OutputDirectory_1 = '../Output/'
   call PH % GetParameter ( OutputDirectory_1, 'OutputDirectory_1' )
@@ -44,122 +53,133 @@ program DirectoryDifference
   OutputDirectory_2 = '../Output/'
   call PH % GetParameter ( OutputDirectory_2, 'OutputDirectory_2' )
   
-  allocate ( GIS )
-  call GIS % Initialize &
+  allocate ( GIS_1 )
+  call GIS_1 % Initialize &
         ( Filename, PH % Communicator, &
           WorkingDirectoryOption = OutputDirectory_1 )
-  
-  call GIS % Open ( GIS % ACCESS_READ, NumberOption = Filenumber )
-  call GIS % ListContents ( ContentTypeOption = 'Directory' )
-  
-  if ( trim ( GIS % ContentList ( 1 ) ) == 'Curves' ) then
-    allocate ( CurveImageForm :: GI_1 )
-    allocate ( CurveImageForm :: GI_2 )
-  else if ( trim ( GIS % ContentList ( 1 ) ) == 'Grid' ) then
-    allocate ( StructuredGridImageForm :: GI_1 )
-    allocate ( StructuredGridImageForm :: GI_2 )
-  end if
-  
-  call GI_1 % Initialize ( GIS )
-  call GI_1 % SetReadAttributes &
-         ( Directory = trim ( GIS % ContentList ( 1 ) ), oValue = 0 )
-  call GI_1 % Read ( )
-  
-  call GIS % Close ( )
- 
-  deallocate ( GIS ) !-- Trigger finalization
-  
-  allocate ( GIS )
-  call GIS % Initialize &
+          
+  allocate ( GIS_2 )
+  call GIS_2 % Initialize &
         ( Filename, PH % Communicator, &
           WorkingDirectoryOption = OutputDirectory_2 )
   
-  call GIS % Open ( GIS % ACCESS_READ, NumberOption = Filenumber )
-  call GIS % ListContents ( ContentTypeOption = 'Directory' )
-  
-  call GI_2 % Initialize ( GIS )
-  call GI_2 % SetReadAttributes &
-         ( Directory = trim ( GIS % ContentList ( 1 ) ), oValue = 0 )
-  call GI_2 % Read ( )
-  
-  call GIS % Close ( )
-  
-  deallocate ( GIS )
-  
-  !-- Calculate and write the differences
-  
-  select type ( GI_1 )
-  type is ( CurveImageForm )
-    iaFirst = 1 
-    iaLast  = [ GI_1 % nTotalCells, 1, 1 ]
-    nProperCells = [ GI_1 % nTotalCells, 1, 1 ]
-  type is ( StructuredGridImageForm )
-    iaFirst = 1 - GI_1 % oProperCellNodeLow
-    iaLast  = max ( GI_1 % nCells - GI_1 % oProperCellNodeLow, 1 )
-    nProperCells &
-      = max ( GI_1 % nCells - GI_1 % oProperCellNodeLow &
-                - GI_1 % oProperCellNodeHigh, 1 )
-  end select
+  do iFile = FilenumberStart, FilenumberEnd
     
-  do iS = 1, GI_1 % nStorages
+    call Show ( iFile, 'Opening Filenumber' )
+  
+    call GIS_1 % Open ( GIS_1 % ACCESS_READ, NumberOption = iFile )
+    call GIS_1 % ListContents ( ContentTypeOption = 'Directory' )
     
-    associate &
-      ( S_1 => GI_1 % Storage ( iS ), &
-        S_2 => GI_2 % Storage ( iS ) )
-    
-    allocate ( L1_Error ( S_1 % nVariables ) )
-    
-    do iV = 1, S_1 % nVariables
-      !-- Use pointer remapping to exclude computing error on ghost cells
-      V_1 &
-        ( iaFirst ( 1 ) : iaLast ( 1 ), &
-          iaFirst ( 2 ) : iaLast ( 2 ), &  
-          iaFirst ( 3 ) : iaLast ( 3 ) ) => S_1 % Value ( :, iV )
-      V_2 &
-        ( iaFirst ( 1 ) : iaLast ( 1 ), &
-          iaFirst ( 2 ) : iaLast ( 2 ), &  
-          iaFirst ( 3 ) : iaLast ( 3 ) ) => S_2 % Value ( :, iV )
+    if ( trim ( GIS_1 % ContentList ( 1 ) ) == 'Curves' ) then
+      allocate ( CurveImageForm :: GI_1 )
+      allocate ( CurveImageForm :: GI_2 )
+    else if ( trim ( GIS_1 % ContentList ( 1 ) ) == 'Grid' ) then
+      allocate ( StructuredGridImageForm :: GI_1 )
+      allocate ( StructuredGridImageForm :: GI_2 )
+    end if
       
-      L1_Error ( iV ) &
-        = L1_Norm ( V_1 ( 1 : nProperCells ( 1 ), &
-                          1 : nProperCells ( 2 ), &
-                          1 : nProperCells ( 3 ) ), &
-                    V_2 ( 1 : nProperCells ( 1 ), &
-                          1 : nProperCells ( 2 ), &
-                          1 : nProperCells ( 3 ) ) )
+    call GI_1 % Initialize ( GIS_1 )
+    call GI_1 % SetReadAttributes &
+           ( Directory = trim ( GIS_1 % ContentList ( 1 ) ), oValue = 0 )
+    call GI_1 % Read ( )
+    
+    call GIS_1 % Close ( )
+   
+    call GIS_2 % Open ( GIS_2 % ACCESS_READ, NumberOption = iFile )
+    call GIS_2 % ListContents ( ContentTypeOption = 'Directory' )
+    
+    call GI_2 % Initialize ( GIS_2 )
+    call GI_2 % SetReadAttributes &
+           ( Directory = trim ( GIS_2 % ContentList ( 1 ) ), oValue = 0 )
+    call GI_2 % Read ( )
+    
+    call GIS_2 % Close ( )
+    
+    
+    !-- Calculate and write the differences
+    
+    select type ( GI_1 )
+    type is ( CurveImageForm )
+      iaFirst = 1 
+      iaLast  = [ GI_1 % nTotalCells, 1, 1 ]
+      nProperCells = [ GI_1 % nTotalCells, 1, 1 ]
+    type is ( StructuredGridImageForm )
+      iaFirst = 1 - GI_1 % oProperCellNodeLow
+      iaLast  = max ( GI_1 % nCells - GI_1 % oProperCellNodeLow, 1 )
+      nProperCells &
+        = max ( GI_1 % nCells - GI_1 % oProperCellNodeLow &
+                  - GI_1 % oProperCellNodeHigh, 1 )
+    end select
       
-      !-- Avoid computing fractional differences for vanishing variable
-      if ( V_1 ( 1, 1, 1 ) == 0.0_KDR ) cycle
+    do iS = 1, GI_1 % nStorages
       
-      !V_1 = ( V_2 - V_1 ) / V_1
+      associate &
+        ( S_1 => GI_1 % Storage ( iS ), &
+          S_2 => GI_2 % Storage ( iS ) )
+      
+      allocate ( L1_Error ( S_1 % nVariables ) )
+      
+      do iV = 1, S_1 % nVariables
+        !-- Use pointer remapping to exclude computing error on ghost cells
+        V_1 &
+          ( iaFirst ( 1 ) : iaLast ( 1 ), &
+            iaFirst ( 2 ) : iaLast ( 2 ), &  
+            iaFirst ( 3 ) : iaLast ( 3 ) ) => S_1 % Value ( :, iV )
+        V_2 &
+          ( iaFirst ( 1 ) : iaLast ( 1 ), &
+            iaFirst ( 2 ) : iaLast ( 2 ), &  
+            iaFirst ( 3 ) : iaLast ( 3 ) ) => S_2 % Value ( :, iV )
+        
+        L1_Error ( iV ) &
+          = L1_Norm ( V_1 ( 1 : nProperCells ( 1 ), &
+                            1 : nProperCells ( 2 ), &
+                            1 : nProperCells ( 3 ) ), &
+                      V_2 ( 1 : nProperCells ( 1 ), &
+                            1 : nProperCells ( 2 ), &
+                            1 : nProperCells ( 3 ) ) )
+        
+        !-- Avoid computing fractional differences for vanishing variable
+        if ( V_1 ( 1, 1, 1 ) == 0.0_KDR ) cycle
+        
+        !V_1 = ( V_2 - V_1 ) / V_1
+        
+      end do
+                    
+      call Show ( 'L1 Norm Error for ' // trim ( S_1 % Name ) )
+      call Show ( L1_Error, S_1 % Variable )
+      
+      deallocate ( L1_Error )
+      
+      end associate
       
     end do
-                  
-    call Show ( 'L1 Norm Error for ' // trim ( S_1 % Name ) )
-    call Show ( L1_Error, S_1 % Variable )
-    
-    deallocate ( L1_Error )
-    
-    end associate
+  
+    deallocate ( GI_2 )
+    deallocate ( GI_1 )
+  
   end do
   
-  allocate ( GIS )
+  deallocate ( GIS_1 ) 
+  deallocate ( GIS_2 )
+    
   
-  call GIS % Initialize &
-         ( trim ( Filename ) // '_Difference', PH % Communicator, &
-           WorkingDirectoryOption = OutputDirectory_1 )
-  
-  call GIS % Open ( GIS % ACCESS_CREATE, SeriesOption = .false. )
-  
-  GI_1 % Stream => GIS
-  call GI_1 % Write ( )
-  
-  call GIS % Close ( )
-  
-  deallocate ( GI_2 )
-  deallocate ( GI_1 )
-  
-  deallocate ( GIS )
+!  allocate ( GIS )
+!  
+!  call GIS % Initialize &
+!         ( trim ( Filename ) // '_Difference', PH % Communicator, &
+!           WorkingDirectoryOption = OutputDirectory_1 )
+!  
+!  call GIS % Open ( GIS % ACCESS_CREATE, SeriesOption = .false. )
+!  
+!  GI_1 % Stream => GIS
+!  call GI_1 % Write ( )
+!  
+!  call GIS % Close ( )
+!  
+!  deallocate ( GI_2 )
+!  deallocate ( GI_1 )
+!  
+!  deallocate ( GIS )
   
   deallocate ( PROGRAM_HEADER )
   
