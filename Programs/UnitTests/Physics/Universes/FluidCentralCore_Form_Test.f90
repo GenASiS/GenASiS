c7251fcbf43f3d4aaafa6f8445a7ac4ef34009f1
Cardall, Christian Y <cardallcy@ornl.gov>
2018-08-15 16:26:12 -0400

    Setting some fluid variables to test coarsening.

--- Programs/UnitTests/Physics/Universes/FluidCentralCore_Form_Test.f90
+++ Programs/UnitTests/Physics/Universes/FluidCentralCore_Form_Test.f90
@@ -19,6 +19,8 @@ program FluidCentralCore_Form_Test
            GeometryType = 'NEWTONIAN', &
            DimensionlessOption = .true. )
 
+  call SetFluid ( FCC )
+
   call FCC % OpenManifoldStreams ( VerboseStreamOption = .true. )
   call FCC % Write ( )
 
@@ -29,3 +31,78 @@ program FluidCentralCore_Form_Test
   deallocate ( PROGRAM_HEADER )
 
 end program FluidCentralCore_Form_Test
+
+
+subroutine SetFluid ( FCC )
+
+  use Basics
+  use Mathematics
+  use StressEnergies
+  use FluidCentralCore_Form
+
+  type ( FluidCentralCoreForm ), intent ( inout ) :: &
+    FCC
+
+  integer ( KDI ) :: &
+    iC, jC, kC  !-- iCell, etc.
+  real ( KDR ) :: &
+    dS
+  real ( KDR ), dimension ( :, :, : ), pointer :: &
+    S_2, S_3
+  class ( Fluid_D_Form ), pointer :: &
+    F
+
+  select type ( FA => FCC % Current_ASC )
+  class is ( Fluid_ASC_Form )
+  F => FA % Fluid_D ( )
+
+  select type ( PS => FCC % PositionSpace )
+  class is ( Atlas_SC_Form )
+
+  select type ( C => PS % Chart )
+  class is ( Chart_SLD_CC_Form )
+
+  associate &
+    ( iaB => C % iaBrick, &
+      nCB => C % nCellsBrick )
+
+  call C % SetVariablePointer &
+         ( F % Value ( :, F % MOMENTUM_DENSITY_D ( 2 ) ), S_2 )
+  call C % SetVariablePointer &
+         ( F % Value ( :, F % MOMENTUM_DENSITY_D ( 3 ) ), S_3 )
+
+  if ( C % nDimensions < 2 ) &
+    return
+
+  dS  =  1.0_KDR  /  C % nCells ( 2 )
+
+  do kC = 1, nCB ( 3 )
+    do jC = 1, nCB ( 2 )
+      do iC = 1, nCB ( 1 )
+        S_2 ( iC, jC, kC )  &
+          =  ( ( iaB ( 2 ) - 1 ) * nCB ( 2 )  +  jC )  *  dS 
+      end do
+    end do
+  end do
+
+  if ( C % nDimensions < 3 ) &
+    return
+
+  dS  =  1.0_KDR  /  C % nCells ( 3 )
+
+  do kC = 1, nCB ( 3 )
+    do jC = 1, nCB ( 2 )
+      do iC = 1, nCB ( 1 )
+        S_3 ( iC, jC, kC )  &
+          =  ( ( iaB ( 3 ) - 1 ) * nCB ( 3 )  +  kC )  *  dS 
+      end do
+    end do
+  end do
+
+  end associate !-- iaB, etc.
+  end select !-- C
+  end select !-- PS
+  end select !-- FA
+  nullify ( F, S_2, S_3 )
+
+end subroutine SetFluid
