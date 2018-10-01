2193de8ac1de6466a1b81ec7a56cc6e40f4e61ef
Cardall, Christian Y <cardallcy@ornl.gov>
2018-10-01 14:49:52 -0400

    In FluidCentralCore_Form, alternating order of singularity coarsening.

--- Modules/Physics/Universes/FluidCentralCore_Form.f90
+++ Modules/Physics/Universes/FluidCentralCore_Form.f90
@@ -243,10 +243,18 @@ contains
     select type ( PS => FCC % PositionSpace )
     class is ( Atlas_SC_CC_Form )
 
-    if ( PS % nDimensions > 2 ) &
-      call FCC % CoarsenSingularityTemplate ( S, iAngular = 3 )
-    if ( PS % nDimensions > 1 ) &
-      call FCC % CoarsenSingularityTemplate ( S, iAngular = 2 )
+    select case ( mod ( FCC % iCycle, 2 ) )
+    case ( 0 )
+      if ( PS % nDimensions > 2 ) &
+        call FCC % CoarsenSingularityTemplate ( S, iAngular = 3 )
+      if ( PS % nDimensions > 1 ) &
+        call FCC % CoarsenSingularityTemplate ( S, iAngular = 2 )
+    case ( 1 )
+      if ( PS % nDimensions > 1 ) &
+        call FCC % CoarsenSingularityTemplate ( S, iAngular = 2 )
+      if ( PS % nDimensions > 2 ) &
+        call FCC % CoarsenSingularityTemplate ( S, iAngular = 3 )
+    end select
   
     end select !-- PS
     end associate !-- FCC
