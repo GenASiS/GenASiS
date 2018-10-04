e8dd10369d392e18f9a460c04b168505ae86b479
Cardall, Christian Y <cardallcy@ornl.gov>
2018-10-04 15:11:12 -0400

    Refactoring suitable members of Integrator_Template to IntegratorHeader_Form.

--- Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Evolutions/EvolutionBasics/IntegratorHeader_Form_Test.f90
+++ Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Evolutions/EvolutionBasics/IntegratorHeader_Form_Test.f90
@@ -9,54 +9,40 @@ module IntegratorHeader_Form_Test__Form
   character ( LDF ), public, parameter :: &
     ProgramName = 'IntegratorHeader_Form_Test'
 
-!   type, public :: IntegratorHeader_Form_Test_Form
-!     type ( IntegratorHeaderForm ), allocatable :: &
-!       IntegratorHeaderBase, &
-!       IntegratorHeaderFiber
-!   contains
-!     procedure, public, pass :: &
-!       Initialize
-!     final :: &
-!       Finalize
-!   end type IntegratorHeader_Form_Test_Form
+  type, public :: IntegratorHeader_Form_Test_Form
+    type ( IntegratorHeaderForm ), allocatable :: &
+      IntegratorHeader
+  contains
+    procedure, public, pass :: &
+      Initialize
+    final :: &
+      Finalize
+  end type IntegratorHeader_Form_Test_Form
 
-! contains
+contains
 
   
-!   subroutine Initialize ( AHFT )
+  subroutine Initialize ( IHFT )
 
-!     class ( IntegratorHeader_Form_Test_Form ), intent ( inout ), target :: &
-!       AHFT
+    class ( IntegratorHeader_Form_Test_Form ), intent ( inout ), target :: &
+      IHFT
 
-!     allocate ( AHFT % IntegratorHeaderBase )
-!     associate ( A => AHFT % IntegratorHeaderBase )
-!     call A % Initialize &
-!            ( 'Base', CommunicatorOption = PROGRAM_HEADER % Communicator, &
-!              iDimensionalityOption = 1 )
-!     call A % Connectivity % Show ( )
-!     call A % Show ( )
-!     end associate !-- AH
+    allocate ( IHFT % IntegratorHeader )
+    associate ( I => IHFT % IntegratorHeader )
+    call I % Initialize ( ProgramName )
+    end associate !-- I
 
-!     allocate ( AHFT % IntegratorHeaderFiber )
-!     associate ( A => AHFT % IntegratorHeaderFiber )
-!     call A % Initialize &
-!            ( 'Fiber', iDimensionalityOption = 2 )
-!     call A % Connectivity % Show ( )
-!     call A % Show ( )
-!     end associate !-- AH
-
-!   end subroutine Initialize
+  end subroutine Initialize
 
 
-!   subroutine Finalize ( AHFT )
+  subroutine Finalize ( IHFT )
 
-!     type ( IntegratorHeader_Form_Test_Form ), intent ( inout ) :: &
-!       AHFT
+    type ( IntegratorHeader_Form_Test_Form ), intent ( inout ) :: &
+      IHFT
 
-!     deallocate ( AHFT % IntegratorHeaderFiber )
-!     deallocate ( AHFT % IntegratorHeaderBase )
+    deallocate ( IHFT % IntegratorHeader )
 
-!   end subroutine Finalize
+  end subroutine Finalize
 
 
 end module IntegratorHeader_Form_Test__Form
@@ -70,15 +56,15 @@ program IntegratorHeader_Form_Test
   
   implicit none
 
-!  type ( IntegratorHeader_Form_Test_Form ), allocatable :: &
-!    IHFT
+  type ( IntegratorHeader_Form_Test_Form ), allocatable :: &
+    IHFT
 
   allocate ( PROGRAM_HEADER )
   call PROGRAM_HEADER % Initialize ( ProgramName )
     
-!  allocate ( IHFT )
-!  call IHFT % Initialize ( )
-!  deallocate ( IHFT )
+  allocate ( IHFT )
+  call IHFT % Initialize ( )
+  deallocate ( IHFT )
 
   deallocate ( PROGRAM_HEADER )
 
