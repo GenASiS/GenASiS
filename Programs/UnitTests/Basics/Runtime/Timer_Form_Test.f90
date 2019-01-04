0b6f93b7d2e222316d4e074f6d75874d995bd41f
Christian Cardall <cardallcy@ornl.gov>
2019-01-04 15:44:13 -0500

    Tweak for Cray compiler in Timer_Form_Test

--- Programs/UnitTests/Basics/Runtime/Timer_Form_Test.f90
+++ Programs/UnitTests/Basics/Runtime/Timer_Form_Test.f90
@@ -8,7 +8,8 @@ program Timer_Form_Test
   implicit none
 
   integer ( KDI ) :: &
-    i
+    i, &
+    nTimers
   type ( TimerForm ), dimension ( 5 ) :: &
     Timer
   type ( CommunicatorForm ), allocatable :: &
@@ -20,10 +21,11 @@ program Timer_Form_Test
 
   call Timer ( 1 ) % Initialize ( 'TestTimer 1', Level = 1 )
   call Timer ( 2 ) % Initialize ( 'TestTimer 2', Level = 1 )
+  nTimers = 2
 
   !-- Interval A1; Timer ( 1 )
 
-  call Show ( 'Interval A1', CONSOLE % INFO_3 )
+  call Show ( 'Interval A1', CONSOLE % INFO_1 )
   call Timer ( 1 ) % Start ( )
 
   do i = 1, 10000000
@@ -31,12 +33,12 @@ program Timer_Form_Test
   end do
 
   call Timer ( 1 ) % Stop ( )
-  call Timer ( 1 ) % ShowInterval ( CONSOLE % INFO_3 )
-  call Timer ( 1 ) % ShowTotal ( CONSOLE % INFO_3 )
+  call Timer ( 1 ) % ShowInterval ( CONSOLE % INFO_1 )
+  call Timer ( 1 ) % ShowTotal ( CONSOLE % INFO_1 )
 
   !-- Interval A2; Timer ( 1 )
 
-  call Show ( 'Interval A2', CONSOLE % INFO_3 )
+  call Show ( 'Interval A2', CONSOLE % INFO_1 )
   call Timer ( 1 ) % Start ( )
 
   do i = 1, 20000000
@@ -44,12 +46,12 @@ program Timer_Form_Test
   end do
 
   call Timer ( 1 ) % Stop ( )
-  call Timer ( 1 ) % ShowInterval ( CONSOLE % INFO_3 )
-  call Timer ( 1 ) % ShowTotal ( CONSOLE % INFO_3 )
+  call Timer ( 1 ) % ShowInterval ( CONSOLE % INFO_1 )
+  call Timer ( 1 ) % ShowTotal ( CONSOLE % INFO_1 )
 
   !-- Interval B; Timer ( 2 )
 
-  call Show ( 'Interval B', CONSOLE % INFO_3 )
+  call Show ( 'Interval B', CONSOLE % INFO_1 )
   call Timer ( 2 ) % Start ( )
 
   do i = 1, 30000000
@@ -57,13 +59,17 @@ program Timer_Form_Test
   end do
 
   call Timer ( 2 ) % Stop ( )
-  call Timer ( 2 ) % ShowInterval ( CONSOLE % INFO_3 )
-  call Timer ( 2 ) % ShowTotal ( CONSOLE % INFO_3 )
+  call Timer ( 2 ) % ShowInterval ( CONSOLE % INFO_1 )
+  call Timer ( 2 ) % ShowTotal ( CONSOLE % INFO_1 )
 
   !-- Show Totals
 
   call Show ( 'Totals for all Timers' )
-  call Timer % ShowTotal ( )
+!-- CCE doesn't like this array call
+!  call Timer % ShowTotal ( CONSOLE % INFO_1 )
+  do i = 1, nTimers
+    call Timer ( i ) % ShowTotal ( CONSOLE % INFO_1 )
+  end do
 
   deallocate ( C )
 
