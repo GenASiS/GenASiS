3840c8da73f44b4fea1756143b9767c07554521c
Cardall, Christian Y <cardallcy@ornl.gov>
2019-01-09 11:26:59 -0500

    Adding setup of ghost exchange in PlaneWaveAdvection, parallel to RiemannProblem.

--- Programs/Examples/Basics/FluidDynamics/PlaneWaveAdvection_Template.f90
+++ Programs/Examples/Basics/FluidDynamics/PlaneWaveAdvection_Template.f90
@@ -52,6 +52,8 @@ contains
       Wavenumber
     type ( MeasuredValueForm ), dimension ( 3 ) :: &
       VelocityUnit
+    type ( StorageForm ) :: &
+      Primitive
 
     call PWA % Initialize ( PROGRAM_HEADER % Communicator )
 
@@ -62,6 +64,10 @@ contains
     type is ( PressurelessFluidForm )
 
     call PF % Initialize ( DM, NameOption = 'PressurelessFluid' )
+    call PF % AllocateDevice ( )
+
+    call Primitive % Initialize ( PF, iaSelectedOption = PF % iaPrimitive )
+    call DM % SetGhostExchange ( Primitive )
 
     nWavelengths = 0
     nWavelengths ( 1 : DM % nDimensions ) = 1
