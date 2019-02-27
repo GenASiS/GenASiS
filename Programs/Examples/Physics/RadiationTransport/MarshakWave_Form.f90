40af265e6aae30090e32b454dca9e9229b095fb7
Cardall, Christian Y <cardallcy@ornl.gov>
2019-02-27 11:09:13 -0800

    InitializeInteractions in MarshakWave.

--- Programs/Examples/Physics/RadiationTransport/MarshakWave_Form.f90
+++ Programs/Examples/Physics/RadiationTransport/MarshakWave_Form.f90
@@ -31,18 +31,19 @@ module MarshakWave_Form
     !   MassDensity, &
     !   Temperature, &
       TemperatureInner, &
-    !   SpecificOpacity, &
-    !   SpecificOpacityFloor, &
-    !   EnergyScale, &
-    !   EnergyMax, &
+      SpecificOpacity, &
+      SpecificOpacityFloor, &
+      EnergyMax, &
     !   SoundSpeed
       FinishTime
     real ( KDR ), dimension ( 3 ), private :: &
       MinCoordinate, &
       MaxCoordinate
+    character ( LDF ) :: &
+      InteractionsType
 
-    class ( MarshakWaveForm ), private, pointer :: &
-      MarshakWave => null ( )
+!    class ( MarshakWaveForm ), private, pointer :: &
+!      MarshakWave => null ( )
 
 contains
 
@@ -58,7 +59,7 @@ contains
     if ( MW % Type == '' ) &
       MW % Type = 'a MarshakWave'
 
-    MarshakWave => MW
+!    MarshakWave => MW
 
 
     !-- Parameters
@@ -67,32 +68,38 @@ contains
       ( L      => BoxLength, &
         ! Gamma  => AdiabaticIndex, &
         ! C_V    => SpecificHeatCapacity, &
-        ! N_0    => MassDensity, &
+        ! Rho_0  => MassDensity, &
         ! T_0    => Temperature, &
-        T_I    => TemperatureInner )!, &
-        ! Kappa  => SpecificOpacity, &
-        ! Kappa_Min => SpecificOpacityFloor )
+        T_I    => TemperatureInner, &
+        Kappa  => SpecificOpacity, &
+        Kappa_Min => SpecificOpacityFloor )
 
     L      =  20.0_KDR    *  UNIT % CENTIMETER
     ! Gamma  =  1.4_KDR
     ! C_V    =  1.0_KDR     *  UNIT % ERG / UNIT % KELVIN / UNIT % GRAM
-    ! N_0    =  1.0e-3_KDR  *  UNIT % MASS_DENSITY_CGS
+    ! Rho_0  =  1.0e-3_KDR  *  UNIT % MASS_DENSITY_CGS
     ! T_0    =  3.0e2_KDR   *  UNIT % KELVIN
-    T_I    =  1.0e3_KDR   *  UNIT % KELVIN
-    ! Kappa  =  1.0e3_KDR   *  UNIT % CENTIMETER ** 2 / UNIT % GRAM
-    ! Kappa_Min    =  10.0_KDR  *  UNIT % CENTIMETER ** 2 / UNIT % GRAM
-    ! EnergyScale  =  T_I
+    T_I        =  1.0e3_KDR  *  UNIT % KELVIN
+    Kappa      =  1.0e3_KDR  *  UNIT % CENTIMETER ** 2 / UNIT % GRAM
+    Kappa_Min  =  10.0_KDR   *  UNIT % CENTIMETER ** 2 / UNIT % GRAM
 
     call PROGRAM_HEADER % GetParameter ( L,     'BoxLength' )
     ! call PROGRAM_HEADER % GetParameter ( Gamma, 'AdiabaticIndex' )
     ! call PROGRAM_HEADER % GetParameter ( C_V,   'SpecificHeatCapacity' )
     ! call PROGRAM_HEADER % GetParameter ( N_0,   'MassDensity' )
     ! call PROGRAM_HEADER % GetParameter ( T_0,   'Temperature' )
-    call PROGRAM_HEADER % GetParameter ( T_I,   'TemperatureInner' )
-    ! call PROGRAM_HEADER % GetParameter ( Kappa, 'SpecificOpacity' )
-    ! call PROGRAM_HEADER % GetParameter ( Kappa, 'SpecificOpacity' )
-    ! call PROGRAM_HEADER % GetParameter ( Kappa_Min,   'SpecificOpacityFloor' )
-    ! call PROGRAM_HEADER % GetParameter ( EnergyScale, 'EnergyScale' )
+    call PROGRAM_HEADER % GetParameter ( T_I,       'TemperatureInner' )
+    call PROGRAM_HEADER % GetParameter ( Kappa,     'SpecificOpacity' )
+    call PROGRAM_HEADER % GetParameter ( Kappa_Min, 'SpecificOpacityFloor' )
+
+    InteractionsType = 'MARSHAK_WAVE_VAYTET_1'
+    call PROGRAM_HEADER % GetParameter ( InteractionsType, 'InteractionsType' )
+
+    select case ( trim ( InteractionsType ) )
+    case ( 'MARSHAK_WAVE_VAYTET_2', 'MARSHAK_WAVE_VAYTET_3' )
+      EnergyMax  =  0.620_KDR * UNIT % ELECTRON_VOLT
+      call PROGRAM_HEADER % GetParameter ( EnergyMax, 'EnergyMax' )
+    end select !-- InteractionsType
 
     FinishTime  =  1.36e-7_KDR  *  UNIT % SECOND
 
@@ -100,7 +107,7 @@ contains
     !-- Initialization
 
     call InitializeRadiationBox ( MW, MomentsType, Name )
-
+    call InitializeInteractions ( MW, MomentsType )
 
     !-- Cleanup
 
@@ -159,6 +166,7 @@ contains
     !-- Momentum space parameters
 
     EnergyScale = TemperatureInner
+    call PROGRAM_HEADER % GetParameter ( EnergyScale, 'EnergyScale' )
 
 
     !-- Units
@@ -211,61 +219,70 @@ contains
   end subroutine InitializeRadiationBox
 
 
-  subroutine InitializeInteractions ( MW )
+  subroutine InitializeInteractions ( MW, MomentsType )
 
     class ( MarshakWaveForm ), intent ( inout ) :: &
       MW
+    character ( * ), intent ( in ) :: &
+      MomentsType
 
-    ! select type ( I => T % Integrator )
-    ! class is ( Integrator_C_1D_PS_C_PS_Form )  !-- Grey
+    select type ( I => MW % Integrator )
+    class is ( Integrator_C_1D_PS_C_PS_Form )  !-- Grey
 
-    !   select type ( RMA => I % Current_ASC_1D ( 1 ) % Element )
-    !   class is ( RadiationMoments_ASC_Form )
+      select type ( RMA => I % Current_ASC_1D ( 1 ) % Element )
+      class is ( RadiationMoments_ASC_Form )
 
-    !   select type ( FA => I % Current_ASC )
-    !   class is ( Fluid_ASC_Form )
+      select type ( FA => I % Current_ASC )
+      class is ( Fluid_ASC_Form )
 
-    !   select type ( PS => I % PositionSpace )
-    !   class is ( Atlas_SC_Form )
+      select type ( PS => I % PositionSpace )
+      class is ( Atlas_SC_Form )
 
-    !   allocate ( InteractionsExamples_ASC_Form :: T % Interactions_ASC )
-    !   select type ( IA => T % Interactions_ASC )
-    !   class is ( InteractionsExamples_ASC_Form )
-    !   call IA % Initialize &
-    !          ( PS, InteractionsType = 'CONSTANT', MomentsType = 'GREY' )
-    !   call IA % Set ( FA, OpacityAbsorption = OpacityAbsorption )
-    !   call RMA % SetInteractions ( IA )
-    !   end select !-- IA
+      allocate ( InteractionsExamples_ASC_Form :: MW % Interactions_ASC )
+      select type ( IA => MW % Interactions_ASC )
+      class is ( InteractionsExamples_ASC_Form )
+      call IA % Initialize &
+             ( PS, InteractionsType = InteractionsType, &
+               MomentsType = MomentsType )
+      select case ( trim ( InteractionsType ) )
+      case ( 'MARSHAK_WAVE_VAYTET_1' )
+        call IA % Set_MWV_Grey ( FA, SpecificOpacity = SpecificOpacity )
+      end select !-- InteractionsType
+      call RMA % SetInteractions ( IA )
+      end select !-- IA
 
-    !   end select !-- PS
-    !   end select !-- FA
-    !   end select !-- RMA
+      end select !-- PS
+      end select !-- FA
+      end select !-- RMA
 
-    ! class is ( Integrator_C_1D_MS_C_PS_Form )  !-- Spectral
+    class is ( Integrator_C_1D_MS_C_PS_Form )  !-- Spectral
 
-    !   select type ( RMB => I % Current_BSLL_ASC_CSLD_1D ( 1 ) % Element )
-    !   class is ( RadiationMoments_BSLL_ASC_CSLD_Form )
+      select type ( RMB => I % Current_BSLL_ASC_CSLD_1D ( 1 ) % Element )
+      class is ( RadiationMoments_BSLL_ASC_CSLD_Form )
 
-    !   select type ( FA => I % Current_ASC )
-    !   class is ( Fluid_ASC_Form )
+      select type ( FA => I % Current_ASC )
+      class is ( Fluid_ASC_Form )
 
-    !   select type ( MS => I % MomentumSpace )
-    !   class is ( Bundle_SLL_ASC_CSLD_Form )
+      select type ( MS => I % MomentumSpace )
+      class is ( Bundle_SLL_ASC_CSLD_Form )
 
-    !   allocate ( InteractionsExamples_BSLL_ASC_CSLD_Form :: &
-    !                T % Interactions_BSLL_ASC_CSLD )
-    !   select type ( IB => T % Interactions_BSLL_ASC_CSLD )
-    !   class is ( InteractionsExamples_BSLL_ASC_CSLD_Form )
-    !   call IB % Initialize ( MS, InteractionsType = 'CONSTANT' )
-    !   call IB % Set ( FA, OpacityAbsorption = OpacityAbsorption )
-    !   call RMB % SetInteractions ( IB )
-    !   end select !-- IB
+      allocate ( InteractionsExamples_BSLL_ASC_CSLD_Form :: &
+                   MW % Interactions_BSLL_ASC_CSLD )
+      select type ( IB => MW % Interactions_BSLL_ASC_CSLD )
+      class is ( InteractionsExamples_BSLL_ASC_CSLD_Form )
+      call IB % Initialize ( MS, InteractionsType = InteractionsType )
+      select case ( trim ( InteractionsType ) )
+      case ( 'MARSHAK_WAVE_VAYTET_1' )
+        call IB % Set_MWV_Spectral ( FA, SpecificOpacity = SpecificOpacity )
+      end select !-- InteractionsType
+      call RMB % SetInteractions ( IB )
+      end select !-- IB
 
-    !   end select !-- PS
-    !   end select !-- FA
-    !   end select !-- RMA
+      end select !-- PS
+      end select !-- FA
+      end select !-- RMA
 
-    ! end select !-- Integrator
+    end select !-- Integrator
 
   end subroutine InitializeInteractions
 
