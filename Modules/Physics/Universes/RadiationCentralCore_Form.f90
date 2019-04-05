36481520eb3972e69654968cf46c1e4ed34d0690
Cardall, Christian Y <cardallcy@ornl.gov>
2019-04-05 14:55:51 -0400

    InitializeMomentumSpace

--- Modules/Physics/Universes/RadiationCentralCore_Form.f90
+++ Modules/Physics/Universes/RadiationCentralCore_Form.f90
@@ -21,17 +21,21 @@ module RadiationCentralCore_Form
       AllocateIntegrator_RCC
     generic, public :: &
       AllocateIntegrator => AllocateIntegrator_RCC
+    procedure, public, pass :: &
+      InitializeMomentumSpace
   end type RadiationCentralCoreForm
 
 contains
 
 
   subroutine Initialize_RCC &
-               ( RCC, RadiationName, RadiationType, MomentsType, GeometryType, &
-                 Name, FinishTimeOption, CourantFactorOption, &
+               ( RCC, RadiationName, RadiationType, MomentsType, FluidType, &
+                 GeometryType, Name, FinishTimeOption, CourantFactorOption, &
                  GravityFactorOption, LimiterParameterOption, &
-                 ShockThresholdOption, RadiusMaxOption, RadiusCoreOption, &
-                 RadialRatioOption, nCellsPolarOption, nWriteOption )
+                 ShockThresholdOption, MinEnergyOption, MaxEnergyOption, &
+                 MinWidthEnergyOption, EnergyScaleOption, RadiusMaxOption, &
+                 RadiusCoreOption, RadialRatioOption, nCellsEnergyOption, &
+                 nCellsPolarOption, nWriteOption )
 
     class ( RadiationCentralCoreForm ), intent ( inout ) :: &
       RCC
@@ -40,6 +44,7 @@ contains
       RadiationType
     character ( * ), intent ( in ) :: &
       MomentsType, &
+      FluidType, &
       GeometryType, &
       Name
     real ( KDR ), intent ( in ), optional :: &
@@ -48,10 +53,15 @@ contains
       GravityFactorOption, &
       LimiterParameterOption, &
       ShockThresholdOption, &
+      MinEnergyOption, &
+      MaxEnergyOption, &
+      MinWidthEnergyOption, &
+      EnergyScaleOption, &
       RadiusMaxOption, &
       RadiusCoreOption, &
       RadialRatioOption
     integer ( KDI ), intent ( in ), optional :: &
+      nCellsEnergyOption, &
       nCellsPolarOption, &
       nWriteOption
 
@@ -69,6 +79,15 @@ contains
              RadiusCoreOption = RadiusCoreOption, &
              RadialRatioOption = RadialRatioOption, &
              nCellsPolarOption = nCellsPolarOption )
+    call RCC % InitializeMomentumSpace &
+           ( MinEnergyOption = MinEnergyOption, &
+             MaxEnergyOption = MaxEnergyOption, &
+             MinWidthEnergyOption = MinWidthEnergyOption, &
+             EnergyScaleOption = EnergyScaleOption, &
+             nCellsEnergyOption = nCellsEnergyOption )
+    call RCC % InitializeFluid &
+           ( FluidType, LimiterParameterOption = LimiterParameterOption, &
+             ShockThresholdOption = ShockThresholdOption )
 
   end subroutine Initialize_RCC
 
@@ -139,6 +158,113 @@ contains
     end select !-- I
 
   end subroutine AllocateIntegrator_RCC
+
+
+  subroutine InitializeMomentumSpace &
+               ( RCC, EnergySpacingOption, MinEnergyOption, MaxEnergyOption, &
+                 MinWidthEnergyOption, EnergyScaleOption, nCellsEnergyOption )
+
+    class ( RadiationCentralCoreForm ), intent ( inout ) :: &
+      RCC
+    character ( * ), intent ( in ), optional :: &
+      EnergySpacingOption
+    real ( KDR ), intent ( in ), optional :: &
+      MinEnergyOption, &
+      MaxEnergyOption, &
+      MinWidthEnergyOption, &
+      EnergyScaleOption
+    integer ( KDI ), intent ( in ), optional :: &
+      nCellsEnergyOption
+
+    integer ( KDI ) :: &
+      nCellsEnergy
+    real ( KDR ) :: &
+      MinEnergy, &
+      MaxEnergy, &
+      MinWidthEnergy, &
+      EnergyScale
+    character ( LDL ) :: &
+      EnergySpacing
+
+    select type ( I => RCC % Integrator )
+    class is ( Integrator_C_1D_MS_C_PS_Form )
+
+    select type ( PS => I % PositionSpace )
+    class is ( Atlas_SC_Form )
+
+    allocate ( Bundle_SLL_ASC_CSLD_Form :: I % MomentumSpace )
+    select type ( MS => I % MomentumSpace )
+    class is ( Bundle_SLL_ASC_CSLD_Form )
+    call MS % Initialize ( PS, 'MomentumSpace' )
+    call MS % SetBoundaryConditionsFace &
+           ( [ 'REFLECTING', 'REFLECTING' ], iDimension = 1 )
+
+    nCellsEnergy = 16
+    if ( present ( nCellsEnergyOption ) ) &
+      nCellsEnergy = nCellsEnergyOption
+    call PROGRAM_HEADER % GetParameter ( nCellsEnergy, 'nCellsEnergy' )
+
+    EnergySpacing = 'GEOMETRIC'
+    if ( present ( EnergySpacingOption ) ) &
+      EnergySpacing = EnergySpacingOption
+    call PROGRAM_HEADER % GetParameter ( EnergySpacing, 'EnergySpacing' )
+
+    select case ( trim ( EnergySpacing ) )
+    case ( 'GEOMETRIC' )
+
+      MinEnergy       =    0.0_KDR
+      MaxEnergy       =  100.0_KDR
+      MinWidthEnergy  =    0.1_KDR
+      if ( present ( MinEnergyOption ) ) &
+        MinEnergy = MinEnergyOption
+      if ( present ( MaxEnergyOption ) ) &
+        MaxEnergy = MaxEnergyOption
+      if ( present ( MinWidthEnergyOption ) ) &
+        MinWidthEnergy = MinWidthEnergyOption
+      call PROGRAM_HEADER % GetParameter ( MinEnergy, 'MinEnergy' )
+      call PROGRAM_HEADER % GetParameter ( MaxEnergy, 'MaxEnergy' )
+      call PROGRAM_HEADER % GetParameter ( MinWidthEnergy, 'MinWidthEnergy' )
+
+      call MS % CreateChart &
+             ( SpacingOption = [ 'GEOMETRIC' ], &
+               CoordinateSystemOption = 'SPHERICAL', &
+               CoordinateUnitOption = RCC % Units % Coordinate_MS, &
+               MinCoordinateOption = [ MinEnergy ], &
+               MaxCoordinateOption = [ MaxEnergy ], &
+               ScaleOption = [ MinWidthEnergy ], &
+               nCellsOption = [ nCellsEnergy ], &
+               nGhostLayersOption = [ 0, 0, 0 ] )
+
+    case ( 'COMPACTIFIED' )
+
+      EnergyScale = 10.0_KDR
+      if ( present ( EnergyScaleOption ) ) &
+        EnergyScale = EnergyScaleOption
+      call PROGRAM_HEADER % GetParameter ( EnergyScale, 'EnergyScale' )
+
+      call MS % CreateChart &
+             ( SpacingOption = [ 'COMPACTIFIED' ], &
+               CoordinateSystemOption = 'SPHERICAL', &
+               CoordinateUnitOption = RCC % Units % Coordinate_MS, &
+               ScaleOption = [ EnergyScale ], &
+               nCellsOption = [ nCellsEnergy ], &
+               nGhostLayersOption = [ 0, 0, 0 ] )
+
+    case default
+
+      call Show ( 'EnergySpacing not recognized', CONSOLE % ERROR )
+      call Show ( EnergySpacing, 'EnergySpacing', CONSOLE % ERROR )
+      call Show ( 'RadiationBox_Form', 'module', CONSOLE % ERROR )
+      call Show ( 'InitializeMomentumSpace', 'subroutine', CONSOLE % ERROR )
+      call PROGRAM_HEADER % Abort ( )
+
+    end select !-- Spacing
+
+    end select !-- MS
+    end select !-- PS
+    end select !-- I
+
+  end subroutine InitializeMomentumSpace
 
 
 end module RadiationCentralCore_Form
