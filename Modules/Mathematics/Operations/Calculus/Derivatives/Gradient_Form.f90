814b3e09b373cfeb70a3c960fe2aea593ce00cfb
Cardall, Christian Y <cardallcy@ornl.gov>
2019-04-16 09:40:15 -0400

    Revert "Towards applying limiter to all variables if one requires it, tinkering with a SlopeFactor."
    This reverts commit 313f0268c9c69b9f967b9690a6dd340e6b5ec8f8.

--- Modules/Mathematics/Operations/Calculus/Derivatives/Gradient_Form.f90
+++ Modules/Mathematics/Operations/Calculus/Derivatives/Gradient_Form.f90
@@ -32,7 +32,6 @@ module Gradient_Form
   end type GradientForm
 
     private :: &
-      ComputeSlopeFactorKernel, &
       ComputeChart_SL_Kernel
 
 contains
@@ -88,12 +87,10 @@ contains
     real ( KDR ), dimension ( :, :, : ), pointer :: &
       dV_I, &
       dX_I, &
-      SF, &
       dVdX, &
       UL_Option
     type ( StorageForm ), allocatable :: &
-      Coordinate, &
-      SlopeFactor
+      Coordinate
     class ( GeometryFlatForm ), pointer :: &
       Gmtry
 
@@ -118,34 +115,13 @@ contains
 
     call VD % Compute ( CSL, Input, iDimension )
 
-    !-- Slope factor
-
-    if ( present ( LimiterParameterOption ) ) then
-
-      allocate ( SlopeFactor )
-      call SlopeFactor % Initialize &
-             ( [ Input % nValues, Input % nVariables ] )
-
-      do iV = 1, Input % nVariables
-        call CSL % SetVariablePointer &
-               ( VD % OutputInner % Value ( :, iV ), dV_I )
-        call CSL % SetVariablePointer &
-               ( SlopeFactor % Value ( :, iV ), SF )
-        call ComputeSlopeFactorKernel &
-               ( Input % Variable ( Input % iaSelected ( iV ) ), dV_I, &
-                 LimiterParameterOption, iDimension, &
-                 CSL % nGhostLayers ( iDimension ), SF )
-      end do !-- iV
-
-    end if !-- Limiter
-
     !-- Compute gradient
 
     call CSL % SetVariablePointer ( CD % OutputInner % Value ( :, 1 ), dX_I )
 
     do iV = 1, Input % nVariables
       call CSL % SetVariablePointer &
-             ( VD % OutputInner % Value ( :, iV ), dV_I )
+             ( VD % OutputInner %  Value ( :, iV ), dV_I )
       call CSL % SetVariablePointer &
              ( G % Output % Value ( :, iV ), dVdX )
       if ( present ( UseLimiterOption ) ) then
@@ -184,70 +160,6 @@ contains
     call Show ( trim ( G % Name ), 'Name', G % IGNORABILITY )
 
   end subroutine Finalize
-
-
-  subroutine ComputeSlopeFactorKernel ( Variable, dV_I, Theta, iD, oV, SF )
-
-    character ( * ), intent ( in ) :: &
-      Variable
-    real ( KDR ), dimension ( :, :, : ), intent ( in ) :: &
-      dV_I
-    real ( KDR ), intent ( in ) :: &
-      Theta
-    integer ( KDI ), intent ( in ) :: &
-      iD, &
-      oV
-    real ( KDR ), dimension ( :, :, : ), intent ( out ) :: &
-      SF
-
-    integer ( KDI ) :: &
-      iV, jV, kV
-    integer ( KDI ), dimension ( 3 ) :: &
-      iaS, &
-      iaVS, &
-      lV, uV
-    real ( KDR ) :: &
-      dV_L, dV_R
-    
-    lV = 1
-    where ( shape ( dV_I ) > 1 )
-      lV = oV + 1
-    end where
-    lV ( iD ) = oV
-    
-    uV = 1
-    where ( shape ( dV_I ) > 1 )
-      uV = shape ( dV_I ) - oV
-    end where
-    uV ( iD ) = size ( dV_I, dim = iD ) - oV + 1 
-      
-    iaS = 0
-    iaS ( iD ) = +1
-
-    !$OMP parallel do private ( iV, jV, kV, iaVS, dV_L, dV_R )
-    do kV = lV ( 3 ), uV ( 3 ) 
-      do jV = lV ( 2 ), uV ( 2 )
-        do iV = lV ( 1 ), uV ( 1 )
-
-          iaVS = [ iV, jV, kV ] + iaS
-
-          dV_L = dV_I ( iV, jV, kV )
-          dV_R = dV_I ( iaVS ( 1 ), iaVS ( 2 ), iaVS ( 3 ) )
-              
-          SF ( iV, jV, kV ) = sign ( 0.5_KDR, dV_L ) + sign ( 0.5_KDR, dV_R )
-
-          if ( abs ( SF ( iV, jV, kV ) ) < 1.0_KDR ) then
-            call Show ( Variable, '>>> Variable' )
-            call Show ( iV, '>>> iV' )
-            call Show ( SF ( iV, jV, kV ), '>>> SF' )
-          end if
-
-        end do !-- iV
-      end do !-- jV
-    end do !-- kV
-    !$OMP end parallel do
-
-  end subroutine ComputeSlopeFactorKernel
 
 
   subroutine ComputeChart_SL_Kernel &
