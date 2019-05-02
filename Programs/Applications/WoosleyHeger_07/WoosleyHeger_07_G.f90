f227b22e80dc2b516771464de2026bce039d336e
Cardall, Christian Y <cardallcy@ornl.gov>
2019-05-02 15:08:40 -0400

    Will use the same WoosleyHeger_07_RM__Form for both grey and spectral WoosleyHeger problems.

--- Programs/Applications/WoosleyHeger_07/WoosleyHeger_07_G.f90
+++ Programs/Applications/WoosleyHeger_07/WoosleyHeger_07_G.f90
@@ -3,11 +3,11 @@ program WoosleyHeger_07_G
   !-- WoosleyHeger_07_Grey
 
   use GenASiS
-  use WoosleyHeger_07_G__Form
+  use WoosleyHeger_07_RM__Form
 
   implicit none
 
-  type ( WoosleyHeger_07_G_Form ), allocatable :: &
+  type ( WoosleyHeger_07_RM_Form ), allocatable :: &
     WH
 
   allocate ( PROGRAM_HEADER )
@@ -15,7 +15,7 @@ program WoosleyHeger_07_G
          ( 'WoosleyHeger_07_G', DimensionalityOption = '1D' )
 
   allocate ( WH )
-  call WH % Initialize ( PROGRAM_HEADER % Name )
+  call WH % Initialize ( 'GREY', PROGRAM_HEADER % Name )
   call WH % Evolve ( )
   deallocate ( WH )
 
---
+++ Programs/Applications/WoosleyHeger_07/WoosleyHeger_07_RM__Form.f90
@@ -0,0 +1,73 @@
+module WoosleyHeger_07_RM__Form
+
+  !-- WoosleyHeger_07_RadiationMoments_Form
+
+  use GenASiS
+  use WoosleyHeger_07__Template
+
+  implicit none
+  private
+
+  type, public, extends ( WoosleyHeger_07_Template ) :: WoosleyHeger_07_RM_Form
+  contains
+    procedure, private, pass :: &
+      Initialize_WH
+    generic, public :: &
+      Initialize => Initialize_WH
+  end type WoosleyHeger_07_RM_Form
+
+    private :: &
+      InitializeRadiationCentralCore
+
+contains
+
+
+  subroutine Initialize_WH ( WH, MomentsType, Name )
+
+    class ( WoosleyHeger_07_RM_Form ), intent ( inout ), target :: &
+      WH
+    character ( * ), intent ( in )  :: &
+      MomentsType, &
+      Name
+
+    if ( WH % Type == '' ) &
+      WH % Type = 'a WoosleyHeger_07_RM'
+
+    call InitializeRadiationCentralCore ( WH, MomentsType, Name )
+    call WH % SetFluid ( )
+
+  end subroutine Initialize_WH
+
+
+  subroutine InitializeRadiationCentralCore ( WH, MomentsType, Name )
+
+    class ( WoosleyHeger_07_RM_Form ), intent ( inout ), target :: &
+      WH
+    character ( * ), intent ( in ) :: &
+      MomentsType, &
+      Name
+
+    character ( LDL ) :: &
+      GeometryType
+    character ( LDL ), dimension ( : ), allocatable :: &
+      RadiationName, &
+      RadiationType
+
+    GeometryType = 'NEWTONIAN'
+    call PROGRAM_HEADER % GetParameter ( GeometryType, 'GeometryType' )
+
+    allocate ( RadiationName ( 2 ) )
+    allocate ( RadiationType ( 2 ) )
+    RadiationName  =  [ 'Nu_E   ', 'NuBar_E' ]
+    RadiationType  =  [ 'NEUTRINOS_E    ', 'NEUTRINOS_E_BAR' ]
+
+    call WH % Initialize &
+           ( RadiationName = RadiationName, RadiationType = RadiationType, &
+             MomentsType = MomentsType, FluidType = 'HEAVY_NUCLEUS', &
+             GeometryType = GeometryType, Name = Name, &
+             ShockThresholdOption = 1.0_KDR, nWriteOption = 30 )
+
+  end subroutine InitializeRadiationCentralCore
+
+
+end module WoosleyHeger_07_RM__Form
