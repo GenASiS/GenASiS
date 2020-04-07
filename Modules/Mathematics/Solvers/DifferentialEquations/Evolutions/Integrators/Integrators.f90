2f4cf4b040e5b1dd7fe3ac90e6151a4c70bd2e0e
Cardall, Christian Y <cardallcy@ornl.gov>
2020-04-07 12:24:55 -0400

    Removing obsolete Integrator classes.

--- Modules/Mathematics/Solvers/DifferentialEquations/Evolutions/Integrators/Integrators.f90
+++ Modules/Mathematics/Solvers/DifferentialEquations/Evolutions/Integrators/Integrators.f90
@@ -3,9 +3,6 @@ module Integrators
   use Integrator_Template
   use TimeSeries_C__Form
   use Integrator_C_PS__Form
-  use Integrator_C_PS_C_PS__Template
-  use Integrator_C_MS_C_PS__Template
-  use Integrator_C_1D_PS__Template
   use TimeSeries_C_1D_C__Form
   use Integrator_C_1D_C_PS__Template
   use Integrator_C_1D_PS_C_PS__Form
