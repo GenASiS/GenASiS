0a2af4c307624d18e7aedc0eb77de3f6ed7b09a7
Cardall, Christian Y <cardallcy@ornl.gov>
2020-04-17 20:02:31 -0400

    Adding file missing from previous commit, whose comment should have included work on ComputeMoment kernels.

---
+++ Modules/Mathematics/Solvers/DifferentialEquations/Constraints/PoissonEquations/LaplacianMultipole_ASC__Kernel.f90
@@ -0,0 +1,49 @@
+#include "Preprocessor"
+
+submodule ( LaplacianMultipole_ASC__Form ) LaplacianMultipole_ASC__Kernel
+
+  use Basics
+  use LaplacianMultipole_Template
+
+  implicit none
+
+contains
+
+
+  module procedure ComputeMomentsLocal_CSL_Kernel
+
+    integer ( KDI ) :: &
+      iC, &  !-- iCell
+      iR     !-- iRadius
+    real ( KDR ) :: &
+      R  !-- Radius
+
+    !$OMP  parallel do &
+    !$OMP& schedule ( OMP_SCHEDULE_HOST ) &
+    !$OMP& private ( iC, iR, R, SH_RC, SH_IC, SH_RS, SH_IS ) &
+    !$OMP& reduction ( + : MyM_RC ) &
+    !$OMP& reduction ( + : MyM_IC ) &
+    !$OMP& reduction ( + : MyM_RS ) &
+    !$OMP& reduction ( + : MyM_IS ) 
+    do iC = 1, nCells
+
+      if ( .not. IsProperCell ( iC ) ) &
+        cycle
+
+      call ComputeSolidHarmonicsKernel &
+             ( CoordinateSystem, Center ( iC, : ), Origin, RadialEdge, &
+               MaxDegree, nDimensions, GridError, SH_RC, SH_IC, SH_RS, SH_IS, &
+               R, iR )
+
+      call ComputeMomentContributionsKernel &
+             ( MyM_RC, MyM_IC, MyM_RS, MyM_IS, SH_RC, SH_IC, SH_RS, SH_IS, &
+                 Source ( iC, : ), Volume ( iC ), iaSelectedSource, &
+                 MaxDegree, nEquations, nAngularMomentCells, iR )
+
+    end do !-- iC
+    !$OMP  end parallel do
+
+  end procedure ComputeMomentsLocal_CSL_Kernel
+
+
+end submodule LaplacianMultipole_ASC__Kernel
