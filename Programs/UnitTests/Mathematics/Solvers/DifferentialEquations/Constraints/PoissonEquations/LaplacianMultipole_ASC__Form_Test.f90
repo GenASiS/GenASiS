1fab93207d07fac68b4f408806e759532e7dfbb5
Cardall, Christian Y <cardallcy@ornl.gov>
2020-10-13 11:48:53 -0400

    Allocating Laplacian in unit test.

--- Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Constraints/PoissonEquations/LaplacianMultipole_ASC__Form_Test.f90
+++ Programs/UnitTests/Mathematics/Solvers/DifferentialEquations/Constraints/PoissonEquations/LaplacianMultipole_ASC__Form_Test.f90
@@ -2,7 +2,7 @@ module LaplacianMultipole_ASC__Form_Test__Form
 
   use Basics
   use Manifolds
-  use LaplacianMultipoleOld_1_ASC__Form
+  use LaplacianMultipole_ASC__Form
   use SetHomogeneousSphere_Command
 
   implicit none
@@ -11,6 +11,89 @@ module LaplacianMultipole_ASC__Form_Test__Form
   character ( LDF ), public, parameter :: &
     ProgramName = 'LaplacianMultipole_ASC__Form_Test'
     
+  type, public :: LaplacianMultipole_ASC__Form_Test_Form
+    type ( Atlas_SC_CC_Form ), allocatable :: &
+      Atlas
+    type ( LaplacianMultipole_ASC_Form ), allocatable :: &
+      Laplacian
+  contains
+    procedure, public, pass :: &
+      Initialize
+    final :: &
+      Finalize
+  end type LaplacianMultipole_ASC__Form_Test_Form
+
+contains
+
+
+  subroutine Initialize ( LMFT, Name )
+
+    class ( LaplacianMultipole_ASC__Form_Test_Form ) :: &
+      LMFT
+    character ( * ), intent ( in ) :: &
+      Name
+
+    integer ( KDI ) :: &
+      nEquations, &
+      MaxDegree
+    class ( GeometryFlatForm ), pointer :: &
+      G
+
+
+    !-- Atlas
+
+    allocate ( LMFT % Atlas )
+    associate ( A => LMFT % Atlas )
+    call A % Initialize ( Name, PROGRAM_HEADER % Communicator )
+    call A % CreateChart_CC ( )
+    call A % SetGeometry ( )
+
+    G => A % Geometry ( )
+
+    select type ( C => A % Chart )
+    class is ( Chart_SLD_Form )
+
+
+    !-- Laplacian
+
+    nEquations = 1
+
+    MaxDegree = 2
+    call PROGRAM_HEADER % GetParameter ( MaxDegree, 'MaxDegree' )
+
+    allocate ( LMFT % Laplacian )
+    associate ( L => LMFT % Laplacian )
+    call L % Initialize ( A, MaxDegree, nEquations )
+
+!    call Show ( L % RadialEdge, 'RadialEdge', CONSOLE % INFO_2 )
+
+
+    !-- Cleanup
+
+    end associate !-- L
+    end select !-- C
+    end associate !-- A
+
+    nullify ( G )!, Source, SolidHarmonics_RC, SolidHarmonics_IC, &
+!              SolidHarmonics_RS, SolidHarmonics_IS, Solution )
+
+
+  end subroutine Initialize
+
+
+  subroutine Finalize ( LMFT )
+
+    type ( LaplacianMultipole_ASC__Form_Test_Form ) :: &
+      LMFT
+
+   if ( allocated ( LMFT % Laplacian ) ) &
+      deallocate ( LMFT % Laplacian )
+    if ( allocated ( LMFT % Atlas ) ) &
+      deallocate ( LMFT % Atlas )
+
+  end subroutine Finalize
+
+
 end module LaplacianMultipole_ASC__Form_Test__Form
 
 
@@ -21,15 +104,15 @@ program LaplacianMultipole_ASC__Form_Test
 
   implicit none
 
-!  type ( LaplacianMultipole_ASC__Form_Test_Form ), allocatable :: &
-!    LMFT
+  type ( LaplacianMultipole_ASC__Form_Test_Form ), allocatable :: &
+    LMFT
 
   allocate ( PROGRAM_HEADER )
   call PROGRAM_HEADER % Initialize ( ProgramName )
 
-!  allocate ( LMFT )
-!  call LMFT % Initialize ( PROGRAM_HEADER % Name )
-!  deallocate ( LMFT )
+  allocate ( LMFT )
+  call LMFT % Initialize ( PROGRAM_HEADER % Name )
+  deallocate ( LMFT )
 
   deallocate ( PROGRAM_HEADER )
 
