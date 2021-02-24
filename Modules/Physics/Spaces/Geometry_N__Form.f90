2e0faae049f3563ea6d7889bad23eb38064a97ad
Cardall, Christian Y <cardallcy@ornl.gov>
2021-02-24 14:54:12 -0500

    Hacked computation and output of pressure gradient.

--- Modules/Physics/Spaces/Geometry_N__Form.f90
+++ Modules/Physics/Spaces/Geometry_N__Form.f90
@@ -12,8 +12,8 @@ module Geometry_N__Form
   private
 
     integer ( KDI ), private, parameter :: &
-      N_FIELDS_NEWTONIAN  = 4, &
-      N_VECTORS_NEWTONIAN = 1
+      N_FIELDS_NEWTONIAN  = 7, &
+      N_VECTORS_NEWTONIAN = 2
 
   type, public, extends ( Geometry_G_Form ) :: Geometry_N_Form
     integer ( KDI ) :: &
@@ -22,7 +22,8 @@ module Geometry_N__Form
     integer ( KDI ) :: &
       POTENTIAL = 0
     integer ( KDI ), dimension ( 3 ) :: &
-      POTENTIAL_GRADIENT_D = 0
+      POTENTIAL_GRADIENT_D = 0, &
+      PRESSURE_GRADIENT_D  = 0
   contains
     procedure, public, pass :: &
       InitializeAllocate_G
@@ -88,6 +89,9 @@ contains
       do iD = 1, 3
         VariableUnit ( G % POTENTIAL_GRADIENT_D ( iD ) ) &
           =  UNIT % SPEED_OF_LIGHT ** 2  /  CoordinateUnit ( iD )
+        VariableUnit ( G % PRESSURE_GRADIENT_D ( iD ) ) &
+          =  UNIT % ATOMIC_MASS_UNIT  *  UNIT % SPEED_OF_LIGHT ** 2  &
+             *  UNIT % NUMBER_DENSITY_NUCLEAR /  CoordinateUnit ( iD )
       end do
     else
       !-- Dimensionless
@@ -110,16 +114,19 @@ contains
     class ( StorageForm ), intent ( inout ) :: &
       Output
 
-    type ( Integer_1D_Form ), dimension ( 1 ) :: &
+    type ( Integer_1D_Form ), dimension ( 2 ) :: &
       VectorIndices
 
     call VectorIndices ( 1 ) % Initialize ( G % POTENTIAL_GRADIENT_D )
+    call VectorIndices ( 2 ) % Initialize ( G % PRESSURE_GRADIENT_D )
     call Output % Initialize &
            ( G, iaSelectedOption &
                   = [ G % CENTER_U, G % METRIC_DD_22, G % METRIC_DD_33, &
                       G % POTENTIAL, &
-                      G % POTENTIAL_GRADIENT_D ], &
-             VectorOption = [ 'PotentialGradient' ], &
+                      G % POTENTIAL_GRADIENT_D, &
+                      G % PRESSURE_GRADIENT_D ], &
+             VectorOption = [ 'PotentialGradient_D', &
+                              'PressureGradient_D ' ], &
              VectorIndicesOption = VectorIndices )
 
   end subroutine SetOutput
@@ -177,8 +184,9 @@ contains
     if ( G % N_FIELDS == 0 ) &
       G % N_FIELDS = oF + G % N_FIELDS_NEWTONIAN
 
-    G % POTENTIAL = oF + 1
-    G % POTENTIAL_GRADIENT_D   = oF + [ 2, 3, 4 ]
+    G % POTENTIAL             =  oF + 1
+    G % POTENTIAL_GRADIENT_D  =  oF + [ 2, 3, 4 ]
+    G % PRESSURE_GRADIENT_D   =  oF + [ 5, 6, 7 ]
 
     !-- variable names
 
@@ -194,7 +202,10 @@ contains
       = [ 'Potential            ', &
           'PotentialGradient_D_1', &
           'PotentialGradient_D_2', &
-          'PotentialGradient_D_3' ]
+          'PotentialGradient_D_3', &
+          'PressureGradient_D_1 ', &
+          'PressureGradient_D_2 ', &
+          'PressureGradient_D_3 ' ]
 
     !-- units
     
@@ -220,7 +231,8 @@ contains
     end if
 
     Vector ( oV + 1 : oV + G % N_VECTORS_NEWTONIAN ) &
-      = [ 'PotentialGradient_D' ]
+      = [ 'PotentialGradient_D', &
+          'PressureGradient_D ' ]
 
     !-- vector indices
 
@@ -236,6 +248,8 @@ contains
 
     call VectorIndices ( oV + 1 ) % Initialize &
            ( G % POTENTIAL_GRADIENT_D )
+    call VectorIndices ( oV + 2 ) % Initialize &
+           ( G % PRESSURE_GRADIENT_D )
 
   end subroutine InitializeBasics
 
