aa2b0fe6fdc784f615ef74cca6b0a6e37e1888d7
Cardall, Christian Y <cardallcy@ornl.gov>
2021-02-25 15:18:14 -0500

    Starting on ChartBasics

---
+++ Modules/Mathematics_2/Manifolds/Atlases/Charts/ChartBasics/ChartHeader_Form.f90
@@ -0,0 +1,12 @@
+!-- ChartHeader_Form contains the most basic functionality characterizing a
+!   coordinate chart.
+
+module ChartHeader_Form
+
+  use Basics
+  use AtlasBasics
+
+  implicit none
+  private
+
+end module ChartHeader_Form
