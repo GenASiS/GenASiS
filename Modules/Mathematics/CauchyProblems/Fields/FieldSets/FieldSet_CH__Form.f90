24f2f77d30597e5fc34496a250684221a44f2225
Cardall, Christian Y <cardallcy@ornl.gov>
2021-04-12 12:12:30 -0400

    Draft FieldSet_CH__Form and _Test.

--- Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSet_CH__Form.f90
+++ Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSet_CH__Form.f90
@@ -9,31 +9,29 @@ module FieldSet_CH__Form
   private
 
   type, public :: FieldSet_CH_Form
-!     integer ( KDI ) :: &
-!       IGNORABILITY = 0, &
-!       nFields      = 0, &
-!       nVectors     = 0
-!     type ( Integer_1D_Form ), dimension ( : ), allocatable :: &
-!       VectorIndices
-!     type ( MeasuredValueForm ), dimension ( : ), allocatable :: &
-!       Unit
-!     character ( LDL ) :: &
-!       Name = '', &
-!       Type = ''
-!     character ( LDL ), dimension ( : ), allocatable :: &
-!       Field, &
-!       Vector
-!     class ( Chart_H_Form ), pointer :: &
-!       Chart => null ( )
-!     class ( FieldSet_MH_Form ), pointer :: &
-!       FieldSet_M => null ( )
+    integer ( KDI ) :: &
+      IGNORABILITY = 0, &
+      nFields      = 0, &
+      nVectors     = 0
+    type ( Integer_1D_Form ), dimension ( : ), allocatable :: &
+      VectorIndices
+    type ( MeasuredValueForm ), dimension ( : ), allocatable :: &
+      Unit
+    character ( LDL ) :: &
+      Type = '', &
+      Name
+    character ( LDL ), dimension ( : ), allocatable :: &
+      Field, &
+      Vector
+    class ( Chart_H_Form ), pointer :: &
+      Chart => null ( )
   contains
     procedure, private, pass :: &
       InitializeAllocate
     generic, public :: &
       Initialize => InitializeAllocate
-!     procedure, public, pass :: &
-!       Show => Show_FSC
+    procedure, public, pass :: &
+      Show => Show_FSC
     final :: &
       Finalize
   end type FieldSet_CH_Form
@@ -48,8 +46,8 @@ contains
 
 
   subroutine InitializeAllocate &
-               ( FSC, C, nFields, FieldOption, VectorOption, UnitOption, &
-                 VectorIndicesOption )
+               ( FSC, C, nFields, FieldOption, VectorOption, NameOption, &
+                 UnitOption, VectorIndicesOption )
 
     class ( FieldSet_CH_Form ), intent ( inout ) :: &
       FSC
@@ -60,112 +58,112 @@ contains
     character ( * ), dimension ( : ), intent ( in ), optional :: &
       FieldOption, &
       VectorOption
+    character ( * ), intent ( in ), optional :: &
+      NameOption
     type ( MeasuredValueForm ), dimension ( : ), intent ( in ), optional :: &
       UnitOption
     type ( Integer_1D_Form ), dimension ( : ), intent ( in ), optional ::&
       VectorIndicesOption
 
-!     integer ( KDI ) :: &
-!       iF, &  !-- iField
-!       iV     !-- iVector
-!     character ( 2 ) :: &
-!       FieldNumber, &
-!       VectorNumber
+    integer ( KDI ) :: &
+      iF, &  !-- iField
+      iV     !-- iVector
+    character ( 2 ) :: &
+      FieldNumber, &
+      VectorNumber
 
-!     FSC % IGNORABILITY  =  C % IGNORABILITY
+    FSC % IGNORABILITY  =  C % IGNORABILITY
 
-!     if ( FSC % Type  ==  '' ) &
-!       FSC % Type  =  'a FieldSet_C' 
+    if ( FSC % Type  ==  '' ) &
+      FSC % Type  =  'a FieldSet_C' 
     
-!     FSC % Name  =  FSM % Name
+    FSC % Name  =  'FieldSet'
+    if ( present ( NameOption ) ) &
+      FSC % Name  =  NameOption
 
-!     call Show ( 'Initializing ' // trim ( FSC % Type ), FSC % IGNORABILITY )
-!     call Show ( FSC % Name, 'Name', FSC % IGNORABILITY )
+    call Show ( 'Initializing ' // trim ( FSC % Type ), FSC % IGNORABILITY )
+    call Show ( FSC % Name, 'Name', FSC % IGNORABILITY )
    
-!     FSC % Chart       =>    C
-!     FSC % FieldSet_M  =>  FSM 
+    FSC % Chart  =>  C
 
-!     associate ( nF  =>  FSC % nFields )
-!     nF  =  nFields
-!     allocate ( FSC % Field ( nF ) )
-!     allocate ( FSC % Unit ( nF ) )
-!     if ( present ( FieldOption ) ) then
-!       FSC % Field  =  FieldOption
-!     else
-!       do iF  =  1, nF
-!         write ( FieldNumber, fmt = '(i2.2)' ) iF
-!         FSC % Field ( iF )  =  'Field_' // FieldNumber
-!       end do  !-- iF
-!     end if  !-- FieldOption
-!     if ( present ( UnitOption ) ) &
-!       FSC % Unit  =  UnitOption
-!     end associate  !-- nF
+    associate ( nF  =>  FSC % nFields )
+    nF  =  nFields
+    allocate ( FSC % Field ( nF ) )
+    allocate ( FSC % Unit ( nF ) )
+    if ( present ( FieldOption ) ) then
+      FSC % Field  =  FieldOption
+    else
+      do iF  =  1, nF
+        write ( FieldNumber, fmt = '(i2.2)' ) iF
+        FSC % Field ( iF )  =  'Field_' // FieldNumber
+      end do  !-- iF
+    end if  !-- FieldOption
+    if ( present ( UnitOption ) ) &
+      FSC % Unit  =  UnitOption
+    end associate  !-- nF
 
-!     if ( present ( VectorIndicesOption ) ) then
+    if ( present ( VectorIndicesOption ) ) then
 
-!       associate ( nV  =>  FSC % nVectors )
-!       nV  =  size ( VectorIndicesOption )
+      associate ( nV  =>  FSC % nVectors )
+      nV  =  size ( VectorIndicesOption )
 
-!       allocate ( FSC % VectorIndices ( nV ) )
-!       do iV  =  1, nV
-!         call FSC % VectorIndices ( iV ) % Initialize &
-!                ( VectorIndicesOption ( iV ) )
-!       end do  !-- iV
+      allocate ( FSC % VectorIndices ( nV ) )
+      do iV  =  1, nV
+        call FSC % VectorIndices ( iV ) % Initialize &
+               ( VectorIndicesOption ( iV ) )
+      end do  !-- iV
 
-!       allocate ( FSC % Vector ( nV ) )
-!       if ( present ( VectorOption ) ) then
-!         FSC % Vector  =  VectorOption
-!       else
-!         do iV  =  1, nV
-!           write ( VectorNumber, fmt = '(i2.2)' ) iV
-!           FSC % Vector ( iV )  =  'Vector_' // VectorNumber
-!         end do  !-- iV
-!       end if  !-- VectorOption 
-!       end associate  !-- nV
+      allocate ( FSC % Vector ( nV ) )
+      if ( present ( VectorOption ) ) then
+        FSC % Vector  =  VectorOption
+      else
+        do iV  =  1, nV
+          write ( VectorNumber, fmt = '(i2.2)' ) iV
+          FSC % Vector ( iV )  =  'Vector_' // VectorNumber
+        end do  !-- iV
+      end if  !-- VectorOption 
+      end associate  !-- nV
 
-!     end if  !-- VectorIndicesOption
+    end if  !-- VectorIndicesOption
 
   end subroutine InitializeAllocate
 
 
-!   subroutine Show_FSC ( FSC )
+  subroutine Show_FSC ( FSC )
 
-!     class ( FieldSet_CH_Form ), intent ( in ) :: &
-!       FSC
+    class ( FieldSet_CH_Form ), intent ( in ) :: &
+      FSC
 
-!     integer ( KDI ) :: &
-!       iF, &  !-- iField
-!       iV     !-- iVector
-!     character ( LDL ), dimension ( : ), allocatable :: &
-!       TypeWord
+    integer ( KDI ) :: &
+      iF, &  !-- iField
+      iV     !-- iVector
+    character ( LDL ), dimension ( : ), allocatable :: &
+      TypeWord
 
-!     call Split ( FSC % Type, ' ', TypeWord )
-!     call Show ( trim ( TypeWord ( 2 ) ) // ' Parameters', FSC % IGNORABILITY )
+    call Split ( FSC % Type, ' ', TypeWord )
+    call Show ( trim ( TypeWord ( 2 ) ) // ' Parameters', FSC % IGNORABILITY )
 
-!     associate &
-!       ( M  =>  FSC % Chart % Manifold, &
-!         C  =>  FSC % Chart )
-!     call Show ( FSC % Name, 'Name',     FSC % IGNORABILITY )
-!     call Show (   M % Name, 'Manifold', FSC % IGNORABILITY )
-!     call Show (   C % Name, 'Chart',    FSC % IGNORABILITY )
-!     end associate  !-- M, etc.
+    associate ( C  =>  FSC % Chart )
+    call Show ( FSC % Name, 'Name',  FSC % IGNORABILITY )
+    call Show (   C % Name, 'Chart', FSC % IGNORABILITY )
+    end associate  !-- C
 
-!     call Show ( FSC % nFields, 'nFields', FSC % IGNORABILITY )
-!     do iF  =  1, FSC % nFields
-!       call Show ( iF,                 'iField', FSC % IGNORABILITY ) 
-!       call Show ( FSC % Field ( iF ), 'Field',  FSC % IGNORABILITY )
-!       call Show ( FSC % Unit ( iF ),  'Unit',   FSC % IGNORABILITY )
-!     end do !-- iF
+    call Show ( FSC % nFields, 'nFields', FSC % IGNORABILITY )
+    do iF  =  1, FSC % nFields
+      call Show ( iF,                 'iField', FSC % IGNORABILITY ) 
+      call Show ( FSC % Field ( iF ), 'Field',  FSC % IGNORABILITY )
+      call Show ( FSC % Unit ( iF ),  'Unit',   FSC % IGNORABILITY )
+    end do !-- iF
     
-!     call Show ( FSC % nVectors, 'nVectors', FSC % IGNORABILITY )
-!     do iV  =  1, FSC % nVectors
-!       call Show ( FSC % Vector ( iV ),                 'Vector', &
-!                   FSC % IGNORABILITY )
-!       call Show ( FSC % VectorIndices ( iV ) % Value, 'VectorIndices', &
-!                   FSC % IGNORABILITY )
-!     end do  !-- iV
+    call Show ( FSC % nVectors, 'nVectors', FSC % IGNORABILITY )
+    do iV  =  1, FSC % nVectors
+      call Show ( FSC % Vector ( iV ),                 'Vector', &
+                  FSC % IGNORABILITY )
+      call Show ( FSC % VectorIndices ( iV ) % Value, 'VectorIndices', &
+                  FSC % IGNORABILITY )
+    end do  !-- iV
 
-!   end subroutine Show_FSC
+  end subroutine Show_FSC
 
 
   impure elemental subroutine Finalize ( FSC )
@@ -173,20 +171,19 @@ contains
     type ( FieldSet_CH_Form ), intent ( inout ) :: &
       FSC
 
-!     nullify ( FSC % FieldSet_M )
-!     nullify ( FSC % Chart )
+    nullify ( FSC % Chart )
 
-!     if ( allocated ( FSC % Vector ) ) &
-!       deallocate ( FSC % Vector )
-!     if ( allocated ( FSC % Field ) ) &
-!       deallocate ( FSC % Field )
-!     if ( allocated ( FSC % Unit ) ) &
-!       deallocate ( FSC % Unit )
-!     if ( allocated ( FSC % VectorIndices ) ) &
-!       deallocate ( FSC % VectorIndices )
+    if ( allocated ( FSC % Vector ) ) &
+      deallocate ( FSC % Vector )
+    if ( allocated ( FSC % Field ) ) &
+      deallocate ( FSC % Field )
+    if ( allocated ( FSC % Unit ) ) &
+      deallocate ( FSC % Unit )
+    if ( allocated ( FSC % VectorIndices ) ) &
+      deallocate ( FSC % VectorIndices )
 
-!     call Show ( 'Finalizing ' // trim ( FSC % Type ), FSC % IGNORABILITY )
-!     call Show ( FSC % Name, 'Name', FSC % IGNORABILITY )
+    call Show ( 'Finalizing ' // trim ( FSC % Type ), FSC % IGNORABILITY )
+    call Show ( FSC % Name, 'Name', FSC % IGNORABILITY )
    
   end subroutine Finalize
 
---
+++ Programs/UnitTests/Mathematics/CauchyProblems/Fields/FieldSets/FieldSet_CH__Form_Test.f90
@@ -0,0 +1,48 @@
+program FieldSet_CH__Form_Test
+
+  !-- FieldSet_ChartHeader__Form_Test
+
+  use Basics
+  use Manifolds
+  use FieldSets
+
+  implicit none
+
+  integer ( KDI ) :: &
+    nFields
+  type ( Integer_1D_Form ), dimension ( 1 ) :: &
+    VectorIndices
+  type ( MeasuredValueForm ), dimension ( 5 ) :: &
+    FieldUnit
+  type ( Chart_H_Form ), allocatable :: &
+    C
+  type ( FieldSet_CH_Form ), allocatable :: &
+    FSC
+
+  allocate ( PROGRAM_HEADER )
+  call PROGRAM_HEADER % Initialize &
+         ( 'FieldSet_CH__Form_Test', DimensionalityOption = '2D' )
+
+  allocate ( C )
+  call C % Initialize ( Periodic = [ .true., .true., .true. ] )
+
+  nFields  =  5
+
+  FieldUnit ( 1 )      =  UNIT % MASS_DENSITY_MKS
+  FieldUnit ( 2 : 4 )  =  UNIT % SPEED_MKS
+  FieldUnit ( 5 )      =  UNIT % JOULE
+
+  call VectorIndices ( 1 ) % Initialize ( [ 2, 3, 4 ] )
+
+  allocate ( FSC )
+  call FSC % Initialize &
+         ( C, nFields, UnitOption = FieldUnit, &
+           VectorIndicesOption = VectorIndices )
+
+  call   C % Show ( )
+  call FSC % Show ( )
+
+  deallocate ( C )
+  deallocate ( PROGRAM_HEADER )
+
+end program FieldSet_CH__Form_Test
