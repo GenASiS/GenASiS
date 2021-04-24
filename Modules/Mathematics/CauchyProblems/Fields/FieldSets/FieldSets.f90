e760c45047e80b9a34e03bdbdd08548a08da66f6
Cardall, Christian Y <cardallcy@ornl.gov>
2021-04-24 19:43:49 -0400

    Removing obsolete FieldSet classes.

--- Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSets.f90
+++ Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSets.f90
@@ -3,10 +3,6 @@ module FieldSets
   use Storage_FSC__Form        !-- Storage_FieldSetChart_Form
   use GhostExchange_FSC__Form  !-- GhostExchange_FieldSetChart_Form
   use FieldSet_C__Form         !-- FieldSet_Chart_Form
-  use FieldSet_CH__Form        !-- FieldSet_ChartHeader_Form
-  use FieldSet_CGS__Form       !-- FieldSet_ChartGridStructured
   use FieldSet_A__Form         !-- FieldSet_Atlas_Form
-  use FieldSet_AH__Form
-  use FieldSet_ASG__Form
   
 end module FieldSets
