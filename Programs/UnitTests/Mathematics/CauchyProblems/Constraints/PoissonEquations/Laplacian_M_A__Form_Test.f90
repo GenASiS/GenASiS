e1ee4aaf3ccc5ca51bd4bf52dac1e7c6b967109e
Cardall, Christian Y <cardallcy@ornl.gov>
2021-06-03 10:09:13 -0400

    Completing draft of Laplacian_M_A__Form_Test, testing moments computation for HomogeneousSphere.

--- Programs/UnitTests/Mathematics/CauchyProblems/Constraints/PoissonEquations/Laplacian_M_A__Form_Test.f90
+++ Programs/UnitTests/Mathematics/CauchyProblems/Constraints/PoissonEquations/Laplacian_M_A__Form_Test.f90
@@ -54,12 +54,12 @@ program Laplacian_M_A__Form_Test
   call LA % Initialize ( GA, MaxDegree, nEquations )
 
   call  A % Show ( )
-  call SA % Show ( )
   call GA % Show ( )
   call LA % Show ( )
 
   call TestAssociatedLegendre ( )
   call TestAngularFunctions ( )
+  call TestHomogeneousSphere ( )
 
   deallocate ( LA )
   deallocate ( GA )
@@ -191,7 +191,7 @@ contains
       end if
       end associate !-- SC
 
-      call GIS_AA % Open ( GIS % ACCESS_CREATE )
+      call GIS_AA % Open ( GIS_AA % ACCESS_CREATE )
       call SAA % Write ( )
       call GIS_AA % Close ( )
 
@@ -207,6 +207,168 @@ contains
     end associate !-- C
 
   end subroutine TestAngularFunctions
+
+
+  subroutine TestHomogeneousSphere ( )
+
+    real ( KDR ) :: &
+      RadiusDensity, &
+      Density
+    character ( LDL ), dimension ( 1 ) :: &
+      Field
+    type ( FieldSet_A_Form ), allocatable :: &
+      Source_A, &
+      Reference_A
+
+    call Show ( 'Testing homogeneous sphere' )
+
+    associate ( C  =>  A % Chart_GS )
+
+    Field  =  [ 'HomogeneousSphere' ]
+
+    allocate ( Source_A )
+    call Source_A % Initialize &
+           ( A, &
+             FieldOption = Field, &
+             NameOption = 'Source', &
+             DeviceMemoryOption = LA % DeviceMemory, &
+             nFieldsOption = nEquations )
+    
+    allocate ( Reference_A )
+    call Reference_A % Initialize &
+           ( A, &
+             FieldOption = Field, &
+             NameOption = 'Reference', &
+             nFieldsOption = nEquations )
+    
+    call SA % AddFieldSet ( Source_A )
+    call SA % AddFieldSet ( Reference_A )
+    call SA % Show ( )
+
+    RadiusDensity = C % MaxCoordinate ( 1 ) / 10.0_KDR
+    call PROGRAM_HEADER % GetParameter ( RadiusDensity, 'RadiusDensity' )
+
+    Density = 1.0_KDR
+    call PROGRAM_HEADER % GetParameter ( Density, 'Density' )
+
+    call SetHomogeneousSphere &
+           ( Source_A, Reference_A, GA, Density, RadiusDensity, iField = 1 )
+
+    call Source_A % UpdateDevice ( )
+
+    call LA % ComputeMoments ( Source_A )
+    call LA % ShowMoments ( )
+
+    call GIS % Open ( GIS % ACCESS_CREATE )
+    call SA % Write ( )
+    call GIS % Close ( )
+
+    end associate !-- C
+
+  end subroutine TestHomogeneousSphere
+
+
+  subroutine SetHomogeneousSphere &
+               ( Source_A, Reference_A, Geometry_A, &
+                 Density, RadiusDensity, iField )
+
+    class ( FieldSet_A_Form ), intent ( inout ) :: &
+      Source_A, &
+      Reference_A
+    class ( Geometry_F_A_Form ), intent ( in ) :: &
+      Geometry_A
+    real ( KDR ), intent ( in ) :: &
+      Density, &
+      RadiusDensity
+    integer ( KDI ), intent ( in ) :: &
+      iField
+
+    !-- Geometry
+
+    select type ( GC  =>  Geometry_A % FieldSet_C ( 1 ) % Element )
+      class is ( Geometry_F_C_Form )
+    associate &
+      ( GV  =>  GC % Storage_FSC % Storage % Value )
+    associate &
+      ( R_E  =>  GV ( :, GC % EDGE_I_U ( 1 ) ), &
+        R_W  =>  GV ( :, GC % WIDTH_U  ( 1 ) ), &
+        R_C  =>  GV ( :, GC % CENTER_U ( 1 ) ) )
+
+    !-- Source
+
+    associate &
+      ( SC  =>  Source_A % FieldSet_C ( 1 ) % Element )
+    associate &
+      ( SV  =>  SC % Storage_FSC % Storage % Value )
+    associate &
+      ( D  =>  SV ( :, iField ) )
+
+    call SetDensityKernel ( R_E, R_W, RadiusDensity, Density, D )
+
+    end associate !-- D
+    end associate !-- SV
+    end associate !-- SC
+
+    !-- Reference
+
+    associate &
+      ( RC  =>  Reference_A % FieldSet_C ( 1 ) % Element )
+    associate &
+      ( RV  =>  RC % Storage_FSC % Storage % Value )
+    associate &
+      ( Phi  =>  RV ( :, iField ), &
+        Pi   =>  CONSTANT % PI )
+
+    where ( R_C  <  RadiusDensity )
+      Phi  =  1.0_KDR / 6.0_KDR  *  Density  *  R_C ** 2  &
+              -  1.0_KDR / 2.0_KDR  *  Density  *  RadiusDensity ** 2
+    elsewhere
+      Phi  =  - 1.0_KDR / 3.0_KDR  *  Density  *  RadiusDensity ** 3  /  R_C
+    end where
+
+    end associate !-- Phi, etc.
+    end associate !-- RV
+    end associate !-- RC
+
+    !-- Cleanup
+
+    end associate !-- R_E, etc.
+    end associate !-- GV
+    end select !-- GC
+
+  end subroutine SetHomogeneousSphere
+
+
+  subroutine SetDensityKernel ( R_E, R_W, RD, Density, D )
+
+    real ( KDR ), dimension ( : ), intent ( in ) :: &
+      R_E, &
+      R_W
+    real ( KDR ), intent ( in ) :: &
+      RD, &
+      Density
+    real ( KDR ), dimension ( : ), intent ( out ) :: &
+      D
+
+    integer ( KDI ) :: &
+      iV
+    real ( KDR ) :: &
+      R_I, R_O
+
+    do iV  =  1, size ( R_E )
+      R_I  =  R_E ( iV )
+      R_O  =  R_E ( iV )  +  R_W ( iV )
+      if ( R_O  <=  RD ) then
+        D ( iV )  =  Density
+      else if ( R_I  <  RD .and. R_O  >  RD ) then
+        D ( iV )  =  Density * ( RD ** 3  -  R_I ** 3 ) &
+                     / ( R_O ** 3  -  R_I ** 3 )
+      else
+        D ( iV )  =  0.0_KDR
+      end if
+    end do !-- iV
+
+  end subroutine SetDensityKernel
 
 
 end program Laplacian_M_A__Form_Test
