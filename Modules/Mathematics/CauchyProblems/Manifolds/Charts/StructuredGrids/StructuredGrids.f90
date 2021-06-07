179928397ff861ab0f2451a41737f18cb3024331
Cardall, Christian Y <cardallcy@ornl.gov>
2021-06-07 14:41:59 -0400

    Connectivity_Form and _Test.

--- Modules/Mathematics/CauchyProblems/Manifolds/Charts/StructuredGrids/StructuredGrids.f90
+++ Modules/Mathematics/CauchyProblems/Manifolds/Charts/StructuredGrids/StructuredGrids.f90
@@ -1,5 +1,6 @@
 module StructuredGrids
 
+  use Connectivity_Form
   use Chart_H__Form   !-- Chart_Header_Form
   use Chart_GS__Form  !-- Chart_GridStructured_Form
   use Chart_GS_C__Form  !-- Chart_GridStructured_Central_Form
