b8718d4472cd3b04e3b6be84a027a47f18cbc1c4
Cardall, Christian Y <cardallcy@ornl.gov>
2021-06-22 08:33:40 -0400

    Starting on new FieldSets, with the intent to eliminate Chart-level classes. Begin here with GhostExchange_Form.

--- Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSets.f90
+++ Modules/Mathematics/CauchyProblems/Fields/FieldSets/FieldSets.f90
@@ -1,9 +1,8 @@
 module FieldSets
 
-  use Storage_FSC__Form        !-- Storage_FieldSetChart_Form
-  use GhostExchange_FSC__Form  !-- GhostExchange_FieldSetChart_Form
-  use Boundaries_FSC__Form     !-- Boundaries_FieldSet_Chart_Form
-  use FieldSet_C__Form         !-- FieldSet_Chart_Form
-  use FieldSet_A__Form         !-- FieldSet_Atlas_Form
+  use GhostExchange_Form
+  ! use Boundaries_FSC__Form     !-- Boundaries_FieldSet_Chart_Form
+  ! use FieldSet_C__Form         !-- FieldSet_Chart_Form
+  ! use FieldSet_A__Form         !-- FieldSet_Atlas_Form
   
 end module FieldSets
---
+++ Modules/Mathematics/CauchyProblems/Fields/FieldSetsOld_1/FieldSets.f90
@@ -0,0 +1,9 @@
+module FieldSets
+
+  use Storage_FSC__Form        !-- Storage_FieldSetChart_Form
+  use GhostExchange_FSC__Form  !-- GhostExchange_FieldSetChart_Form
+  use Boundaries_FSC__Form     !-- Boundaries_FieldSet_Chart_Form
+  use FieldSet_C__Form         !-- FieldSet_Chart_Form
+  use FieldSet_A__Form         !-- FieldSet_Atlas_Form
+  
+end module FieldSets
