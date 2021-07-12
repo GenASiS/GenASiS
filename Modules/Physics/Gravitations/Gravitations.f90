fa850eeb7a466e027ba56c78fe9d08182140af99
Cardall, Christian Y <cardallcy@ornl.gov>
2021-07-12 13:39:53 -0400

    Gravitations_G__Form and _Test.

---
+++ Modules/Physics/Gravitations/Gravitations.f90
@@ -0,0 +1,9 @@
+module Gravitations
+
+  use Gravitation_G__Form
+  ! use Gravitation_G_A__Form
+  ! use Gravitation_NH_C__Form
+  ! use Gravitation_NH_A__Form
+  ! use Gravitation_NSG_A__Form
+
+end module Gravitations
---
+++ Programs/UnitTests/Physics/Gravitations/Gravitation_G__Form_Test.f90
@@ -0,0 +1,59 @@
+program Gravitation_G__Form_Test
+
+  !-- Gravitation_Galileo__Form_Test
+
+  use Basics
+  use Mathematics
+  use Gravitations
+
+  implicit none
+
+  type ( MeasuredValueForm ), dimension ( 3 ) :: &
+    CoordinateUnit
+  type ( GridImageStreamForm ), allocatable :: &
+    GIS
+  type ( Atlas_SCG_Form ), allocatable :: &
+    A
+  type ( StreamForm ), allocatable :: &
+    S
+  type ( Gravitation_G_Form ), allocatable :: &
+    G
+
+  allocate ( PROGRAM_HEADER )
+  call PROGRAM_HEADER % Initialize &
+         ( 'Gravitation_G__Form_Test', DimensionalityOption = '2D' )
+
+  allocate ( GIS )
+  call GIS % Initialize &
+         ( PROGRAM_HEADER % Name, &
+           CommunicatorOption = PROGRAM_HEADER % Communicator )
+
+  CoordinateUnit  =  UNIT % KILOMETER
+
+  allocate ( A )
+  call A % Initialize &
+         ( CommunicatorOption = PROGRAM_HEADER % Communicator, &
+           CoordinateUnitOption = CoordinateUnit )
+
+  allocate ( S )
+  call S % Initialize ( A, GIS )
+
+  allocate ( G )
+  call G % Initialize ( A )
+  call G % SetStream ( S )
+
+  call A % Show ( )
+  call G % Show ( )
+  call S % Show ( )
+
+  call GIS % Open ( GIS % ACCESS_CREATE )
+  call S % Write ( )
+  call GIS % Close ( )
+
+  deallocate ( G )
+  deallocate ( S )
+  deallocate ( A )
+  deallocate ( GIS )
+  deallocate ( PROGRAM_HEADER )
+
+end program Gravitation_G__Form_Test
