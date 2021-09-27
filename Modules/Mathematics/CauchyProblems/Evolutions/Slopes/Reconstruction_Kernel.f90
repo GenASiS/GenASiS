d179edf335bee556e126423cb48c9bd3a8e36071
Cardall, Christian Y <cardallcy@ornl.gov>
2021-09-27 13:00:20 -0400

    Device kernels in Reconstruction_Form.

--- Modules/Mathematics/CauchyProblems/Evolutions/Slopes/Reconstruction_Kernel.f90
+++ Modules/Mathematics/CauchyProblems/Evolutions/Slopes/Reconstruction_Kernel.f90
@@ -137,6 +137,90 @@ contains
     
     if ( UseDevice ) then
     
+      !$OMP OMP_TARGET_DIRECTIVE parallel do collapse ( 4 ) &
+      !$OMP schedule ( OMP_SCHEDULE_TARGET ) &
+      !$OMP private ( iF, iF_R, iaVP, iaVM, fM, fC, fP, fI, fO ) &
+      !$OMP private ( xAM, xAC, xAP, xI, xO, c0, c1 )
+      do iS  =  1,  size ( iaSlctd )
+        do kV  =  lV ( 3 ),  uV ( 3 ) 
+          do jV  =  lV ( 2 ),  uV ( 2 )
+            do iV  =  lV ( 1 ),  uV ( 1 )
+
+              iF    =  iaSlctd   ( iS )
+              iF_R  =  iaSlctd_R ( iS )
+
+              iaVP  =  [ iV, jV, kV ]  +  iaS
+              iaVM  =  [ iV, jV, kV ]  -  iaS
+
+              fM  =  F ( iaVM ( 1 ), iaVM ( 2 ), iaVM ( 3 ), iF )
+              fC  =  F ( iV, jV, kV, iF )
+              fP  =  F ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ), iF )
+
+              xAM  =  XA ( iaVM ( 1 ), iaVM ( 2 ), iaVM ( 3 ) )
+              xAC  =  XA ( iV, jV, kV )
+              xAP  =  XA ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ) )
+
+              xI  =  X ( iV, jV, kV )  -  0.5 * dX ( iV, jV, kV )
+              xO  =  X ( iV, jV, kV )  +  0.5 * dX ( iV, jV, kV )
+
+!call Show ( iV, '>>> iV' )
+!call Show ( [ fM, fC, fP ], '>>> fM, fC, fP' )
+              !-- Local extremum of cell average values? 
+              !   Then reconstruction is constant.
+              if ( ( fC - fM ) * ( fP - fC )  <  0.0_KDR ) then
+!call Show ( '>>> Local extremum' )
+
+                c1  =  0.0_KDR
+                c0  =  fC
+
+              else  !-- Linear reconstruction
+
+                c1  =  ( fP - fM ) / ( xAP - xAM )
+                c0  =  fC  -  c1 * xAC
+
+                fI  =  c0  +  c1 * xI
+                fO  =  c0  +  c1 * xO
+
+                !-- Overshoot at inner face?
+                !   Reduce slope.
+                if ( c1 * ( fI - fM )  <  0.0_KDR ) then
+!call Show ( '>>> Overshoot inner' )
+!call Show ( [ fM, fI, fC, fO, fP ], '>>> fM, fI, fC, fO, fP' )
+
+                  c1  =  ( fC - fM ) / ( xAC - xAM )
+                  c0  =  fC  -  c1 * xAC
+
+                end if  !-- Overshoot inner
+
+                !-- Overshoot at outer face?
+                !   Reduce slope.
+                if ( c1 * ( fP - fO )  <  0.0_KDR ) then
+!call Show ( '>>> Overshoot outer' )
+!call Show ( [ fM, fI, fC, fO, fP ], '>>> fM, fI, fC, fO, fP' )
+
+                  c1  =  ( fP - fC ) / ( xAP - xAC )
+                  c0  =  fC  -  c1 * xAC
+
+                end if  !-- Overshoot outer
+
+              end if  !-- Local extremum
+
+              F_IR ( iV, jV, kV, iF_R )  &
+                =  c0  +  c1 * xI
+
+              F_IL ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ), iF_R )  &
+                =  c0  +  c1 * xO
+
+!call Show ( '>>> Final values' )
+!call Show ( [ fM, F_IR ( iV, jV, kV, iS ), fC, &
+!              F_IL ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ), iS ), fP ], &
+!            '>>> fM, fI, fC, fO, fP' )
+            end do !-- iV
+          end do !-- jV
+        end do !-- kV
+      end do !-- iS
+      !$OMP end OMP_TARGET_DIRECTIVE parallel do
+
     else !-- use host
               
       !$OMP parallel do collapse ( 4 ) &
@@ -278,7 +362,7 @@ contains
       !$OMP private ( iF, iF_R, iaVP, iaVM, fM, fC, fP, fI, fO ) &
       !$OMP private ( xAM, xAC, xAP, x2AM, x2AC, x2AP, xI, xO, xE ) &
       !$OMP private ( c0, c1, c2, c2_S, d ) &
-      !$OMP firstprivate ( SqrtTiny, iaS )
+      !$OMP firstprivate ( SqrtTiny )
       do iS  =  1,  size ( iaSlctd )
         do kV  =  lV ( 3 ),  uV ( 3 ) 
           do jV  =  lV ( 2 ),  uV ( 2 )
@@ -298,9 +382,9 @@ contains
               xAC  =  XA ( iV, jV, kV )
               xAP  =  XA ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ) )
 
-              x2AM  =  XA ( iaVM ( 1 ), iaVM ( 2 ), iaVM ( 3 ) ) ** 2
-              x2AC  =  X ( iV, jV, kV ) ** 2 
-              x2AP  =  X ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ) ) ** 2
+              x2AM  =  X2A ( iaVM ( 1 ), iaVM ( 2 ), iaVM ( 3 ) )
+              x2AC  =  X2A ( iV, jV, kV )
+              x2AP  =  X2A ( iaVP ( 1 ), iaVP ( 2 ), iaVP ( 3 ) )
 
               xI  =  X ( iV, jV, kV )  -  0.5 * dX ( iV, jV, kV )
               xO  =  X ( iV, jV, kV )  +  0.5 * dX ( iV, jV, kV )
@@ -455,7 +539,7 @@ contains
         end do !-- kV
       end do !-- iS
       !$OMP end OMP_TARGET_DIRECTIVE parallel do
-    
+      
     else !-- use host
               
       !$OMP parallel do collapse ( 4 ) &
