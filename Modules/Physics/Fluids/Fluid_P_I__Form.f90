c77b27145bab0a2c0462a157ef55d0cb1ece0e02
Cardall, Christian Y <cardallcy@ornl.gov>
2021-10-08 12:37:00 -0400

    Additional parameters in Fluid_P_I

--- Modules/Physics/Fluids/Fluid_P_I__Form.f90
+++ Modules/Physics/Fluids/Fluid_P_I__Form.f90
@@ -25,22 +25,22 @@ module Fluid_P_I__Form
       N_VECTORS_I   = N_VECTORS_I
     real ( KDR ) :: &
       BoltzmannConstant, &
-      AdiabaticIndex!, &
-    !   MeanMolecularWeight, &
-    !   SpecificHeatVolume, &  !-- per baryon
-    !   FiducialBaryonDensity, &
-    !   FiducialPressure
+      AdiabaticIndex, &
+      MeanMolecularWeight, &
+      SpecificHeatVolume, &  !-- per baryon
+      FiducialBaryonDensity, &
+      FiducialPressure
   contains
     procedure, private, pass :: &
       InitializeAllocate_F
     procedure, public, pass :: &
       SetAdiabaticIndex
-  !   procedure, public, pass :: &
-  !     SetMeanMolecularWeight
-  !   procedure, public, pass :: &
-  !     SetSpecificHeatVolume
-  !   procedure, public, pass :: &
-  !     SetFiducialParameters
+    procedure, public, pass :: &
+      SetMeanMolecularWeight
+    procedure, public, pass :: &
+      SetSpecificHeatVolume
+    procedure, public, pass :: &
+      SetFiducialParameters
   !   procedure, public, pass :: &
   !     SetOutput
     procedure, public, pass :: &
@@ -127,15 +127,32 @@ contains
 
     !-- Parameters
 
+    associate &
+      ( k     => F % BoltzmannConstant, &
+        gamma => F % AdiabaticIndex, &
+        mu    => F % MeanMolecularWeight, &
+        c_v   => F % SpecificHeatVolume, &
+        n_0   => F % FiducialBaryonDensity, &
+        p_0   => F % FiducialPressure )
+
     if ( Units_F ( 1 ) % Temperature  ==  UNIT % IDENTITY ) then
-      F % BoltzmannConstant  =  1.0_KDR
+      k  =  1.0_KDR
     else
-      F % BoltzmannConstant  =  CONSTANT % BOLTZMANN
+      k  =  CONSTANT % BOLTZMANN
     end if
 
-    F % AdiabaticIndex  =  1.4_KDR
-    call PROGRAM_HEADER % GetParameter &
-           ( F % AdiabaticIndex, 'AdiabaticIndex' )
+    gamma  =  1.4_KDR
+    mu     =  1.0_KDR
+    c_v    =  k / ( mu * ( gamma - 1.0_KDR ) )
+    n_0    =  1.0_KDR
+    p_0    =  1.0_KDR
+    call PROGRAM_HEADER % GetParameter ( gamma, 'AdiabaticIndex' )
+    call PROGRAM_HEADER % GetParameter ( mu,    'MeanMolecularWeight' )
+    call PROGRAM_HEADER % GetParameter ( c_v,   'SpecificHeatVolume' )
+    call PROGRAM_HEADER % GetParameter ( n_0,   'FiducialBaryonDensity' )
+    call PROGRAM_HEADER % GetParameter ( p_0,   'FiducialPressure' )
+
+    end associate !-- k, etc.
 
   end subroutine InitializeAllocate_F
 
@@ -156,6 +173,91 @@ contains
   end subroutine SetAdiabaticIndex
 
 
+  subroutine SetMeanMolecularWeight ( F, MeanMolecularWeight )
+
+    !-- Assumes AdiabaticIndex already set.
+
+    class ( Fluid_P_I_Form ), intent ( inout ) :: &
+      F
+    real ( KDR ), intent ( in ) :: &
+      MeanMolecularWeight
+
+    associate &
+      ( k     => F % BoltzmannConstant, &
+        gamma => F % AdiabaticIndex, &
+        mu    => F % MeanMolecularWeight, &
+        c_v   => F % SpecificHeatVolume )
+
+    mu  =  MeanMolecularWeight
+
+    c_v  =  k / ( mu * ( gamma - 1.0_KDR ) )
+
+    call Show ( 'Setting MeanMolecularWeight of a Fluid', F % IGNORABILITY + 1 )
+    call Show ( F % Name, 'Name', F % IGNORABILITY + 1 )
+    call Show ( mu, 'MeanMolecularWeight', &
+                F % IGNORABILITY + 1 )
+    call Show ( c_v, F % Unit ( F % ENTROPY_PER_BARYON, 1 ), &
+                'SpecificHeatVolume', F % IGNORABILITY + 1 )
+
+    end associate !-- k, etc.
+
+  end subroutine SetMeanMolecularWeight
+
+
+  subroutine SetSpecificHeatVolume ( F, SpecificHeatVolume )
+
+    !-- Assumes AdiabaticIndex already set.
+
+    class ( Fluid_P_I_Form ), intent ( inout ) :: &
+      F
+    real ( KDR ), intent ( in ) :: &
+      SpecificHeatVolume
+
+    associate &
+      ( k     => F % BoltzmannConstant, &
+        gamma => F % AdiabaticIndex, &
+        mu    => F % MeanMolecularWeight, &
+        c_v   => F % SpecificHeatVolume )
+
+    c_v  =  SpecificHeatVolume
+
+    mu  =  k / ( c_v * ( gamma - 1.0_KDR ) )
+
+    call Show ( 'Setting SpecificHeatVolume of a Fluid', F % IGNORABILITY + 1 )
+    call Show ( F % Name, 'Name', F % IGNORABILITY + 1 )
+    call Show ( c_v, F % Unit ( F % ENTROPY_PER_BARYON, 1 ), &
+                'SpecificHeatVolume', F % IGNORABILITY + 1 )
+    call Show ( F % MeanMolecularWeight, 'MeanMolecularWeight', &
+                F % IGNORABILITY + 1 )
+
+    end associate !-- amu, etc.
+
+  end subroutine SetSpecificHeatVolume
+
+
+  subroutine SetFiducialParameters &
+               ( F, FiducialBaryonDensity, FiducialPressure )
+
+    class ( Fluid_P_I_Form ), intent ( inout ) :: &
+      F
+    real ( KDR ), intent ( in ) :: &
+      FiducialBaryonDensity, &
+      FiducialPressure
+
+    F % FiducialBaryonDensity = FiducialBaryonDensity
+    F % FiducialPressure = FiducialPressure
+
+    call Show ( 'Setting fiducial parameters of a Fluid', F % IGNORABILITY + 1 )
+    call Show ( F % Name, 'Name', F % IGNORABILITY + 1 )
+    call Show ( F % FiducialBaryonDensity, &
+                F % Unit ( F % BARYON_DENSITY_C, 1 ), &
+                'FiducialBaryonDensity', F % IGNORABILITY + 1 )
+    call Show ( F % FiducialPressure, F % Unit ( F % PRESSURE, 1 ), &
+                'FiducialPressure', F % IGNORABILITY + 1 )
+
+  end subroutine SetFiducialParameters
+
+
   subroutine Show_FS ( FS )
 
     class ( Fluid_P_I_Form ), intent ( in ) :: &
@@ -167,6 +269,17 @@ contains
                 FS % Unit ( FS % ENTROPY_PER_BARYON, 1 ), &
                 'BoltzmannConstant', FS % IGNORABILITY )
     call Show ( FS % AdiabaticIndex, 'AdiabaticIndex', FS % IGNORABILITY )
+    call Show ( FS % MeanMolecularWeight, 'MeanMolecularWeight', &
+                FS % IGNORABILITY )
+    call Show ( FS % SpecificHeatVolume, &
+                FS % Unit ( FS % ENTROPY_PER_BARYON, 1 ), &
+                'SpecificHeatVolume', FS % IGNORABILITY )
+    call Show ( FS % FiducialBaryonDensity, &
+                FS % Unit ( FS % BARYON_DENSITY_C, 1 ), &
+                'FiducialBaryonDensity', FS % IGNORABILITY )
+    call Show ( FS % FiducialPressure, &
+                FS % Unit ( FS % PRESSURE, 1 ), &
+                'FiducialPressure', FS % IGNORABILITY )
 
   end subroutine Show_FS
 
