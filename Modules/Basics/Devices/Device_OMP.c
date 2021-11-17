15851d812a60ce3163fad0f89925d9eeb7152fc8
Reuben D. Budiardja <reubendb@ornl.gov>
2021-11-17 15:05:55 -0500

    AllocateDevice and AssociateHost for logical 1D. Added overloading subroutines for 2D and 4D OnDevice.

--- Modules/Basics/Devices/Device_OMP.c
+++ Modules/Basics/Devices/Device_OMP.c
@@ -61,6 +61,29 @@ void * AllocateTargetDouble_OMP ( int nValues )
   }
 
 
+void * AllocateTargetLogical_OMP ( int nValues )
+  {
+  int iDevice;
+  void * D_Pointer;
+  
+  D_Pointer = NULL; 
+  
+  #ifdef ENABLE_OMP_OFFLOAD
+  iDevice = omp_get_default_device();
+  
+  /*
+  printf("nValues Alloc: %d\n", nValues );
+  printf("iDevice: %d\n", iDevice );
+  */ 
+  D_Pointer = omp_target_alloc ( sizeof ( _Bool ) * nValues, iDevice );
+  
+  // printf("D_Pointer : %p\n", D_Pointer);
+  #endif 
+  
+  return D_Pointer;
+  }
+
+
 int AssociateTargetInteger_OMP 
       ( void * Host, void * Device, int nValues, int oValue )
   {    
@@ -121,6 +144,40 @@ int AssociateTargetDouble_OMP
   printf("Offset : %d\n", Offset);
   printf("Device : %d\n", iDevice);
   */
+  
+  retval = omp_target_associate_ptr ( Host, Device, Size, Offset, iDevice );
+  #endif 
+  //printf("retval assoc: %d\n", retval);
+  return retval;
+  }
+  
+
+int AssociateTargetLogical_OMP 
+      ( void * Host, void * Device, int nValues, int oValue )
+  {    
+  int iDevice, retval;
+  size_t Size, Offset;
+  
+  retval = -1;
+  
+  /*
+  printf("nValues Assoc: %d\n", nValues );
+  printf("Host     : %p\n", Host );
+  printf("Device   : %p\n", Device);
+  */
+
+  #ifdef ENABLE_OMP_OFFLOAD
+  Size    = sizeof ( _Bool ) * nValues;
+  Offset  = sizeof ( _Bool ) * oValue;
+  iDevice = omp_get_default_device();
+  
+  
+  printf("Host   : %p\n", Host );
+  printf("Device : %p\n", Device);
+  printf("Size   : %d\n", Size);
+  printf("Offset : %d\n", Offset);
+  printf("Device : %d\n", iDevice);
+  
   
   retval = omp_target_associate_ptr ( Host, Device, Size, Offset, iDevice );
   #endif 
--- Modules/Basics/Devices/OnDevice_Function.f90
+++ Modules/Basics/Devices/OnDevice_Function.f90
@@ -9,7 +9,9 @@ module OnDevice_Function
 
   interface OnDevice
     module procedure OnDevice_1D
+    module procedure OnDevice_2D
     module procedure OnDevice_3D
+    module procedure OnDevice_4D
   end interface OnDevice
 
   public :: &
@@ -37,6 +39,27 @@ contains
     end if
 
   end function OnDevice_1D
+  
+  
+  function OnDevice_2D ( Value ) result ( IP )
+
+    real ( KDR ), dimension ( :, : ), intent ( in ), target :: &
+      Value
+
+    integer ( c_int ) :: &
+      Retval
+    logical :: &
+      IP
+
+    Retval = OnTarget ( c_loc ( Value ) )
+
+    if ( Retval /= 0 ) then
+      IP = .true.
+    else
+      IP = .false.
+    end if
+
+  end function OnDevice_2D
 
 
   function OnDevice_3D ( Value ) result ( IP )
@@ -58,6 +81,27 @@ contains
     end if
 
   end function OnDevice_3D
+
+
+  function OnDevice_4D ( Value ) result ( IP )
+
+    real ( KDR ), dimension ( :, :, :, : ), intent ( in ), target :: &
+      Value
+
+    integer ( c_int ) :: &
+      Retval
+    logical :: &
+      IP
+
+    Retval = OnTarget ( c_loc ( Value ) )
+
+    if ( Retval /= 0 ) then
+      IP = .true.
+    else
+      IP = .false.
+    end if
+
+  end function OnDevice_4D
 
 
 end module OnDevice_Function
