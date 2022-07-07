35e570c013f8ee172697a5fa7370fb3fd88b8317
Christian Cardall <cardallcy@ornl.gov>
2022-07-07 15:26:33 -0400

    Starting FishboneMoncrief driver.

--- Programs/Examples/Physics/FluidDynamics/FishboneMoncrief_Form.f90
+++ Programs/Examples/Physics/FluidDynamics/FishboneMoncrief_Form.f90
@@ -6,7 +6,46 @@ module FishboneMoncrief_Form
   private
 
   type, public, extends ( Universe_F_CE_Form ) :: FishboneMoncriefForm
-
+  contains
+    procedure, private, pass :: &
+      Initialize_H
+    final :: &
+      Finalize
   end type FishboneMoncriefForm
+
+
+contains
+
+
+  subroutine Initialize_H ( U, NameOption )
+
+    class ( FishboneMoncriefForm ), intent ( inout ), target :: &
+      U
+    character ( * ), intent ( in ), optional :: &
+      NameOption
+
+    character ( LDL ) :: &
+      Name
+
+    if ( U % Type == '' ) &
+      U % Type = 'a FishboneMoncrief'
+
+    Name  =  'FishboneMoncrief'
+    if ( present ( NameOption ) ) &
+      Name  =  NameOption
+
+    ! call InitializeUniverse ( U, Name )
+    ! call InitializeDiagnostics ( U )
+
+  end subroutine Initialize_H
+
+
+  impure elemental subroutine Finalize ( FM )
+    
+    type ( FishboneMoncriefForm ), intent ( inout ) :: &
+      FM
+
+  end subroutine Finalize
+
 
 end module FishboneMoncrief_Form
