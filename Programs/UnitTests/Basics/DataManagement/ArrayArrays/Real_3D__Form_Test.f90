8a88071e68dbd9e36fe79ddd9f4e82a80938fb76
Reuben D. Budiardja <reubendb@ornl.gov>
2022-08-12 15:31:19 -0400

    Modified slighltly Real_3D__Form_Test to call MultiplyAdd

--- Programs/UnitTests/Basics/DataManagement/ArrayArrays/Real_3D__Form_Test.f90
+++ Programs/UnitTests/Basics/DataManagement/ArrayArrays/Real_3D__Form_Test.f90
@@ -9,7 +9,7 @@ program Real_3D__Form_Test
     SHAPE = [ 4, 3, 2 ]
   integer ( KDI ) :: &
     i
-  type ( Real_3D_Form ), dimension ( 3 ) :: &
+  type ( Real_3D_Form ), dimension ( 5 ) :: &
     R_3D
 
   call R_3D ( 1 ) % Initialize ( SHAPE, ClearOption = .true. )
@@ -31,5 +31,22 @@ program Real_3D__Form_Test
   print *, &
     'lbound ( R_3D ( 3 ) % Value ) = ', lbound ( R_3D ( 3 ) % Value )
   print *, 'R_3D ( 3 ) % Value = ', R_3D ( 3 ) % Value
+  
+  print*, 'R_3D ( 4 )'
+  call R_3D ( 4 ) % Initialize ( [ 32, 64, 128 ], ClearOption = .true. )
+  call random_number ( R_3D ( 4 ) % Value )
+  call R_3D ( 4 ) % AllocateDevice ( )
+  call R_3D ( 4 ) % UpdateDevice ( )
+  print*, 'End R_3D ( 4 )'
+  
+  print*, 'R_3D ( 5 )'
+  call R_3D ( 5 ) % Initialize ( [ 32, 64, 128 ], ClearOption = .true. )
+  call random_number ( R_3D ( 5 ) % Value )
+  call R_3D ( 5 ) % AllocateDevice ( )
+  call R_3D ( 5 ) % UpdateDevice ( )
+  print*, 'End R_3D ( 5 )'
+  
+  call R_3D ( 5 ) % MultiplyAdd ( R_3D ( 4 ), 13.0_KDR, &
+                                  UseDeviceOption = .true. )
 
 end program Real_3D__Form_Test
