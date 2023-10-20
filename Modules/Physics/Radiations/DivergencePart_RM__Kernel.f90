bf01e32d48c22fefa38a03943f844fdd224e2edd
ccardall <cardallcy@ornl.gov>
2023-10-20 12:43:25 -0400

    TARGET kernels for DivergencePart_RM___Kernel.

--- Modules/Physics/Radiations/DivergencePart_RM__Kernel.f90
+++ Modules/Physics/Radiations/DivergencePart_RM__Kernel.f90
@@ -43,6 +43,49 @@ contains
     SqrtTiny  =  sqrt ( tiny ( 0.0_KDR ) )
     if ( UseDevice ) then
     
+      !$OMP OMP_TARGET_DIRECTIVE parallel do &
+      !$OMP schedule ( OMP_SCHEDULE_TARGET ) &
+      !$OMP shared ( Delta_1, Delta_2, Delta_3, SqrtTiny ) &
+      !$OMP private ( H_Sq, K_U_Dim_D_1, K_U_Dim_D_2, K_U_Dim_D_3 )
+      do iV = 1, nV
+
+        F_E ( iV )  =  M_UU_Dim ( iV )  *  S_Dim ( iV ) 
+
+        H_Sq  =     M_DD_11 ( iV )  *  H_1 ( iV ) ** 2  &
+                 +  M_DD_22 ( iV )  *  H_2 ( iV ) ** 2  &
+                 +  M_DD_33 ( iV )  *  H_3 ( iV ) ** 2
+        H_Sq  =  max ( H_Sq, SqrtTiny )
+
+        K_U_Dim_D_1  &
+          =  0.5_KDR * ( ( 1.0_KDR  -  SF ( iV ) )  *  Delta_1  &
+                         +  ( 3.0_KDR * SF ( iV )  -  1.0_KDR )  &
+                            *  H_Dim ( iV )  &
+                            *  M_DD_11 ( iV ) * H_1 ( iV )  /  H_Sq )  &
+             *  J ( iV )
+
+        K_U_Dim_D_2  &
+          =  0.5_KDR * ( ( 1.0_KDR  -  SF ( iV ) )  *  Delta_2  &
+                         +  ( 3.0_KDR * SF ( iV )  -  1.0_KDR )  &
+                            *  H_Dim ( iV )  &
+                            *  M_DD_22 ( iV ) * H_2 ( iV )  /  H_Sq )  &
+             *  J ( iV )
+
+        K_U_Dim_D_3  &
+          =  0.5_KDR * ( ( 1.0_KDR  -  SF ( iV ) )  *  Delta_3  &
+                         +  ( 3.0_KDR * SF ( iV )  -  1.0_KDR )  &
+                            *  H_Dim ( iV )  &
+                            *  M_DD_33 ( iV ) * H_3 ( iV )  /  H_Sq )  &
+             *  J ( iV )
+
+        !-- FIXME: Add velocity dependence
+
+        F_S_1 ( iV )  =  K_U_Dim_D_1
+        F_S_2 ( iV )  =  K_U_Dim_D_2
+        F_S_3 ( iV )  =  K_U_Dim_D_3
+
+      end do !-- iV
+      !$OMP end OMP_TARGET_DIRECTIVE parallel do
+
     else
 
       !$OMP parallel do &
