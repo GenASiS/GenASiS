69dab1f13051ff3b6fe96b8325b96e95053aa9cf
ccardall <cardallcy@ornl.gov>
2023-12-18 09:51:46 -0500

    No imposing diffusion/equilibrium limit, no ignoring time step limitations.

--- Modules/Physics/Universes/Universe_R_CC__Kernel.f90
+++ Modules/Physics/Universes/Universe_R_CC__Kernel.f90
@@ -168,22 +168,28 @@ contains
 !          then
 !          if ( DI ( iV )  ==  0.0_KDR ) then
 
+!          if ( J_RD ( iV )  >  1.0e-3_KDR ) then
             FE_E  =  max ( Tolerance * SqrtTiny, abs ( E_E ( iV ) ) )  &
                      /  max ( SqrtTiny, abs ( E ( iV ) ) )
+            F_E  =  sqrt ( Tolerance / FE_E )
+            dT_E  =  min ( dT_E, F_E * dT )
+!          end if
+
+!          if ( SF_RD ( iV )  >  1.0e-6_KDR ) then
             FE_S  =  max ( Tolerance * SqrtTiny, abs ( E_S ( iV ) ) )  &
                      /  max ( SqrtTiny, &
                               1.0e-4 * abs ( E ( iV ) )  +  abs ( S ( iV ) ) )
   !                   /  max ( SqrtTiny, abs ( S ( iV ) ) )
+            F_S  =  sqrt ( Tolerance / FE_S )
+            dT_S  =  min ( dT_S, F_S * dT )
+!          end if
+
+!          if ( N_RD ( iV )  >  1.0e-3_KDR ) then
             FE_N  =  max ( Tolerance * SqrtTiny, abs ( E_N ( iV ) ) )  &
                      /  max ( SqrtTiny, abs ( N ( iV ) ) )
-
-            F_E  =  sqrt ( Tolerance / FE_E )
-            F_S  =  sqrt ( Tolerance / FE_S )
             F_N  =  sqrt ( Tolerance / FE_N )
-
-            dT_E  =  min ( dT_E, F_E * dT )
-            dT_S  =  min ( dT_S, F_S * dT )
             dT_N  =  min ( dT_N, F_N * dT )
+!          end if
 
 !          end if
 
