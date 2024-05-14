3d9c3b6f6e806d0ae94ec66f6ac7d940118fcabe
ccardall <cardallcy@ornl.gov>
2024-05-14 10:31:47 -0400

    Moving ImplicitDiagnostics_RM__Form and Step_RK_RM__Form to the end of Radiations.

--- Modules/Physics/Radiations/Radiations.f90
+++ Modules/Physics/Radiations/Radiations.f90
@@ -9,7 +9,6 @@ module Radiations
   use Slope_RM_I_I__Form
   use Slope_RM_I_I_J__Form
   use Slope_RM_DFV_I__Form
-  use Step_RK_RM__Form
   use PhotonMoments_G__Form
   use NeutrinoMoments_G__Form
   use Interactions_NM_G__Form
@@ -18,5 +17,7 @@ module Radiations
   use Slope_NM_G_I_I__Form
   use Slope_NM_G_I_I_J_N__Form
   use Slope_NM_G_DFV_I__Form
+  use ImplicitDiagnostics_RM__Form
+  use Step_RK_RM__Form
 
 end module Radiations
