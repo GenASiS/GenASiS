3ee923bec9c8340be7c50192de42b258fa19f2ca
ccardall <cardallcy@ornl.gov>
2024-07-24 16:13:44 -0400

    Missing from previous commit.

---
+++ Modules/Physics/Radiations/Interactions_NM_G__Form.f90
@@ -0,0 +1,454 @@
+module Interactions_NM_G__Form
+
+  use Basics
+  use Mathematics
+  use Fluids
+  use Units_R__Form
+  use RadiationMoments_BM__Form
+  use Interactions_BM__Form
+  use NeutrinoMoments_G__Form
+
+ implicit none
+ private
+
+     integer ( KDI ), private, parameter :: &
+      N_FIELDS_NM_G = 5
+
+  type, public, extends ( Interactions_BM_Form ) :: Interactions_NM_G_Form
+    integer ( KDI ) :: &
+      N_FIELDS_NM_G = N_FIELDS_NM_G
+    integer ( KDI ) :: &
+      EMISSIVITY_N = 0, &
+         OPACITY_N = 0
+    integer ( KDI ) :: &
+      !-- Electron capture on neutrons
+      EMISSIVITY_J_EC_N    = 0, & 
+         OPACITY_J_EC_N    = 0, &
+         OPACITY_J_EC_N_DB = 0     !-- From grey detailed balance
+    real ( KDR ) :: &
+      DensityDetailedBalance
+  contains
+    procedure, private, pass :: &
+      InitializeAllocate_I
+    procedure, public, pass ( I ) :: &
+      SetStream
+    procedure, private, pass :: &
+      ComputeAll
+    procedure, private, pass :: &
+      ComputeSingle
+    final :: &
+      Finalize
+  end type Interactions_NM_G_Form
+
+    private :: &
+      Compute_EA_E_A_Kernel
+
+    interface
+
+      module subroutine Compute_EA_E_A_Kernel &
+               ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+                 Xi_J_EC_N, Chi_J_EC_N, Chi_J_EC_N_DB, &
+                 J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+                 M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p, &
+                 Rho_DB, UseDeviceOption )
+        !-- Compute_EmissionAbsorption_Electron_All_Kernel
+        implicit none
+        real ( KDR ), dimension ( : ), intent ( inout ) :: &
+           Xi_J,  Xi_H,  Xi_N, &
+          Chi_J, Chi_H, Chi_N
+        real ( KDR ), dimension ( : ), intent ( inout ) :: &
+           Xi_J_EC_N, &
+          Chi_J_EC_N, &
+          Chi_J_EC_N_DB
+        real ( KDR ), dimension ( : ), intent ( in ) :: &
+          J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+          M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p
+        real ( KDR ), intent ( in ) :: &
+          Rho_DB
+        logical ( KDL ), intent ( in ), optional :: &
+          UseDeviceOption
+      end subroutine Compute_EA_E_A_Kernel
+
+      module subroutine Compute_EA_E_S_Kernel &
+               ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+                 Xi_J_EC_N, Chi_J_EC_N, Chi_J_EC_N_DB, &
+                 J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+                 M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p, &
+                 Rho_DB, iV )
+        !-- Compute_EmissionAbsorption_Electron_Single_Kernel
+        implicit none
+        real ( KDR ), dimension ( : ), intent ( inout ) :: &
+           Xi_J,  Xi_H,  Xi_N, &
+          Chi_J, Chi_H, Chi_N
+        real ( KDR ), dimension ( : ), intent ( inout ) :: &
+           Xi_J_EC_N, &
+          Chi_J_EC_N, &
+          Chi_J_EC_N_DB
+        real ( KDR ), dimension ( : ), intent ( in ) :: &
+          J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+          M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p
+        real ( KDR ), intent ( in ) :: &
+          Rho_DB
+        integer ( KDI ), intent ( in ) :: &
+          iV
+      end subroutine Compute_EA_E_S_Kernel
+
+    end interface
+
+
+contains
+
+
+  subroutine InitializeAllocate_I &
+               ( I, R, Units_R, F, FieldOption, NameOption, UnitOption, &
+                 nFieldsOption, IgnorabilityOption )
+
+    class ( Interactions_NM_G_Form ), intent ( inout ) :: &
+      I
+    class ( RadiationMoments_BM_Form ), intent ( inout ), target :: &
+      R
+    class ( Units_R_Form ), dimension ( : ), intent ( in ) :: &
+      Units_R
+    class ( Fluid_P_Form ), intent ( in ), target :: &
+      F
+    character ( * ), dimension ( : ), intent ( in ), optional :: &
+      FieldOption
+    character ( * ), intent ( in ), optional :: &
+      NameOption
+    type ( QuantityForm ), dimension ( :, : ), intent ( in ), optional :: &
+      UnitOption
+    integer ( KDI ), intent ( in ), optional :: &
+      nFieldsOption, &
+      IgnorabilityOption
+
+    integer ( KDI ) :: &
+      iC, &  !-- iChart
+      oF, &  !-- oField
+      nFields
+    type ( QuantityForm ), dimension ( :, : ), allocatable :: &
+      FieldUnit
+    character ( LDL ) :: &
+      Name
+    character ( LDL ), dimension ( : ), allocatable :: &
+      Field
+
+    if ( I % Type  ==  '' ) &
+      I % Type  =  'an Interactions_NM_G' 
+    
+    Name  =  'Interactions_' // trim ( R % Name )
+    if ( present ( NameOption ) ) &
+      Name  =  NameOption
+
+    !-- Field indices
+
+    oF  =  I % N_FIELDS_I
+
+    I % EMISSIVITY_N  =  oF + 1
+    I %    OPACITY_N  =  oF + 2
+
+    I % EMISSIVITY_J_EC_N     =  oF + 3 
+    I %    OPACITY_J_EC_N     =  oF + 4
+    I %    OPACITY_J_EC_N_DB  =  oF + 5
+
+    nFields  =  oF  +  I % N_FIELDS_NM_G
+    if ( present ( nFieldsOption ) ) &
+      nFields  =  nFieldsOption
+
+    !-- Field names
+
+    if ( present ( FieldOption ) ) then
+      allocate ( Field, source = FieldOption )
+    else
+      allocate ( Field ( nFields ) )
+    end if !-- FieldOption
+
+    Field ( oF + 1 : oF + I % N_FIELDS_NM_G ) &
+      = [ 'Emissivity_N     ', &
+          'Opacity_N        ', &
+          'Emissivity_J_EC_N', &
+          'Opacity_J_EC_N   ', &
+          'Opacity_J_EC_N_DB' ]
+          
+    !-- Units
+
+    associate ( nC  =>  F % Atlas % nCharts )
+
+    if ( present ( UnitOption ) ) then
+      allocate ( FieldUnit, source = UnitOption )
+    else
+      allocate ( FieldUnit ( nFields, nC ) )
+    end if !-- FieldOption
+
+    do iC  =  1, nC
+
+      FieldUnit ( I % EMISSIVITY_N, iC ) &
+        =  Units_R ( iC ) % NumberDensity  &
+           *  ( UNIT % SPEED_OF_LIGHT * Units_R ( iC ) % Time ) ** (-1)
+      FieldUnit ( I % OPACITY_N, iC ) &
+        =  ( UNIT % SPEED_OF_LIGHT * Units_R ( iC ) % Time ) ** (-1)
+
+      FieldUnit ( I % EMISSIVITY_J_EC_N, iC ) &
+        =  Units_R ( iC ) % EnergyDensity  &
+           *  ( UNIT % SPEED_OF_LIGHT * Units_R ( iC ) % Time ) ** (-1)
+      FieldUnit ( I % OPACITY_J_EC_N, iC ) &
+        =  ( UNIT % SPEED_OF_LIGHT * Units_R ( iC ) % Time ) ** (-1)
+      FieldUnit ( I % OPACITY_J_EC_N_DB, iC ) &
+        =  ( UNIT % SPEED_OF_LIGHT * Units_R ( iC ) % Time ) ** (-1)
+
+    end do !-- iC
+
+    end associate !-- nC
+
+    !-- Interactions_BM
+
+    call I % Interactions_BM_Form % Initialize &
+           ( R, Units_R, F, &
+             FieldOption = Field, &
+             NameOption = Name, &
+             UnitOption = FieldUnit, &
+             nFieldsOption = nFields, &
+             IgnorabilityOption = IgnorabilityOption )
+
+    call R % SetInteractions ( I )
+
+    !-- Parameters
+
+    I % DensityDetailedBalance  =  1.0e12_KDR * UNIT % MASS_DENSITY_CGS
+
+  end subroutine InitializeAllocate_I
+
+
+  subroutine SetStream ( S, I )
+
+    class ( Stream_BM_Form ), intent ( inout ) :: &
+      S
+    class ( Interactions_NM_G_Form ), intent ( in ) :: &
+      I
+
+    call S % AddFieldSet &
+           ( I, &
+             iaSelectedOption &
+               = [ I % EMISSIVITY_J, &
+                   I % EMISSIVITY_H, &
+                   I % EMISSIVITY_N, &
+                   I % OPACITY_J, &
+                   I % OPACITY_H, &
+                   I % OPACITY_N, &
+                   I % EMISSIVITY_J_EC_N, &
+                   I % OPACITY_J_EC_N, &
+                   I % OPACITY_J_EC_N_DB ] )
+
+  end subroutine SetStream
+
+
+  subroutine ComputeAll ( I )
+
+    class ( Interactions_NM_G_Form ), intent ( inout ) :: &
+      I
+
+    integer ( KDI ) :: &
+      iC
+
+    call Show ( 'ComputeAll', CONSOLE % INFO_6 )
+    call Show ( I % Name, 'Interactions', CONSOLE % INFO_6 )
+
+    select type ( R  =>  I % Radiation )
+      class is ( NeutrinoMoments_G_Form )
+    select type ( F  =>  I % Fluid )
+      class is ( Fluid_P_HN_Form )
+
+    call R % ComputeSpectralParameters ( )
+    call R % ComputeEquilibrium ( )
+
+    do iC  =  1,  I % Atlas % nCharts
+      associate &
+        ( IV  =>  I % Storage ( iC ) % Value, &
+          RV  =>  R % Storage ( iC ) % Value, &
+          FV  =>  F % Storage ( iC ) % Value )
+      associate &
+        (  Xi_J          =>  IV ( :, I % EMISSIVITY_J ), &
+           Xi_H          =>  IV ( :, I % EMISSIVITY_H ), &
+           Xi_N          =>  IV ( :, I % EMISSIVITY_N ), &
+           Xi_J_EC_N     =>  IV ( :, I % EMISSIVITY_J_EC_N ), &
+          Chi_J          =>  IV ( :, I % OPACITY_J ), &
+          Chi_H          =>  IV ( :, I % OPACITY_H ), &
+          Chi_N          =>  IV ( :, I % OPACITY_N ), &
+          Chi_J_EC_N     =>  IV ( :, I % OPACITY_J_EC_N ), &
+          Chi_J_EC_N_DB  =>  IV ( :, I % OPACITY_J_EC_N_DB ), &
+            T_Nu         =>  RV ( :, R % TEMPERATURE_GREY ), &
+          Eta_Nu         =>  RV ( :, R % DEGENERACY_GREY ), &
+            J_Eq         =>  RV ( :, R % ENERGY_DENSITY_C_EQ ), &
+            N_Eq         =>  RV ( :, R % NUMBER_DENSITY_C_EQ ), &
+            E_Ave        =>  RV ( :, R % ENERGY_AVERAGE ), &
+            F_Ave        =>  RV ( :, R % OCCUPANCY_AVERAGE ), &
+            M            =>  FV ( :, F % BARYON_MASS ), &
+            N            =>  FV ( :, F % BARYON_DENSITY_C ), &
+            T            =>  FV ( :, F % TEMPERATURE ), &
+            X_p          =>  FV ( :, F % MASS_FRACTION_PROTON ), &
+            X_n          =>  FV ( :, F % MASS_FRACTION_NEUTRON ), &
+            X_A          =>  FV ( :, F % MASS_FRACTION_HEAVY ), &
+            Z            =>  FV ( :, F % ATOMIC_NUMBER_HEAVY ), &
+            A            =>  FV ( :, F % MASS_NUMBER_HEAVY ), &
+           Mu_e          =>  FV ( :, F % CHEMICAL_POTENTIAL_E ), &
+           Mu_n_p        =>  FV ( :, F % CHEMICAL_POTENTIAL_N_P ) )
+
+      !-- Emission / Absorption
+
+      select case ( trim ( R % RadiationType ) )
+      case ( 'NEUTRINOS_E' )
+        call Compute_EA_E_A_Kernel &
+               ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+                 Xi_J_EC_N, Chi_J_EC_N, Chi_J_EC_N_DB, &
+                 J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+                 M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p, &
+                 Rho_DB = I % DensityDetailedBalance, &
+                 UseDeviceOption = I % DeviceMemory )
+      ! case ( 'NEUTRINOS_EB' )
+      !   call Compute_EA_E_Bar_A_Kernel &
+      !          ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+      !            J_Eq, N_Eq, F_Ave, M, N, T, X_n, Mu_e, &
+      !            UseDeviceOption = I % DeviceMemory )
+      ! case ( 'NEUTRINOS_HL' )
+      !   call I % Clear ( )
+      end select !-- RadiationType
+             
+      ! !-- Pair emission
+
+      ! select case ( trim ( R % RadiationType ) )
+      ! case ( 'NEUTRINOS_E', 'NEUTRINOS_EB' )
+      !   call Compute_P_A_Kernel &
+      !          ( Xi_J, Xi_N, Chi_J, Chi_H, Chi_N, &
+      !            J_Eq, N_Eq, M, N, T, Mu_e, &
+      !            Sign = +1, nSpecies = 1, UseDeviceOption = I % DeviceMemory )
+      ! case ( 'NEUTRINOS_HL' )
+      !   call Compute_P_A_Kernel &
+      !          ( Xi_J, Xi_N, Chi_J, Chi_H, Chi_N, &
+      !            J_Eq, N_Eq, M, N, T, Mu_e, &
+      !            Sign = -1, nSpecies = 4, UseDeviceOption = I % DeviceMemory )
+      ! end select !-- RadiationType
+
+      ! !-- Elastic scattering on nucleons and nuclei
+
+      ! call Compute_S_N_A_A_Kernel &
+      !        ( Chi_H, T_nu, Eta_nu, M, N, X_p, X_n, X_A, Z, A, &
+      !          UseDeviceOption = I % DeviceMemory )
+
+      end associate !-- Xi_J, etc.
+      end associate !-- FV, etc.
+    end do !-- iC
+
+    end select !-- F
+    end select !-- R
+
+  end subroutine ComputeAll
+
+
+  subroutine ComputeSingle ( I, iC, iV )
+
+    class ( Interactions_NM_G_Form ), intent ( inout ) :: &
+      I
+    integer ( KDI ), intent ( in ) :: &
+      iC, &
+      iV
+
+integer ( KDI ) :: &
+  iShow
+
+!    call Show ( 'ComputeSingle', CONSOLE % INFO_6 )
+!    call Show ( I % Name, 'Interactions', CONSOLE % INFO_6 )
+
+    select type ( R  =>  I % Radiation )
+      class is ( NeutrinoMoments_G_Form )
+    select type ( F  =>  I % Fluid )
+      class is ( Fluid_P_HN_Form )
+
+    associate &
+      ( I_V  =>  I % Storage ( iC ) % Value, &
+        R_V  =>  R % Storage ( iC ) % Value, &
+        F_V  =>  F % Storage ( iC ) % Value )
+    associate &
+      (  Xi_J          =>  I_V ( :, I % EMISSIVITY_J ), &
+         Xi_H          =>  I_V ( :, I % EMISSIVITY_H ), &
+         Xi_N          =>  I_V ( :, I % EMISSIVITY_N ), &
+         Xi_J_EC_N     =>  I_V ( :, I % EMISSIVITY_J_EC_N ), &
+        Chi_J          =>  I_V ( :, I % OPACITY_J ), &
+        Chi_H          =>  I_V ( :, I % OPACITY_H ), &
+        Chi_N          =>  I_V ( :, I % OPACITY_N ), &
+        Chi_J_EC_N     =>  I_V ( :, I % OPACITY_J_EC_N ), &
+        Chi_J_EC_N_DB  =>  I_V ( :, I % OPACITY_J_EC_N_DB ), &
+          T_Nu         =>  R_V ( :, R % TEMPERATURE_GREY ), &
+        Eta_Nu         =>  R_V ( :, R % DEGENERACY_GREY ), &
+          J_Eq         =>  R_V ( :, R % ENERGY_DENSITY_C_EQ ), &
+          N_Eq         =>  R_V ( :, R % NUMBER_DENSITY_C_EQ ), &
+          E_Ave        =>  R_V ( :, R % ENERGY_AVERAGE ), &
+          F_Ave        =>  R_V ( :, R % OCCUPANCY_AVERAGE ), &
+          M            =>  F_V ( :, F % BARYON_MASS ), &
+          N            =>  F_V ( :, F % BARYON_DENSITY_C ), &
+          T            =>  F_V ( :, F % TEMPERATURE ), &
+          X_p          =>  F_V ( :, F % MASS_FRACTION_PROTON ), &
+          X_n          =>  F_V ( :, F % MASS_FRACTION_NEUTRON ), &
+          X_A          =>  F_V ( :, F % MASS_FRACTION_HEAVY ), &
+          Z            =>  F_V ( :, F % ATOMIC_NUMBER_HEAVY ), &
+          A            =>  F_V ( :, F % MASS_NUMBER_HEAVY ), &
+         Mu_e          =>  F_V ( :, F % CHEMICAL_POTENTIAL_E ), &
+         Mu_n_p        =>  F_V ( :, F % CHEMICAL_POTENTIAL_N_P ) )
+
+    call R % ComputeSpectralParameters ( iC, iV )
+    call R % ComputeEquilibrium ( iC, iV )
+
+    !-- Emission / Absorption
+
+    select case ( trim ( R % RadiationType ) )
+    case ( 'NEUTRINOS_E' )
+      call Compute_EA_E_S_Kernel &
+             ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+               Xi_J_EC_N, Chi_J_EC_N, Chi_J_EC_N_DB, &
+               J_Eq, N_Eq, T_nu, Eta_nu, E_Ave, F_Ave, &
+               M, N, T, X_n, X_p, X_A, Z, A, Mu_e, Mu_n_p, &
+               Rho_DB = I % DensityDetailedBalance, iV = iV )
+    ! case ( 'NEUTRINOS_EB' )
+    !   call Compute_EA_E_Bar_S_Kernel &
+    !          ( Xi_J, Xi_H, Xi_N, Chi_J, Chi_H, Chi_N, &
+    !            J_Eq, N_Eq, F_Ave, M, N, T, X_n, Mu_e, iV )
+    ! case ( 'NEUTRINOS_HL' )
+    !   call I % Clear ( )
+    end select !-- RadiationType
+           
+    ! !-- Pair emission
+
+    ! select case ( trim ( R % RadiationType ) )
+    ! case ( 'NEUTRINOS_E', 'NEUTRINOS_EB' )
+    !   call Compute_P_S_Kernel &
+    !          ( Xi_J, Xi_N, Chi_J, Chi_H, Chi_N, &
+    !            J_Eq, N_Eq, M, N, T, Mu_e, &
+    !            Sign = +1, nSpecies = 1, iV = iV )
+    ! case ( 'NEUTRINOS_HL' )
+    !   call Compute_P_S_Kernel &
+    !          ( Xi_J, Xi_N, Chi_J, Chi_H, Chi_N, &
+    !            J_Eq, N_Eq, M, N, T, Mu_e, &
+    !            Sign = -1, nSpecies = 4, iV = iV )
+    ! end select !-- RadiationType
+
+    ! !-- Elastic scattering on nucleons and nuclei
+
+    ! call Compute_S_N_A_S_Kernel &
+    !        ( Chi_H, T_nu, Eta_nu, M, N, X_p, X_n, X_A, Z, A, iV )
+
+    end associate !-- Xi_J, etc.
+    end associate !-- FV, etc.
+
+    end select !-- F
+    end select !-- R
+
+  end subroutine ComputeSingle
+
+
+  impure elemental subroutine Finalize ( I )
+
+    type ( Interactions_NM_G_Form ), intent ( inout ) :: &
+      I
+
+  end subroutine Finalize
+
+
+end module Interactions_NM_G__Form
---
+++ Modules/Physics/Radiations/Interactions_NM_G__Kernel.f90
@@ -0,0 +1,311 @@
+#include "Preprocessor"
+
+submodule ( Interactions_NM_G__Form ) Interactions_NM_G__Kernel
+
+  use Basics 
+  
+  implicit none
+
+  real ( KDR ), private, parameter :: &
+    SqrtTiny  =  sqrt ( tiny ( 0.0_KDR ) )
+  real ( KDR ), private, parameter :: &
+     Pi    =  ( CONSTANT % PI ), &
+     Pi_2  =  ( CONSTANT % PI ) ** 2, &
+     Pi_3  =  ( CONSTANT % PI ) ** 3, &
+      amu  =  ( CONSTANT % ATOMIC_MASS_UNIT ), &
+        Q  =  ( CONSTANT % NEUTRON_MASS )  -  ( CONSTANT % PROTON_MASS ), &
+    G_F_2  =  ( CONSTANT % FERMI_COUPLING ) ** 2, &
+    g_A_2  =  ( CONSTANT % NEUTRON_AXIAL_COUPLING ) ** 2
+
+contains
+
+
+  module procedure Compute_EA_E_A_Kernel
+
+    !-- Compute_EmissionAbsorption_Electron_All_Kernel
+
+    integer ( KDI ) :: &
+      iV, &
+      nV
+    real ( KDR ) :: &
+      Factor_p, Factor_n, &
+      N_p, N_n, Eta_e_Q, F_e, &
+      Fermi_2_e_Q, Fermi_3_e_Q, Fermi_4_e_Q, Fermi_5_e_Q, &
+      Fermi_2_nu,  Fermi_3_nu,  Fermi_4_nu,  Fermi_5_nu, &
+       Xi_J_p,   Xi_H_p,  Xi_N_p, &
+      Chi_J_n,  Chi_H_n,  Chi_N_n, &
+      Chi_J_DB, Chi_H_DB, Chi_N_DB
+    logical ( KDL ) :: &
+      UseDevice      
+
+    UseDevice = .false.
+    if ( present ( UseDeviceOption ) ) &
+      UseDevice = UseDeviceOption
+      
+    nV  =  size ( J_Eq )
+
+    Factor_p  =  G_F_2 / ( 2 * Pi_3 )  *  ( 1  +  3 * g_A_2 )
+    Factor_n  =  G_F_2 / Pi            *  ( 1  +  3 * g_A_2 )
+
+    if ( UseDevice ) then
+    else
+      do iV = 1, nV
+
+        if ( T ( iV ) == 0.0_KDR ) &
+          cycle
+
+        !-- e- + p  ->  n + nu_e 
+
+        N_p  =  M ( iV )  *  N ( iV )  *  X_p ( iV )  /  amu
+
+        Eta_e_Q  =  ( Mu_e ( iV )  -  Q )  /  T ( iV )
+
+        Fermi_2_e_Q  =  Fermi_2 ( Eta_e_Q )
+        Fermi_3_e_Q  =  Fermi_3 ( Eta_e_Q )
+        Fermi_4_e_Q  =  Fermi_4 ( Eta_e_Q )
+        Fermi_5_e_Q  =  Fermi_5 ( Eta_e_Q )
+
+        Xi_J_p  =  Factor_p  *  N_p  *  T ( iV ) ** 4  &
+                   *  (    T ( iV ) ** 2     *  Fermi_5_e_Q  &
+                        +  2 * Q * T ( iV )  *  Fermi_4_e_Q  &
+                        +  Q ** 2            *  Fermi_3_e_Q )  &
+                   *  ( 1.0_KDR  -  F_Ave ( iV ) )
+
+        Xi_H_p  =  0.0_KDR
+
+        Xi_N_p  =  Factor_p  *  N_p  *  T ( iV ) ** 3  &
+                   *  (    T ( iV ) ** 2     *  Fermi_4_e_Q  &
+                        +  2 * Q * T ( iV )  *  Fermi_3_e_Q  &
+                        +  Q ** 2            *  Fermi_2_e_Q )  &
+                   *  ( 1.0_KDR  -  F_Ave ( iV ) )
+
+        !-- nu_e + n  ->  p + e-
+
+        N_n  =  M ( iV )  *  N ( iV )  *  X_n ( iV )  /  amu
+
+        Fermi_2_nu  =  Fermi_2 ( Eta_nu ( iV ) )
+        Fermi_3_nu  =  Fermi_3 ( Eta_nu ( iV ) )
+        Fermi_4_nu  =  Fermi_4 ( Eta_nu ( iV ) )
+        Fermi_5_nu  =  Fermi_5 ( Eta_nu ( iV ) )
+
+        F_e  =  1.  &
+                /  ( 1. +  &
+                     exp ( ( E_Ave ( iV )  -  Mu_e ( iV ) ) / T ( iV ) ) )
+
+        Chi_J_n  =  Factor_n  *  N_n  /  Fermi_3_nu  &
+                   *  (    T_nu ( iV ) ** 2     *  Fermi_5_nu  &
+                        +  2 * Q * T_nu ( iV )  *  Fermi_4_nu  &
+                        +  Q ** 2               *  Fermi_3_nu )  &
+                   *  ( 1.0_KDR  -  F_e )
+
+        Chi_H_n  =  Chi_J_n
+
+        Chi_N_n  =  Factor_n  *  N_n  /  Fermi_2_nu  &
+                   *  (    T_nu ( iV ) ** 2     *  Fermi_4_nu  &
+                        +  2 * Q * T_nu ( iV )  *  Fermi_3_nu  &
+                        +  Q ** 2               *  Fermi_2_nu )  &
+                   *  ( 1.0_KDR  -  F_e )
+
+        !-- nu_e + n  ->  p + e-, detailed balance
+
+        Chi_J_DB  =  Xi_J_p / J_Eq ( iV )
+
+        Chi_H_DB  =  Chi_J_DB
+
+        Chi_N_DB  =  Xi_N_p / N_Eq ( iV )
+
+        !-- Total
+
+         Xi_J_EC_N    ( iV )  =   Xi_J_p
+        Chi_J_EC_N    ( iV )  =  Chi_J_n
+        Chi_J_EC_N_DB ( iV )  =  Chi_J_DB
+
+        Xi_J ( iV )  =  Xi_J_p
+        Xi_H ( iV )  =  Xi_H_p
+        Xi_N ( iV )  =  Xi_N_p
+        
+        if ( M ( iV )  *  N ( iV )  <  Rho_DB ) then
+          Chi_J ( iV )  =  Chi_J_n
+          Chi_H ( iV )  =  Chi_H_n
+          Chi_N ( iV )  =  Chi_N_n
+        else
+          Chi_J ( iV )  =  Chi_J_DB
+          Chi_H ( iV )  =  Chi_H_DB
+          Chi_N ( iV )  =  Chi_N_DB
+        end if
+
+      end do
+    end if
+
+  end procedure Compute_EA_E_A_Kernel
+
+
+  module procedure Compute_EA_E_S_Kernel
+
+    !-- Compute_EmissionAbsorption_Electron_Single_Kernel
+
+    real ( KDR ) :: &
+      Factor_p, Factor_n, &
+      N_p, N_n, Eta_e_Q, F_e, &
+      Fermi_2_e_Q, Fermi_3_e_Q, Fermi_4_e_Q, Fermi_5_e_Q, &
+      Fermi_2_nu,  Fermi_3_nu,  Fermi_4_nu,  Fermi_5_nu, &
+       Xi_J_p,   Xi_H_p,  Xi_N_p, &
+      Chi_J_n,  Chi_H_n,  Chi_N_n, &
+      Chi_J_DB, Chi_H_DB, Chi_N_DB
+
+    Factor_p  =  G_F_2 / ( 2 * Pi_3 )  *  ( 1  +  3 * g_A_2 )
+    Factor_n  =  G_F_2 / Pi            *  ( 1  +  3 * g_A_2 )
+
+    if ( T ( iV ) == 0.0_KDR ) &
+      return
+
+    !-- e- + p  ->  n + nu_e 
+
+    N_p  =  M ( iV )  *  N ( iV )  *  X_p ( iV )  /  amu
+
+    Eta_e_Q  =  ( Mu_e ( iV )  -  Q )  /  T ( iV )
+
+    Fermi_2_e_Q  =  Fermi_2 ( Eta_e_Q )
+    Fermi_3_e_Q  =  Fermi_3 ( Eta_e_Q )
+    Fermi_4_e_Q  =  Fermi_4 ( Eta_e_Q )
+    Fermi_5_e_Q  =  Fermi_5 ( Eta_e_Q )
+
+    Xi_J_p  =  Factor_p  *  N_p  *  T ( iV ) ** 4  &
+               *  (    T ( iV ) ** 2     *  Fermi_5_e_Q  &
+                    +  2 * Q * T ( iV )  *  Fermi_4_e_Q  &
+                    +  Q ** 2            *  Fermi_3_e_Q )  &
+               *  ( 1.0_KDR  -  F_Ave ( iV ) )
+
+    Xi_H_p  =  0.0_KDR
+
+    Xi_N_p  =  Factor_p  *  N_p  *  T ( iV ) ** 3  &
+               *  (    T ( iV ) ** 2     *  Fermi_4_e_Q  &
+                    +  2 * Q * T ( iV )  *  Fermi_3_e_Q  &
+                    +  Q ** 2            *  Fermi_2_e_Q )  &
+               *  ( 1.0_KDR  -  F_Ave ( iV ) )
+
+    !-- nu_e + n  ->  p + e-
+
+    N_n  =  M ( iV )  *  N ( iV )  *  X_n ( iV )  /  amu
+
+    Fermi_2_nu  =  Fermi_2 ( Eta_nu ( iV ) )
+    Fermi_3_nu  =  Fermi_3 ( Eta_nu ( iV ) )
+    Fermi_4_nu  =  Fermi_4 ( Eta_nu ( iV ) )
+    Fermi_5_nu  =  Fermi_5 ( Eta_nu ( iV ) )
+
+    F_e  =  1.  &
+            /  ( 1. +  &
+                 exp ( ( E_Ave ( iV )  -  Mu_e ( iV ) ) / T ( iV ) ) )
+
+    Chi_J_n  =  Factor_n  *  N_n  /  Fermi_3_nu  &
+               *  (    T_nu ( iV ) ** 2     *  Fermi_5_nu  &
+                    +  2 * Q * T_nu ( iV )  *  Fermi_4_nu  &
+                    +  Q ** 2               *  Fermi_3_nu )  &
+               *  ( 1.0_KDR  -  F_e )
+
+    Chi_H_n  =  Chi_J_n
+
+    Chi_N_n  =  Factor_n  *  N_n  /  Fermi_2_nu  &
+               *  (    T_nu ( iV ) ** 2     *  Fermi_4_nu  &
+                    +  2 * Q * T_nu ( iV )  *  Fermi_3_nu  &
+                    +  Q ** 2               *  Fermi_2_nu )  &
+               *  ( 1.0_KDR  -  F_e )
+
+    !-- nu_e + n  ->  p + e-, detailed balance
+
+    Chi_J_DB  =  Xi_J_p / J_Eq ( iV )
+
+    Chi_H_DB  =  Chi_J_DB
+
+    Chi_N_DB  =  Xi_N_p / N_Eq ( iV )
+
+    !-- Total
+
+     Xi_J_EC_N    ( iV )  =   Xi_J_p
+    Chi_J_EC_N    ( iV )  =  Chi_J_n
+    Chi_J_EC_N_DB ( iV )  =  Chi_J_DB
+
+    Xi_J ( iV )  =  Xi_J_p
+    Xi_H ( iV )  =  Xi_H_p
+    Xi_N ( iV )  =  Xi_N_p
+    
+    ! if ( M ( iV )  *  N ( iV )  <  Rho_DB ) then
+    !   Chi_J ( iV )  =  Chi_J_n
+    !   Chi_H ( iV )  =  Chi_H_n
+    !   Chi_N ( iV )  =  Chi_N_n
+    ! else
+      Chi_J ( iV )  =  Chi_J_DB
+      Chi_H ( iV )  =  Chi_H_DB
+      Chi_N ( iV )  =  Chi_N_DB
+    ! end if
+
+  end procedure Compute_EA_E_S_Kernel
+
+
+  function Fermi_2 ( Eta ) result ( F_2 )
+
+    real ( KDR ), intent ( in ) :: &
+      Eta
+    real ( KDR ) :: &
+      F_2
+
+    if ( Eta  >  0.0_KDR ) then
+      F_2  =  Eta**3 / 3.  +  4. * Eta  +  2. * exp ( -Eta )
+    else
+      F_2  =  2. * exp ( Eta )
+    end if
+
+  end function Fermi_2
+
+
+  function Fermi_3 ( Eta ) result ( F_3 )
+
+    real ( KDR ), intent ( in ) :: &
+      Eta
+    real ( KDR ) :: &
+      F_3
+
+    if ( Eta  >  0.0_KDR ) then
+      F_3  =  Eta**4 / 4.  +  Pi_2 * Eta**2 / 2.  +  12.  -  6. * exp ( -Eta )
+    else
+      F_3  =  6. * exp ( Eta )
+    end if
+
+  end function Fermi_3
+
+
+  function Fermi_4 ( Eta ) result ( F_4 )
+
+    real ( KDR ), intent ( in ) :: &
+      Eta
+    real ( KDR ) :: &
+      F_4
+
+    if ( Eta  >  0.0_KDR ) then
+      F_4  =  Eta**5 / 5.  +  2. * Pi_2 * Eta**3 / 3.  +  48. * Eta  &
+              +  24. * exp ( -Eta )
+    else
+      F_4  =  24. * exp ( Eta )
+    end if
+
+  end function Fermi_4
+
+
+  function Fermi_5 ( Eta ) result ( F_5 )
+
+    real ( KDR ), intent ( in ) :: &
+      Eta
+    real ( KDR ) :: &
+      F_5
+
+    if ( Eta  >  0.0_KDR ) then
+      F_5  =  Eta**6 / 6.  +  5. * Pi_2 * Eta**4 / 6.  +  110. * Eta**2  &
+              +  240.  -  120. * exp ( -Eta )
+    else
+      F_5  =  120. * exp ( Eta )
+    end if
+
+  end function Fermi_5
+
+
+end submodule Interactions_NM_G__Kernel
