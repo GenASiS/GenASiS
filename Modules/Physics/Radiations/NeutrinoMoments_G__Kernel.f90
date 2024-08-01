638a36e8ca9b2af6f18e96d33674e239505093e7
Reuben D. Budiardja <reubendb@ornl.gov>
2024-08-01 13:31:02 -0400

    Revert "Tweaks to parameters in solver for neutrino degeneracy parameter, to accommodate dropping core temperature in nCellsPolar=128 models."
    This reverts commit 68b5687b9783cc05124c5c36f3683ae30e0be227.

--- Modules/Physics/Radiations/NeutrinoMoments_G__Kernel.f90
+++ Modules/Physics/Radiations/NeutrinoMoments_G__Kernel.f90
@@ -571,7 +571,7 @@ contains
  !                   /  SixPi_2 ** ( 1.0_KDR / 3.0_KDR )
  !   Factor_ED_2  =  6.0_KDR  /  Pi ** 2
 
-    EtaMax  =  50.
+    EtaMax  =  25.
 
     if ( UseDevice ) then
   !     !$OMP OMP_TARGET_DIRECTIVE parallel do &
@@ -697,7 +697,7 @@ contains
  !                   /  SixPi_2 ** ( 1.0_KDR / 3.0_KDR )
  !   Factor_ED_2  =  6.0_KDR  /  Pi ** 2
 
-    EtaMax  =  50.
+    EtaMax  =  25.
 
     if ( J ( iV )  <=  0.0_KDR  .or.  N ( iV )  <=  0.0_KDR ) &
       return
@@ -1121,7 +1121,7 @@ contains
       RelativePrecision
       
     MaxIterations  =  50
-    Tolerance      =  1.0e-6_KDR !epsilon ( 1.0_KDR ) * 10.0_KDR 
+    Tolerance      =  1.0e-9_KDR !epsilon ( 1.0_KDR ) * 10.0_KDR 
     Factor         =  1.6_KDR
 
     X_0  =  0.0
