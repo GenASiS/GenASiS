06b102820ec57978bdf14a7751c8efe79fbc7757
ccardall <cardallcy@ornl.gov>
2026-03-08 14:18:25 -0400

    With a hack (should really have a _HN_ extension), adding HEAVY_NUCLEUS to features. This extends diffusive flux out to about 25km, suppressing another troublesome region.

--- Modules/Physics/Fluids/Features_F_P__Form.f90
+++ Modules/Physics/Fluids/Features_F_P__Form.f90
@@ -10,7 +10,7 @@ module Features_F_P__Form
   private
 
     integer ( KDI ), private, parameter :: &
-      N_FIELDS_P  = 7, &
+      N_FIELDS_P  = 8, &
       N_VECTORS_P = 0
 
   type, public, extends ( Features_CS_Form ) :: Features_F_P_Form
@@ -20,7 +20,8 @@ module Features_F_P__Form
       EOS_ERROR        = 0, &
       SHOCK            = 0, &
       PHASE_TRANSITION = 0, &
-      JAGGED_ENTROPY   = 0
+      JAGGED_ENTROPY   = 0, &
+      HEAVY_NUCLEUS    = 0
     integer ( KDI ), dimension ( 3 ) :: &
       SHOCK_I = 0
     real ( KDR ) :: &
@@ -45,7 +46,8 @@ module Features_F_P__Form
     private :: &
       DetectShocksKernel, &
       DetectPhaseTransitionKernel, &
-      DetectJaggedEntropyKernel
+      DetectJaggedEntropyKernel, &
+      DetectHeavyNucleusKernel
 
   interface
   
@@ -109,6 +111,26 @@ module Features_F_P__Form
       logical ( KDL ), intent ( in ), optional :: &
         UseDeviceOption
     end subroutine DetectJaggedEntropyKernel
+
+    module subroutine DetectHeavyNucleusKernel &
+             ( HN, DF_I_iD, DF_I_jD, DF_I_kD, XA, HNT, iD, jD, kD, oV, &
+               UseDeviceOption )
+      use Basics
+      real ( KDR ), dimension ( :, :, : ), intent ( inout ) :: &
+        HN, &
+        DF_I_iD, &
+        DF_I_jD, &
+        DF_I_kD
+      real ( KDR ), dimension ( :, :, : ), intent ( in ) :: &
+        XA
+      real ( KDR ), intent ( in ) :: &
+        HNT
+      integer ( KDI ), intent ( in ) :: &
+        iD, jD, kD, &
+        oV
+      logical ( KDL ), intent ( in ), optional :: &
+        UseDeviceOption
+    end subroutine DetectHeavyNucleusKernel
 
     module subroutine ClearBoundaryKernel &
                ( S, PT, S_I_iD, DF_I_iD, DF_I_jD, DF_I_kD, &
@@ -180,7 +202,8 @@ contains
     F % SHOCK            =  oF + 2
     F % PHASE_TRANSITION =  oF + 3
     F % JAGGED_ENTROPY   =  oF + 4
-    F % SHOCK_I          =  oF + [ 5, 6, 7 ]
+    F % HEAVY_NUCLEUS    =  oF + 5
+    F % SHOCK_I          =  oF + [ 6, 7, 8 ]
 
     nFields  =  oF  +  F % N_FIELDS_P
     if ( present ( nFieldsOption ) ) &
@@ -199,6 +222,7 @@ contains
           'Shock          ', &
           'PhaseTransition', &
           'JaggedEntropy  ', &
+          'HeavyNucleus   ', &
           'Shock_I_1      ', &
           'Shock_I_2      ', &
           'Shock_I_3      ' ]
@@ -251,10 +275,14 @@ contains
       S_I_iD, &
       PT, &
       JE, &
+      HN, &
       P, &
       Gamma, &
       SB, &
+      XA, &
       V_iD
+integer ( KDI ) :: &
+  iHeavy
 
     call Show ( 'Detecting Fluid features', CONSOLE % INFO_6 )
     call Show ( F % Name, 'Features', CONSOLE % INFO_6 )
@@ -273,12 +301,16 @@ contains
         ( FV   =>  F  % Storage ( iC ) % Value, &
           FPV  =>  FP % Storage ( iC ) % Value )
 
+iHeavy = 22
       call C % SetFieldPointer ( FV  ( :, F  % SHOCK ),              S )
       call C % SetFieldPointer ( FV  ( :, F  % PHASE_TRANSITION ),   PT )
       call C % SetFieldPointer ( FV  ( :, F  % JAGGED_ENTROPY ),     JE )
+      call C % SetFieldPointer ( FV  ( :, F  % HEAVY_NUCLEUS ),      HN )
       call C % SetFieldPointer ( FPV ( :, FP % PRESSURE ),           P )
       call C % SetFieldPointer ( FPV ( :, FP % ADIABATIC_INDEX ),    Gamma )
       call C % SetFieldPointer ( FPV ( :, FP % ENTROPY_PER_BARYON ), SB )
+      call C % SetFieldPointer ( FPV ( :, FP % ENTROPY_PER_BARYON ), SB )
+      call C % SetFieldPointer ( FPV ( :, iHeavy ), XA )
 
       do iD = 1, C % nDimensions
 
@@ -308,6 +340,10 @@ contains
         call DetectJaggedEntropyKernel &
                ( JE, DF_I_iD, DF_I_jD, DF_I_kD, SB, &
                  F % ShockThreshold, iD, jD, kD, C % nGhostLayers ( iD ), &
+                 UseDeviceOption = F % DeviceMemory )
+        call DetectHeavyNucleusKernel &
+               ( HN, DF_I_iD, DF_I_jD, DF_I_kD, XA, &
+                 0.01_KDR, iD, jD, kD, C % nGhostLayers ( iD ), &
                  UseDeviceOption = F % DeviceMemory )
 
       end do !-- iD
@@ -368,7 +404,11 @@ contains
 
     call S % AddFieldSet &
            ( F, iaSelectedOption &
-                  = [ F % DIFFUSIVE_FLUX_I, F % SHOCK, F % PHASE_TRANSITION ] )
+                  = [ F % DIFFUSIVE_FLUX_I, &
+                      F % SHOCK, &
+                      F % PHASE_TRANSITION, &
+                      F % JAGGED_ENTROPY, &
+                      F % HEAVY_NUCLEUS ] )
 
   end subroutine SetStream
 
