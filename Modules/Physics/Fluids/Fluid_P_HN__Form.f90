4ec78c3bdb73f9e1462c3b2fa728017d4357ed93
ccardall <cardallcy@ornl.gov>
2026-03-14 12:21:38 -0400

    Higher threshold for switching from evolved entropy to evolved conserved energy.

--- Modules/Physics/Fluids/Fluid_P_HN__Form.f90
+++ Modules/Physics/Fluids/Fluid_P_HN__Form.f90
@@ -1095,7 +1095,7 @@ contains
           associate ( FV  =>  CS % Features % Storage ( iC ) % Value )
           associate ( Shock  =>  FV ( :, F % SHOCK ) )
           call CS % EOS % ComputeFromEnergyEntropy &
-                 ( FS, Mask = Shock, Threshold = 0.0_KDR, &
+                 ( FS, Mask = Shock, Threshold = 0.2_KDR, &
                    iaFluidInput = [ CS % BARYON_DENSITY_C, &
                                     CS % TEMPERATURE, &
                                     CS % ELECTRON_FRACTION ], &
